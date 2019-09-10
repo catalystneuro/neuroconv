5614c31f6ea6d289d10b8941d88ad3973939c714
luiztauffer <luiz_tauffer@hotmail.com>
2019-09-10 23:18:55 +0300

    sketch of NWBFile form

--- nwbn_conversion_tools/gui/main_gui.py
+++ nwbn_conversion_tools/gui/main_gui.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QApplication, QAction,
     QTabWidget, QPushButton, QLineEdit, QTextEdit, QVBoxLayout, QHBoxLayout,
-    QGridLayout, QSplitter, QLabel, QFileDialog)
+    QGridLayout, QSplitter, QLabel, QFileDialog, QGroupBox)
 import numpy as np
 import os
 import sys
@@ -61,42 +61,87 @@ class Application(QMainWindow):
         btn_load_meta.clicked.connect(lambda: self.open_meta_file())
         btn_save_meta = QPushButton('Save to file')
         btn_save_meta.clicked.connect(lambda: self.save_meta_file())
-        label1 = QLabel('Something:')
-        line1 = QLineEdit('User enter here')
-        label2 = QLabel('Something else:')
-        line2 = QLineEdit('User enter here')
+        #label1 = QLabel('Something:')
+        #line1 = QLineEdit('User enter here')
+        #label2 = QLabel('Something else:')
+        #line2 = QLineEdit('User enter here')
 
-        grid1 = QGridLayout()
-        grid1.addWidget(btn_load_meta, 0, 0, 1, 3)
-        grid1.addWidget(btn_save_meta, 0, 3, 1, 3)
-        grid1.addWidget(QLabel(), 1, 0, 1, 6)
-        grid1.addWidget(label1, 2, 0, 1, 3)
-        grid1.addWidget(line1, 2, 3, 1, 3)
-        grid1.addWidget(label2, 3, 0, 1, 3)
-        grid1.addWidget(line2, 3, 3, 1, 3)
+        l_grid1 = QGridLayout()
+        l_grid1.addWidget(btn_load_meta, 0, 0, 1, 3)
+        l_grid1.addWidget(btn_save_meta, 0, 3, 1, 3)
 
-        vbox1 = QVBoxLayout()
-        vbox1.addLayout(grid1)
-        vbox1.addStretch()
+        l_vbox1 = QVBoxLayout()
+        l_vbox1.addLayout(l_grid1)
+        l_vbox1.addWidget(QLabel())
+        l_vbox1.addWidget(self.create_group_nwbfile())
+        l_vbox1.addStretch()
 
         # Right-side panel: meta-data text
-        vbox2 = QVBoxLayout()
         r_label1 = QLabel('Resulting meta-data file:')
+        btn_editor_form = QPushButton('Editor -> Form')
+        btn_editor_form.clicked.connect(lambda: self.editor_to_form())
+        btn_form_editor = QPushButton('Form -> Editor')
+        btn_form_editor.clicked.connect(lambda: self.form_to_editor())
+        r_grid1 = QGridLayout()
+        r_grid1.addWidget(r_label1, 0, 0, 1, 4)
+        r_grid1.addWidget(btn_editor_form, 0, 4, 1, 1)
+        r_grid1.addWidget(btn_form_editor, 0, 5, 1, 1)
+
         self.text1 = QTextEdit()
-        vbox2.addWidget(r_label1)
-        vbox2.addWidget(self.text1)
+        r_vbox1 = QVBoxLayout()
+        r_vbox1.addLayout(r_grid1)
+        r_vbox1.addWidget(self.text1)
 
         # Main Layout
         left_w = QWidget()
-        left_w.setLayout(vbox1)
+        left_w.setLayout(l_vbox1)
         right_w = QWidget()
-        right_w.setLayout(vbox2)
+        right_w.setLayout(r_vbox1)
         self.tab1.layout.addWidget(left_w)
         self.tab1.layout.addWidget(right_w)
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.tab1.layout)
         self.tab1.setLayout(main_layout)
 
+
+    def create_group_nwbfile(self):
+        groupBox = QGroupBox('NWBFile')
+        lbl_session_description = QLabel('session_description:')
+        lin_session_description = QLineEdit('')
+        lbl_identifier = QLabel('identifier:')
+        lin_identifier = QLineEdit('')
+        lbl_session_start_time = QLabel('session_start_time:')
+        lin_session_start_time = QLineEdit('')
+        lbl_file_create_date = QLabel('file_create_date:')
+        lin_file_create_date = QLineEdit('')
+        lbl_experimenter = QLabel('experimenter:')
+        lin_experimenter = QLineEdit('')
+        lbl_experiment_description = QLabel('experiment_description:')
+        lin_experiment_description = QLineEdit('')
+        lbl_session_id = QLabel('session_id:')
+        lin_session_id = QLineEdit('')
+        lbl_institution = QLabel('institution:')
+        lin_institution = QLineEdit('')
+
+        grid = QGridLayout()
+        grid.addWidget(lbl_session_description, 0, 0, 1, 3)
+        grid.addWidget(lin_session_description, 0, 3, 1, 3)
+        grid.addWidget(lbl_identifier, 1, 0, 1, 3)
+        grid.addWidget(lin_identifier, 1, 3, 1, 3)
+        grid.addWidget(lbl_session_start_time, 2, 0, 1, 3)
+        grid.addWidget(lin_session_start_time, 2, 3, 1, 3)
+        grid.addWidget(lbl_file_create_date, 3, 0, 1, 3)
+        grid.addWidget(lin_file_create_date, 3, 3, 1, 3)
+        grid.addWidget(lbl_experimenter, 4, 0, 1, 3)
+        grid.addWidget(lin_experimenter, 4, 3, 1, 3)
+        grid.addWidget(lbl_experiment_description, 5, 0, 1, 3)
+        grid.addWidget(lin_experiment_description, 5, 3, 1, 3)
+        grid.addWidget(lbl_session_id, 6, 0, 1, 3)
+        grid.addWidget(lin_session_id, 6, 3, 1, 3)
+        grid.addWidget(lbl_institution, 7, 0, 1, 3)
+        grid.addWidget(lin_institution, 7, 3, 1, 3)
+        groupBox.setLayout(grid)
+        return groupBox
 
     def open_meta_file(self):
         ''' Opens .txt file containing metadata for NWB.'''
@@ -110,6 +155,16 @@ class Application(QMainWindow):
 
     def save_meta_file(self):
         ''' Saves metadata to .txt file.'''
+        pass
+
+
+    def form_to_editor(self):
+        """Loads data from form to editor."""
+        pass
+
+
+    def editor_to_form(self):
+        """Loads data from editor to form."""
         pass
 
 
