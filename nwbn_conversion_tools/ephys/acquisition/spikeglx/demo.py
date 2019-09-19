27c3726e2c7d5522540f52dd6bdcda6c49960910
luiztauffer <luiz_tauffer@hotmail.com>
2019-09-19 17:02:29 +0300

    spikeglx extractor demo

---
+++ nwbn_conversion_tools/ephys/acquisition/spikeglx/demo.py
@@ -0,0 +1,18 @@
+from nwbn_conversion_tools.ephys.acquisition.spikeglx.spikeglx import Spikeglx2NWB
+import yaml
+
+metafile = 'metafile.yml'
+with open(metafile, 'r') as f:
+    metadata = yaml.safe_load(f)
+npx_file = 'G4_190620_keicontrasttrack_10secBaseline1_g0_t0.imec0.ap.bin'
+
+extractor = Spikeglx2NWB(nwbfile=None, metadata=metadata, npx_file=npx_file)
+
+# To visualize NWB contents:
+print(extractor.nwbfile)
+
+# To add Acquisition:
+extractor.add_acquisition(es_name='ElectricalSeries', metadata=metadata['Ephys'])
+
+# To save content to NWB file:
+extractor.save(to_path='output.nwb')
