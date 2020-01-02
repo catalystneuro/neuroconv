df67a1c01eecb2d7fb989750f90280bc93aeb831
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-01-02 15:41:12 -0800

    in NWBConverter add support for subject, devices, and icephys electrodes

--- nwbn_conversion_tools/base.py
+++ nwbn_conversion_tools/base.py
@@ -1,42 +1,80 @@
-from datetime import datetime
-from dateutil.tz import tzlocal
+import uuid
 
 from pynwb import NWBFile, NWBHDF5IO
+from pynwb.file import Subject
 
 
-nwbfile = NWBFile('my first synthetic recording', 'EXAMPLE_ID', datetime.now(tzlocal()),
-                  experimenter='Dr. Bilbo Baggins',
-                  lab='Bag End Laboratory',
-                  institution='University of Middle Earth at the Shire',
-                  experiment_description=('I went on an adventure with thirteen '
-                                          'dwarves to reclaim vast treasures.'),
-                  session_id='LONELYMTN')
+class NWBConverter:
 
-
-class Convert2NWB:
-
-    def __init__(self, nwbfile=None, metadata={}):
+    def __init__(self, metadata, nwbfile=None):
         """
 
         Parameters
         ----------
+        metadata: dict
         nwbfile: pynwb.NWBFile
         """
         if nwbfile is None:
-            self.nwbfile = NWBFile(**metadata['NWBFile'])
+            nwbfile_args = dict(
+                identifier=str(uuid.uuid4()),
+            )
+            nwbfile_args.update(metadata['NWBFile'])
+            self.nwbfile = NWBFile(**nwbfile_args)
         else:
             self.nwbfile = nwbfile
 
-    def save(self, to_path):
+        if 'Subject' in metadata:
+            self.nwbfile.subject = Subject(**metadata['Subject'])
+
+        # add devices
+        self.devices = dict()
+        for domain in ('Icephys', 'Ecephys', 'Ophys'):
+            if domain in metadata and 'Device' in metadata[domain]:
+                self.devices.update(self.create_devices(metadata[domain]['Device']))
+
+        if 'Icephys' in metadata and 'Electrodes' in metadata['Icephys']:
+            self.elecs = self.create_icephys_elecs(metadata['Icephys']['Electrodes'])
+
+    def create_devices(self, device_meta):
+        devices = dict()
+        if isinstance(device_meta, list):
+            for idevice_meta in device_meta:
+                if 'tag' in device_meta:
+                    devices[idevice_meta['tag']] = self.nwbfile.create_device(**idevice_meta)
+        else:
+            devices[str(uuid.uuid4())] = self.nwbfile.create_device(**device_meta)
+        return devices
+
+    def create_icephys_elecs(self, elec_meta):
+        if isinstance(elec_meta, list):
+            for ielec_meta in elec_meta:
+                if ielec_meta['device'] in self.devices:
+                    device = self.devices[ielec_meta['device']]
+                else:
+                    raise ValueError('device not found for icephys electrode {}'.format(ielec_meta['name']))
+                self.nwbfile.create_ic_electrode(device=device, **ielec_meta)
+        else:
+            if len(list(self.devices)) == 1:
+                self.nwbfile.create_ic_electrode(device=list(self.devices.values())[0], **elec_meta)
+            else:
+                raise ValueError('must specify device for icephys electrode {}'.format(elec_meta['name']))
+
+    def save(self, to_path, read_check=True):
         """
 
         Parameters
         ----------
         to_path: str
+        read_check: bool
+            If True, try to read the file after writing
         """
 
         with NWBHDF5IO(to_path, 'w') as io:
             io.write(self.nwbfile)
+
+        if read_check:
+            with NWBHDF5IO(to_path, 'r') as io:
+                io.read()
 
     def check_module(self, name, description=None):
         """Check if processing module exists. If not, create it. Then return module
@@ -57,4 +95,4 @@ class Convert2NWB:
         else:
             if description is None:
                 description = name
-            return nwbfile.create_processing_module(name, description)
+            return self.nwbfile.create_processing_module(name, description)
