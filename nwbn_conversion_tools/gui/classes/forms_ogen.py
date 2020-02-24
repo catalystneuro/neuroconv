74128f67e82c5c849fa89d06a0398a08ebdb69d0
luiztauffer <luiz_tauffer@hotmail.com>
2020-02-24 16:56:40 +0700

    small fix ogen

--- nwbn_conversion_tools/gui/classes/forms_ogen.py
+++ nwbn_conversion_tools/gui/classes/forms_ogen.py
@@ -91,12 +91,13 @@ class GroupOgen(QGroupBox):
         grp_type_count = {value: len(list(freq)) for value, freq in groupby(sorted(grp_types))}
         # initiate lists as values for groups keys with count > 1
         for k, v in grp_type_count.items():
-            if v > 1 or k == 'Device' or k == 'ElectrodeGroup' or k == 'ElectricalSeries':
+            if v > 1 or k == 'Device' or k == 'OptogeneticStimulusSite' or k == 'OptogeneticSeries':
                 data[k] = []
         # iterate over existing groups and copy their metadata
         for grp in self.groups_list:
             if grp_type_count[grp.group_type] > 1 or grp.group_type == 'Device' \
-               or grp.group_type == 'ElectrodeGroup' or grp.group_type == 'ElectricalSeries':
+               or grp.group_type == 'OptogeneticStimulusSite' \
+               or grp.group_type == 'OptogeneticSeries':
                 data[grp.group_type].append(grp.read_fields())
             else:
                 data[grp.group_type] = grp.read_fields()
