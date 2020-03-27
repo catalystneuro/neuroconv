c686730cd0e97ebe6587fa84fb627e1af39e293e
luiztauffer <luiz_tauffer@hotmail.com>
2020-03-27 17:23:03 +0700

    update Intan2NWB

--- nwbn_conversion_tools/ecephys/intan/intan.py
+++ nwbn_conversion_tools/ecephys/intan/intan.py
@@ -2,7 +2,7 @@ from pynwb import NWBFile
 from pynwb.ecephys import ElectricalSeries
 from hdmf.data_utils import DataChunkIterator
 from nwbn_conversion_tools.converter import NWBConverter
-from nwbn_conversion_tools.ephys.intan.load_intan import load_intan, read_header
+from nwbn_conversion_tools.ecephys.intan.load_intan import load_intan, read_header
 
 from pathlib import Path
 from datetime import datetime
@@ -56,12 +56,15 @@ class Intan2NWB(NWBConverter):
         date_time_obj = datetime.strptime(date_time_string, '%y%m%d %H%M%S')
         return {'session_start_time': date_time_obj}
 
-    def add_acquisition(self, metadata_ecephys):
+    def run_conversion(self):
         """
         Reads extracellular electrophysiology data from .rhd files and adds data to nwbfile.
         """
         dir_ecephys_rhd = Path(self.source_paths['dir_ecephys_rhd']['path'])
-        electrodes_file = Path(self.source_paths['file_electrodes']['path'])
+        if 'file_electrodes' in self.source_paths:
+            electrodes_file = Path(self.source_paths['file_electrodes']['path'])
+        else:
+            electrodes_file = None
 
         def data_gen(source_dir):
             all_files_rhd = list(dir_ecephys_rhd.glob('*.rhd'))
@@ -84,18 +87,52 @@ class Intan2NWB(NWBConverter):
         header = read_header.read_header(fid)
         sampling_rate = header['sample_rate']
 
-        # Electrodes Groups
-        device_name = metadata_ecephys['Device'][0]['name']
-        device = self.nwbfile.devices[device_name]
-        meta_electrode_groups = metadata_ecephys['ElectrodeGroup']
-        for meta in meta_electrode_groups:
-            self.nwbfile.create_electrode_group(
-                name=meta['name'],
-                description=meta['description'],
-                location=meta['location'],
-                device=device
-            )
+        self.create_electrodes_ecephys(
+            all_files_rhd=all_files_rhd,
+            electrodes_file=electrodes_file
+        )
 
+        # Gets electrodes info from first rhd file
+        file_data = load_intan.read_data(filename=all_files_rhd[0])
+        electrodes_info = file_data['amplifier_channels']
+        n_electrodes = len(electrodes_info)
+        electrode_table_region = self.nwbfile.create_electrode_table_region(
+            region=list(np.arange(n_electrodes)),
+            description='no description'
+        )
+
+        # Create iterator
+        data_iter = DataChunkIterator(
+            data=data_gen(source_dir=dir_ecephys_rhd),
+            iter_axis=0,
+            buffer_size=10000,
+            maxshape=(None, n_electrodes)
+        )
+
+        # Electrical Series
+        metadata_ecephys = self.metadata['Ecephys']
+        # Gets electricalseries conversion factor
+        es_conversion_factor = file_data['amplifier_data_conversion_factor']
+        ephys_ts = ElectricalSeries(
+            name=metadata_ecephys['ElectricalSeries'][0]['name'],
+            description=metadata_ecephys['ElectricalSeries'][0]['description'],
+            data=data_iter,
+            electrodes=electrode_table_region,
+            rate=sampling_rate,
+            starting_time=0.0,
+            conversion=es_conversion_factor
+        )
+        self.nwbfile.add_acquisition(ephys_ts)
+
+    def create_electrodes_ecephys(self, all_files_rhd, electrodes_file):
+        """
+        Parameters
+        ----------
+        all_files_rhd: list
+            List of paths to rhd files
+        electrodes_file : str
+            Path to CSV file containing extra electrodes information
+        """
         # Gets electrodes info from first rhd file
         file_data = load_intan.read_data(filename=all_files_rhd[0])
         electrodes_info = file_data['amplifier_channels']
@@ -128,30 +165,3 @@ class Intan2NWB(NWBConverter):
                     filtering='none',
                     group=electrode_group
                 )
-
-        electrode_table_region = self.nwbfile.create_electrode_table_region(
-            region=list(np.arange(n_electrodes)),
-            description='no description'
-        )
-
-        # Create iterator
-        data_iter = DataChunkIterator(
-            data=data_gen(source_dir=dir_ecephys_rhd),
-            iter_axis=0,
-            buffer_size=10000,
-            maxshape=(None, n_electrodes)
-        )
-
-        # Electrical Series
-        # Gets electricalseries conversion factor
-        es_conversion_factor = file_data['amplifier_data_conversion_factor']
-        ephys_ts = ElectricalSeries(
-            name=metadata_ecephys['ElectricalSeries'][0]['name'],
-            description=metadata_ecephys['ElectricalSeries'][0]['description'],
-            data=data_iter,
-            electrodes=electrode_table_region,
-            rate=sampling_rate,
-            starting_time=0.0,
-            conversion=es_conversion_factor
-        )
-        self.nwbfile.add_acquisition(ephys_ts)
