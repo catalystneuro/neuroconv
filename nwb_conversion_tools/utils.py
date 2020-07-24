04917318ca0f31caffa8bb5ca8f1fcfdc97af41f
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-07-24 15:55:16 -0400

    add get_schema_from_hdmf_class

--- nwb_conversion_tools/utils.py
+++ nwb_conversion_tools/utils.py
@@ -1,12 +1,16 @@
 import inspect
+from copy import deepcopy
 
-
-def get_schema_from_method_signature(class_method):
-    input_schema = dict(
+base_schema = dict(
         required=[],
         properties=[],
         type='object',
-        additionalProperties='false')
+        additionalProperties=False)
+
+
+def get_schema_from_method_signature(class_method):
+
+    input_schema = deepcopy(base_schema)
 
     for param in inspect.signature(class_method.__init__).parameters.values():
         if param.name is not 'self':
@@ -16,7 +20,29 @@ def get_schema_from_method_signature(class_method):
             elif param.default is not None:
                 arg_spec.update(default=param.default)
             input_schema['properties'].append(arg_spec)
-        if param.kind == inspect.Parameter.VAR_KEYWORD:
-            input_schema['additionalProperties'] = 'true'
+        input_schema['additionalProperties'] = param.kind == inspect.Parameter.VAR_KEYWORD
 
     return input_schema
+
+
+def get_schema_from_docval(docval):
+
+    schema = deepcopy(base_schema)
+    for docval_arg in docval['args']:
+        if docval_arg['type'] is str or (isinstance(docval_arg['type'], tuple) and str in docval_arg['type']):
+            schema_arg = dict(name=docval_arg['name'], type='string', description=docval_arg['doc'])
+            if 'default' in docval_arg:
+                if docval_arg['default'] is not None:
+                    schema_arg.update(default=docval_arg['default'])
+            else:
+                schema['required'].append(docval_arg['name'])
+            schema['properties'].append(schema_arg)
+
+    if 'allow_extra' in docval:
+        schema['additionalProperties'] = docval['allow_extra']
+
+    return schema
+
+
+def get_schema_from_hdmf_class(hdmf_class):
+    return get_schema_from_docval(hdmf_class.__init__.__docval__)
