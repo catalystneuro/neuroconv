bb1d5accc6578eb6a18b61011f7a5cec7b1ce430
Cody Baker <cbaker9@nd.edu>
2020-08-31 15:08:25 -0400

    fixed filename cases

---
+++ nwb_conversion_tools/neuroscopedatainterface.py
@@ -0,0 +1,12 @@
+"""Authors: Cody Baker and Ben Dichter."""
+from .BaseRecordingExtractorInterface import BaseRecordingExtractorInterface
+from .BaseSortingExtractorInterface import BaseSortingExtractorInterface
+import spikeextractors as se
+
+
+class NeuroscopeRecordingInterface(BaseRecordingExtractorInterface):
+    RX = se.NeuroscopeRecordingExtractor
+
+
+class NeuroscopeSortingInterface(BaseSortingExtractorInterface):
+    SX = se.NeuroscopeMultiSortingExtractor
