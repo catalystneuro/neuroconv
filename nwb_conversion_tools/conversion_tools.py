e26c8c1ec4064068e714fd3d92242c5feb9a090e
Cody Baker <cbaker9@nd.edu>
2020-11-12 12:53:34 -0500

    created placeholder for si object conversion function

---
+++ nwb_conversion_tools/conversion_tools.py
@@ -0,0 +1,8 @@
+"""Authors: Cody Baker."""
+
+def save_si_object(si_object):
+    """Take an arbitrary SpikeInterface object and save to a temporary location for NWB conversion."""
+    # TODO
+    # Cache, dump to pickle (checks for what is/isnt dumbable)
+    # return pickle file location?
+    raise NotImplementedError
