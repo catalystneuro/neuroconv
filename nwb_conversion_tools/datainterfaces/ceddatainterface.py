12f5fa163dac4e23c06e3199ea4409aa6634c825
luiz <luiz_tauffer@hotmail.com>
2020-12-10 18:23:18 +0100

    exclude smrx_ch_inds from source schema

--- nwb_conversion_tools/datainterfaces/ceddatainterface.py
+++ nwb_conversion_tools/datainterfaces/ceddatainterface.py
@@ -2,9 +2,18 @@
 import spikeextractors as se
 
 from ..baserecordingextractorinterface import BaseRecordingExtractorInterface
+from ..json_schema_utils import get_schema_from_method_signature
 
 
 class CEDRecordingInterface(BaseRecordingExtractorInterface):
     """Primary data interface class for converting a CEDRecordingExtractor."""
 
     RX = se.CEDRecordingExtractor
+
+    @classmethod
+    def get_source_schema(cls):
+        """Compile input schema for the RecordingExtractor."""
+        return get_schema_from_method_signature(
+            class_method=cls.RX.__init__,
+            exclude=['smrx_ch_inds']
+        )
