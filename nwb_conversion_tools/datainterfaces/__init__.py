311cbae86389f6ea5e8c55754ca57b31eb6632ee
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2020-12-14 14:47:31 -0500

    updated data interface import structure to be similar to roi (#119)

--- nwb_conversion_tools/datainterfaces/__init__.py
+++ nwb_conversion_tools/datainterfaces/__init__.py
@@ -1,6 +1,6 @@
-from .neuroscopedatainterface import NeuroscopeRecordingInterface, NeuroscopeSortingInterface
+from .neuroscopedatainterface import *
 from .spikeglxdatainterface import SpikeGLXRecordingInterface
-from .sipickledatainterfaces import SIPickleRecordingExtractorInterface, SIPickleSortingExtractorInterface
+from .sipickledatainterfaces import *
 from .intandatainterface import IntanRecordingInterface
 from .ceddatainterface import CEDRecordingInterface
 from .roiextractordatainterface import *
