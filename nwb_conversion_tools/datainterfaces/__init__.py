58d5b9d93f09aaa2035ad40daf0833c5a5e3eeca
Ben Dichter <ben.dichter@gmail.com>
2020-12-15 13:23:20 -0500

    * create list of all interfaces (#120)
    * validate all interface schemas against json schema draft 7

--- nwb_conversion_tools/datainterfaces/__init__.py
+++ nwb_conversion_tools/datainterfaces/__init__.py
@@ -1,6 +1,37 @@
-from .neuroscopedatainterface import *
+from .neuroscopedatainterface import (
+    NeuroscopeRecordingInterface,
+    NeuroscopeLFPInterface,
+    NeuroscopeMultiRecordingTimeInterface,
+    NeuroscopeSortingInterface
+)
 from .spikeglxdatainterface import SpikeGLXRecordingInterface
-from .sipickledatainterfaces import *
+from .sipickledatainterfaces import (
+    SIPickleRecordingExtractorInterface,
+    SIPickleSortingExtractorInterface)
 from .intandatainterface import IntanRecordingInterface
 from .ceddatainterface import CEDRecordingInterface
-from .roiextractordatainterface import *
+from .cellexplorerdatainterface import CellExplorerSortingInterface
+from .roiextractordatainterface import (
+    CaimanSegmentationInterface,
+    CnmfeSegmentationInterface,
+    Suite2pSegmentationInterface,
+    ExtractSegmentationInterface,
+    SimaSegmentationInterface
+)
+
+
+interface_list = [
+    NeuroscopeRecordingInterface,
+    NeuroscopeSortingInterface,
+    SpikeGLXRecordingInterface,
+    SIPickleRecordingExtractorInterface,
+    SIPickleSortingExtractorInterface,
+    IntanRecordingInterface,
+    CellExplorerSortingInterface,
+    CEDRecordingInterface,
+    CaimanSegmentationInterface,
+    CnmfeSegmentationInterface,
+    Suite2pSegmentationInterface,
+    ExtractSegmentationInterface,
+    SimaSegmentationInterface
+]
---
+++ tests/test_interfaces.py
@@ -0,0 +1,14 @@
+from jsonschema import Draft7Validator
+
+from nwb_conversion_tools import interface_list
+
+
+def test_interface_schemas():
+    for data_interface in interface_list:
+        # check validity of source schema
+        schema = data_interface.get_source_schema()
+        Draft7Validator.check_schema(schema)
+
+        # check validity of conversion options schema
+        schema = data_interface.get_conversion_options_schema()
+        Draft7Validator.check_schema(schema)
