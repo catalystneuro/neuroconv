b877459e1eecfd4af264a72abce7b4f910a70a15
Cody Baker <cbaker9@nd.edu>
2021-03-27 14:55:43 -0400

    added tests for check_module

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,24 @@
-from nwb_conversion_tools.conversion_tools import check_regular_timestamps
+from unittest import TestCase
+
+from pynwb.base import ProcessingModule
+
+from nwb_conversion_tools.conversion_tools import check_regular_timestamps, check_module, make_nwbfile_from_metadata
 
 
-def test_check_regular_timestamps():
-    assert check_regular_timestamps([1,2,3])
-    assert not check_regular_timestamps([1,2,4])
+class TestConversionTools(TestCase):
+
+    def test_check_regular_timestamps():
+        assert check_regular_timestamps([1, 2, 3])
+        assert not check_regular_timestamps([1, 2, 4])
+
+    def test_check_module(self):
+        nwbfile = make_nwbfile_from_metadata(metadata=dict())
+
+        name_1 = "test_1"
+        name_2 = "test_2"
+        description_1 = "description_1"
+        description_2 = "description_2"
+        nwbfile.create_processing_module(name=name_1, description=description_1)
+        assert isinstance(check_module(nwbfile=nwbfile, name=name_1, description=description_1), ProcessingModule)
+        assert isinstance(check_module(nwbfile=nwbfile, name=name_2, description=description_1), ProcessingModule)
+        self.assertWarns(check_module(nwbfile=nwbfile, name=name_1, description=description_2))
