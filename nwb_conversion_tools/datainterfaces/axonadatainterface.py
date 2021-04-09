3b1e281b010f0ea782970c2e5d3dc0ce0f53b062
sbuergers <sbuergers@gmail.com>
2021-04-09 15:02:05 -0400

    pep8 changes

--- nwb_conversion_tools/datainterfaces/axonadatainterface.py
+++ nwb_conversion_tools/datainterfaces/axonadatainterface.py
@@ -13,15 +13,15 @@ def parse_generic_header(filename, params):
     Given a binary file with phrases and line breaks, enters the
     first word of a phrase as dictionary key and the following
     string (without linebreaks) as value. Returns the dictionary.
-    
+
     INPUT
     filename (str): .set file path and name.
-    params (list or set): parameter names to search for. 
-    
+    params (list or set): parameter names to search for.
+
     OUTPUT
     header (dict): dictionary with keys being the parameters that
                    were found & values being strings of the data.
-                   
+
     EXAMPLE
     parse_generic_header('myset_file.set', ['experimenter', 'trial_time'])
     """
@@ -31,18 +31,19 @@ def parse_generic_header(filename, params):
         for bin_line in f:
             if b'data_start' in bin_line:
                 break
-            line = bin_line.decode('cp1252').replace('\r\n', '').replace('\r', '').strip()
+            line = bin_line.decode('cp1252').replace('\r\n', '').\
+                replace('\r', '').strip()
             parts = line.split(' ')
             key = parts[0]
             if key in params:
                 header[key] = ' '.join(parts[1:])
-            
+
     return header
 
 
 def read_iso_datetime(set_file):
-    """ 
-    Creates datetime object (y, m, d, h, m, s) from .set file header 
+    """
+    Creates datetime object (y, m, d, h, m, s) from .set file header
     """
     with open(set_file, 'r', encoding='cp1252') as f:
         for line in f:
@@ -51,12 +52,14 @@ def read_iso_datetime(set_file):
             if line.startswith('trial_time'):
                 time_string = line[len('trial_time')+1::].replace('\n', '')
 
-    return datetime.datetime.strptime(date_string + ', ' + time_string, \
-        "%d %b %Y, %H:%M:%S").isoformat()
+    return datetime.datetime.strptime(date_string + ', ' + time_string,
+                                      "%d %b %Y, %H:%M:%S").isoformat()
 
 
 class AxonaRecordingExtractorInterface(BaseRecordingExtractorInterface):
-    """Primary data interface class for converting a AxonaRecordingExtractor."""
+    """
+    Primary data interface class for converting a AxonaRecordingExtractor
+    """
 
     RX = se.AxonaRecordingExtractor
 
@@ -75,19 +78,21 @@ class AxonaRecordingExtractorInterface(BaseRecordingExtractorInterface):
             'additionalProperties': True
         }
         return source_schema
-       
+
     def __init__(self, **source_data):
         super().__init__(**source_data)
-        
+
     def get_metadata(self):
-        """Auto-fill as much of the metadata as possible. Must comply with metadata schema."""        
-                
+        """
+        Auto-fill metadata where possible. Must comply with metadata schema.
+        """
+
         # Extract information for specific parameters from .set file
         # TODO not all are currently used or part of the metadata_schema
         params_of_interest = [
-            'experimenter', 
+            'experimenter',
             'comments',
-            'duration', 
+            'duration',
             'sw_version',
             'tracker_version',
             'stim_version',
@@ -95,23 +100,25 @@ class AxonaRecordingExtractorInterface(BaseRecordingExtractorInterface):
         ]
         set_file = self.source_data['filename']+'.set'
         par = parse_generic_header(set_file, params_of_interest)
-        
+
         # Add available metadata
-        # NOTE that this interface is meant to be used within an NWBconverter, 
+        # NOTE that this interface is meant to be used within an NWBconverter,
         # which contains a much larger metadata_schema. As such a datainterface
         # metadata by itself does not necessarily validate with its own schema!
         metadata = super().get_metadata()
         metadata['NWBFile'] = dict(
             session_start_time=read_iso_datetime(set_file),
             session_description=par['comments'],
-            identifier=''.join(random.choices(string.ascii_uppercase + string.digits, k=12)),
+            identifier=''.join(random.choices(string.ascii_uppercase +
+                                              string.digits, k=12)),
             experimenter=[par['experimenter']]
         )
-        
+
         metadata['Ecephys'] = dict(
             Device=dict(
                     name="Axona",
-                    description="Axona DacqUSB, sw_version={}".format(par['sw_version']),
+                    description="Axona DacqUSB, sw_version={}".format(
+                        par['sw_version']),
                     manufacturer="Axona"
                 ),
             ElectrodeGroup=dict(
@@ -125,8 +132,5 @@ class AxonaRecordingExtractorInterface(BaseRecordingExtractorInterface):
                 description="Raw acquisition traces."
             )
         )
-  
+
         return metadata
-
-# eof
-
