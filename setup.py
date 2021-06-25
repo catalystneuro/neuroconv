1560b3c1104ed9d77995b6d1c6d759634ef636c1
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2021-06-25 15:23:20 -0400

    v0.8.2
    v0.8.2 includes fixes to writing brain area locations for adding electrode columns

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='nwb-conversion-tools',
-    version='0.8.1',
+    version='0.8.2',
     description='Convert data to nwb',
     long_description=long_description,
     long_description_content_type='text/markdown',
