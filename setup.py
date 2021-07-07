20da42b79dd968d3e449d87754b599f0c37228fc
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2021-07-07 11:49:07 -0400

    v0.8.5
    v0.8.5 Includes the added feature of loading metadata from json/yaml files.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='nwb-conversion-tools',
-    version='0.8.4',
+    version='0.8.5',
     description='Convert data to nwb',
     long_description=long_description,
     long_description_content_type='text/markdown',
