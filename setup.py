00d8f5dc7343e5b00b1d6d031be09b3e028647b3
luiz <luiz_tauffer@hotmail.com>
2021-07-12 13:33:08 +0200

    get version from release, update requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import subprocess
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -6,13 +7,25 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-
+# Get the long description from the README file
 with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
+# Get requirements
+with open(path.join(here, 'requirements.txt')) as f:
+    install_requires = f.read().strip().split('\n')
+
+# Get remote version
+remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+assert "." in remote_version
+
 setup(
     name='nwb-conversion-tools',
-    version='0.8.6',
+    version=remote_version,
     description='Convert data to nwb',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,12 +34,10 @@ setup(
     url='https://github.com/catalystneuro/nwb-conversion-tools',
     keywords='nwb',
     packages=find_packages(),
-    package_data={'': ['template_metafile.yml']},
+    package_data={"": ['template_metafile.yml']},
     include_package_data=True,
-    install_requires=[
-        'pynwb', 'tqdm', 'natsort', 'numpy', 'scipy', 'pandas', 'h5py',
-        'pyyaml', 'spikeextractors', 'spikesorters', 'spiketoolkit',
-        'roiextractors', 'jsonschema', 'lxml', 'psutil'],
+    python_requires=">=3.7",
+    install_requires=install_requires,
     entry_points={
         'console_scripts': ['nwb-gui=nwb_conversion_tools.gui.command_line:main'],
     }
