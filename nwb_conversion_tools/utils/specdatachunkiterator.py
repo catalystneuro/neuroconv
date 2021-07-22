8aa0ced8ab5f80a2ffca58bb3dd05845dc9a1c5e
Cody Baker <cbaker9@nd.edu>
2021-07-22 14:13:09 -0400

    added recommended_chunk_shape; simplified block read

--- nwb_conversion_tools/utils/specdatachunkiterator.py
+++ nwb_conversion_tools/utils/specdatachunkiterator.py
@@ -8,28 +8,20 @@ from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
 class SpecDataChunkIterator(AbstractDataChunkIterator):
     """DataChunkIterator that lets the user specify chunk shapes."""
 
-    def __init__(self, data: Iterable, full_shape: tuple, chunk_shape: tuple):
+    def __init__(self, data: Iterable, chunk_shape: tuple):
         self.data = data
-        self.full_shape = full_shape
+        self.full_shape = data.shape
         self.chunk_shape = chunk_shape
         self.__dtype = data.dtype
         self.__curr_timestep = 0
         self.__curr_channel_idx = 0
 
     def __iter__(self):
-        """Return the iterator object"""
+        """Return the iterator object."""
         return self
 
-    def __read_block(self, start_time_idx, stop_time_idx, start_ch_idx, stop_ch_idx):
-        arr = self.data[start_time_idx:stop_time_idx, start_ch_idx:stop_ch_idx]
-        return arr
-
     def __next__(self):
-        """
-        Return the next data chunk or raise a StopIteration exception if all chunks have been retrieved.
-        :returns: DataChunk object with the data and selection of the current chunk
-        :rtype: DataChunk
-        """
+        """Return the next data chunk or raise a StopIteration exception if all chunks have been retrieved."""
         end_timestep = self.__curr_timestep + self.chunk_shape[0]
         if self.__curr_channel_idx >= self.full_shape[1]:
             self.__curr_channel_idx = 0
@@ -44,22 +36,15 @@ class SpecDataChunkIterator(AbstractDataChunkIterator):
             end_timestep = self.full_shape[0]
         if end_channel_idx > self.full_shape[1]:
             end_channel_idx = self.full_shape[1]
-        selection = np.s_[self.__curr_timestep:end_timestep, self.__curr_channel_idx:end_channel_idx]
-        data = self.__read_block(self.__curr_timestep, end_timestep, self.__curr_channel_idx, end_channel_idx)
-        self.__curr_channel_idx = end_channel_idx
 
+        selection = np.s_[self.__curr_timestep:end_timestep, self.__curr_channel_idx:end_channel_idx]
+        data = self.data[selection]
+
+        self.__curr_channel_idx = end_channel_idx
         return DataChunk(data=data, selection=selection)
 
     def recommended_data_shape(self):
-        """
-        Recommend the initial shape for the data array.
-        This is useful in particular to avoid repeated resized of the target array when reading from
-        this data iterator. This should typically be either the final size of the array or the known
-        minimal shape of the array.
-        :return: NumPy-style shape tuple indicating the recommended initial shape for the target array.
-                 This may or may not be the final full shape of the array, i.e., the array is allowed
-                 to grow. This should not be None.
-        """
+        """Recommend the initial shape for the data array."""
         return (self.full_shape[0], self.full_shape[1])
 
     @property
@@ -71,4 +56,4 @@ class SpecDataChunkIterator(AbstractDataChunkIterator):
         return self.full_shape
 
     def recommended_chunk_shape(self):
-        return None
+        return self.chunk_shape
