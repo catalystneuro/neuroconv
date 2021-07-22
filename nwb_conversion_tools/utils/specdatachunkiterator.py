6f57bce4e349654f12e15a2e8199d8f28c2fb1ef
Cody Baker <cbaker9@nd.edu>
2021-07-22 17:37:36 -0400

    changed chunk shape specification to buffer_mb

--- nwb_conversion_tools/utils/specdatachunkiterator.py
+++ nwb_conversion_tools/utils/specdatachunkiterator.py
@@ -1,6 +1,7 @@
 """Authors: Cody Baker, Oliver Ruebel."""
 from typing import Iterable
 import numpy as np
+import psutil
 
 from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
 
@@ -8,13 +9,32 @@ from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
 class SpecDataChunkIterator(AbstractDataChunkIterator):
     """DataChunkIterator that lets the user specify chunk shapes."""
 
-    def __init__(self, data: Iterable, chunk_shape: tuple):
+    def __init__(self, data: Iterable, buffer_mb: int):
+        assert buffer_mb < psutil.virtual_memory().available / 1e6, \
+            f"Not enough memory in system handle buffer_mb of {buffer_mb}!"
         self.data = data
         self.full_shape = data.shape
-        self.chunk_shape = chunk_shape
         self.__dtype = data.dtype
         self.__curr_timestep = 0
         self.__curr_channel_idx = 0
+
+        def get_chunksize_mb(chunk_shape, typesize):
+            return np.product(chunk_shape) * typesize / 1e6
+
+        def set_chunk_shape(chunk_shape, typesize, buffer_mb):
+            n_dim = len(chunk_shape)
+            iter_idx = 0
+            while (get_chunksize_mb(chunk_shape, typesize) > buffer_mb) and np.product(chunk_shape) > 2:
+                dim = iter_idx % n_dim
+                chunk_shape[dim] = np.ceil(chunk_shape[dim] / 2.0)
+                iter_idx += 1
+            return tuple(int(x) for x in chunk_shape)
+
+        self.chunk_shape = set_chunk_shape(
+            chunk_shape=list(self.full_shape),
+            typesize=self.__dtype.itemsize,
+            buffer_mb=buffer_mb
+        )
 
     def __iter__(self):
         """Return the iterator object."""
