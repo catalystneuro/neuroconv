56012d07ac230b7fe540def8d25ade6e6235fc6c
Cody Baker <cbaker9@nd.edu>
2021-07-26 12:08:27 -0400

    debug for channel_ids

--- nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
+++ nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
@@ -11,11 +11,12 @@ class RecordingExtractorDataChunkIterator(GenericDataChunkIterator):
 
     def __init__(self, recording_extractor: RecordingExtractor, chunk_shape: tuple = (), buffer_mb: int = 20):
         self.recording_extractor = recording_extractor
+        self.channel_ids = recording_extractor.get_channel_ids()
         super().__init__(chunk_shape=chunk_shape, buffer_mb=buffer_mb)
 
     def _get_data(self, selection: Tuple[slice]) -> Iterable:
         return self.recording_extractor.get_traces(
-            channel_ids=list(range(selection[1].start, selection[1].stop)),
+            channel_ids=self.channel_ids[selection[1]],
             start_frame=selection[0].start,
             end_frame=selection[0].stop
         ).T
