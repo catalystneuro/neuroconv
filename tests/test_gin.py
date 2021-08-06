40b7c986416dedf51a7281b974a45a9afeb5840b
Cody Baker <cody.c.baker.phd@gmail.com>
2021-08-06 18:17:20 +0000

    Automated changes

--- tests/test_gin.py
+++ tests/test_gin.py
@@ -14,7 +14,7 @@ from nwb_conversion_tools import (
     NeuroscopeRecordingInterface,
     OpenEphysRecordingExtractorInterface,
     CEDRecordingInterface,
-    SpikeGLXRecordingInterface
+    SpikeGLXRecordingInterface,
 )
 
 try:
@@ -28,81 +28,95 @@ except ImportError:
 run_local = False
 
 if HAVE_DATALAD and (sys.platform == "linux" or run_local):
-    class TestNwbConversions(unittest.TestCase):
 
+    class TestNwbConversions(unittest.TestCase):
         def setUp(self):
-            pt = Path.cwd() / 'ephy_testing_data'
+            pt = Path.cwd() / "ephy_testing_data"
             if pt.exists():
                 self.dataset = Dataset(pt)
             else:
-                self.dataset = install('https://gin.g-node.org/NeuralEnsemble/ephy_testing_data')
+                self.dataset = install("https://gin.g-node.org/NeuralEnsemble/ephy_testing_data")
             self.savedir = Path(tempfile.mkdtemp())
 
-        @parameterized.expand([
-            (
-                AxonaRecordingExtractorInterface,
-                "axona",
-                dict(filename=str(Path.cwd() / "ephy_testing_data" / "axona" / "axona_raw.set"))
-            ),
-            (
-                BlackrockRecordingExtractorInterface,
-                "blackrock/blackrock_2_1",
-                dict(
-                    filename=str(Path.cwd() / "ephy_testing_data" / "blackrock" / "blackrock_2_1" / "l101210-001"),
-                    seg_index=0,
-                    nsx_to_load=5
-                )
-            ),
-            (
-                IntanRecordingInterface,
-                "intan",
-                dict(file_path=Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhd_test_1.rhd")
-            ),
-            (
-                IntanRecordingInterface,
-                "intan",
-                dict(file_path=Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhs_test_1.rhs")
-            ),
-            (
-                NeuroscopeRecordingInterface,
-                "neuroscope/test1",
-                dict(file_path=Path.cwd() / "ephy_testing_data" / "neuroscope" / "test1" / "test1.dat")
-            ),
-            (
-                OpenEphysRecordingExtractorInterface,
-                "openephys/OpenEphys_SampleData_1",
-                dict(folder_path=Path.cwd() / "ephy_testing_data" / "openephys" / "OpenEphys_SampleData_1")
-            ),
-            (
-                OpenEphysRecordingExtractorInterface,
-                "openephysbinary/v0.4.4.1_with_video_tracking",
-                dict(folder_path=Path.cwd() / "ephy_testing_data" / "openephysbinary" / "v0.4.4.1_with_video_tracking")
-            ),
-            (
-                OpenEphysRecordingExtractorInterface,
-                "openephysbinary/v0.5.3_two_neuropixels_stream",
-                dict(
-                    folder_path=Path.cwd() / "ephy_testing_data" / "openephysbinary" / "v0.5.3_two_neuropixels_stream"
-                    / "Record_Node_107"
-                )
-            ),
-            (
-                CEDRecordingInterface,
-                "spike2/m365_1sec.smrx",
-                dict(
-                    file_path=Path.cwd() / "ephy_testing_data" / "spike2" / "m365_1sec.smrx",
-                    smrx_channel_ids=range(10)
-                )
-            ),
-            (
-                SpikeGLXRecordingInterface,
-                "spikeglx/Noise4Sam_g0",
-                dict(
-                    file_path=Path.cwd() / "ephy_testing_data" / "spikeglx" / "Noise4Sam_g0" / "Noise4Sam_g0_imec0" /
-                    "Noise4Sam_g0_t0.imec0.ap.bin"
-                )
-            )
-        ])
+        @parameterized.expand(
+            [
+                (
+                    AxonaRecordingExtractorInterface,
+                    "axona",
+                    dict(filename=str(Path.cwd() / "ephy_testing_data" / "axona" / "axona_raw.set")),
+                ),
+                (
+                    BlackrockRecordingExtractorInterface,
+                    "blackrock/blackrock_2_1",
+                    dict(
+                        filename=str(Path.cwd() / "ephy_testing_data" / "blackrock" / "blackrock_2_1" / "l101210-001"),
+                        seg_index=0,
+                        nsx_to_load=5,
+                    ),
+                ),
+                (
+                    IntanRecordingInterface,
+                    "intan",
+                    dict(file_path=Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhd_test_1.rhd"),
+                ),
+                (
+                    IntanRecordingInterface,
+                    "intan",
+                    dict(file_path=Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhs_test_1.rhs"),
+                ),
+                (
+                    NeuroscopeRecordingInterface,
+                    "neuroscope/test1",
+                    dict(file_path=Path.cwd() / "ephy_testing_data" / "neuroscope" / "test1" / "test1.dat"),
+                ),
+                (
+                    OpenEphysRecordingExtractorInterface,
+                    "openephys/OpenEphys_SampleData_1",
+                    dict(folder_path=Path.cwd() / "ephy_testing_data" / "openephys" / "OpenEphys_SampleData_1"),
+                ),
+                (
+                    OpenEphysRecordingExtractorInterface,
+                    "openephysbinary/v0.4.4.1_with_video_tracking",
+                    dict(
+                        folder_path=Path.cwd()
+                        / "ephy_testing_data"
+                        / "openephysbinary"
+                        / "v0.4.4.1_with_video_tracking"
+                    ),
+                ),
+                (
+                    OpenEphysRecordingExtractorInterface,
+                    "openephysbinary/v0.5.3_two_neuropixels_stream",
+                    dict(
+                        folder_path=Path.cwd()
+                        / "ephy_testing_data"
+                        / "openephysbinary"
+                        / "v0.5.3_two_neuropixels_stream"
+                        / "Record_Node_107"
+                    ),
+                ),
+                (
+                    CEDRecordingInterface,
+                    "spike2/m365_1sec.smrx",
+                    dict(
+                        file_path=Path.cwd() / "ephy_testing_data" / "spike2" / "m365_1sec.smrx",
+                        smrx_channel_ids=range(10),
+                    ),
+                ),
+                (
+                    SpikeGLXRecordingInterface,
+                    "spikeglx/Noise4Sam_g0",
+                    dict(
+                        file_path=Path.cwd()
+                        / "ephy_testing_data"
+                        / "spikeglx"
+                        / "Noise4Sam_g0"
+                        / "Noise4Sam_g0_imec0"
+                        / "Noise4Sam_g0_t0.imec0.ap.bin"
+                    ),
+                ),
+            ]
+        )
         def test_convert_recording_extractor_to_nwb(self, recording_interface, dataset_path, interface_kwargs):
             print(f"\n\n\n TESTING {recording_interface.__name__}...")
             dataset_stem = Path(dataset_path).stem
@@ -118,20 +132,22 @@ if HAVE_DATALAD and (sys.platform == "linux" or run_local):
             check_recordings_equal(
                 RX1=converter.data_interface_objects["TestRecording"].recording_extractor,
                 RX2=nwb_recording,
-                check_times=False
+                check_times=False,
             )
 
-        @parameterized.expand([
-            (
-                BlackrockSortingExtractorInterface,
-                "blackrock/blackrock_2_1",
-                dict(
-                    filename=str(Path.cwd() / "ephy_testing_data" / "blackrock" / "blackrock_2_1" / "l101210-001"),
-                    seg_index=0,
-                    nsx_to_load=5
-                 )
-            )
-        ])
+        @parameterized.expand(
+            [
+                (
+                    BlackrockSortingExtractorInterface,
+                    "blackrock/blackrock_2_1",
+                    dict(
+                        filename=str(Path.cwd() / "ephy_testing_data" / "blackrock" / "blackrock_2_1" / "l101210-001"),
+                        seg_index=0,
+                        nsx_to_load=5,
+                    ),
+                )
+            ]
+        )
         def test_convert_sorting_extractor_to_nwb(self, sorting_interface, dataset_path, interface_kwargs):
             print(f"\n\n\n TESTING {sorting_interface.__name__}...")
             dataset_stem = Path(dataset_path).stem
@@ -148,5 +164,6 @@ if HAVE_DATALAD and (sys.platform == "linux" or run_local):
                 SX1=converter.data_interface_objects["TestRecording"].recording_extractor, SX2=nwb_recording
             )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
