37c1cc692e1722f87f844d62f2ef2cdbb5842160
luiz <luiz_tauffer@hotmail.com>
2021-08-09 10:10:05 +0200

    fix for gui

--- nwb_conversion_tools/datainterfaces/ophys/tiff/tiffdatainterface.py
+++ nwb_conversion_tools/datainterfaces/ophys/tiff/tiffdatainterface.py
@@ -1,5 +1,6 @@
 from roiextractors import SbxImagingExtractor, Hdf5ImagingExtractor, TiffImagingExtractor
 
+from ....utils.json_schema import get_schema_from_method_signature
 from ..baseimagingextractorinterface import BaseImagingExtractorInterface
 
 
@@ -7,3 +8,13 @@ class TiffImagingInterface(BaseImagingExtractorInterface):
     """Data Interface for TIffImagingExtractor"""
 
     IX = TiffImagingExtractor
+
+    @classmethod
+    def get_source_schema(cls):
+        source_schema = get_schema_from_method_signature(
+            cls.IX.__init__,
+            exclude=["channel_names"]
+        )
+        source_schema["properties"]["file_path"]["format"] = "file"
+        source_schema["properties"]["file_path"]["description"] = "Path to Tiff file."
+        return source_schema
