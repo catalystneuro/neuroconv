c9572dbf1dec2d158efafe72554bc66b89372d86
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2021-08-23 02:23:31 -0400

    implemented proper class workflow for intan; reverted datatype change in spike interface writer; simplified action for CI cost

--- nwb_conversion_tools/datainterfaces/ecephys/intan/intandatainterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/intan/intandatainterface.py
@@ -24,16 +24,6 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
     def __init__(self, *args, **kwargs):
         assert HAVE_PYINTAN, INSTALL_MESSAGE
         super().__init__(*args, **kwargs)
-
-    def get_metadata_schema(self):
-        metadata_schema = super().get_metadata_schema()
-        metadata_schema["properties"]["Ecephys"]["properties"].update(
-            ElectricalSeries_raw=get_schema_from_hdmf_class(ElectricalSeries)
-        )
-        return metadata_schema
-
-    def get_metadata(self):
-        """Retrieve Ecephys metadata specific to the Intan format."""
         if ".rhd" in Path(self.source_data["file_path"]).suffixes:
             intan_file_metadata = read_rhd(self.source_data["file_path"])[1]
         else:
@@ -53,6 +43,29 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
                 channel_id=channel_id, property_name="group_name", value=f"Group{channel_group}"
             )
 
+        if len(unique_group_names) > 1:
+            for channel_id, group_electrode_number in zip(channel_ids, group_electrode_numbers):
+                self.recording_extractor.set_channel_property(
+                    channel_id=channel_id, property_name="group_electrode_number", value=group_electrode_number
+                )
+
+        custom_names = [channel["custom_channel_name"] for channel in valid_channels]
+        if any(custom_names):
+            for channel_id, custom_name in zip(channel_ids, custom_names):
+                self.recording_extractor.set_channel_property(
+                    channel_id=channel_id, property_name="custom_channel_name", value=custom_name
+                )
+
+    def get_metadata_schema(self):
+        metadata_schema = super().get_metadata_schema()
+        metadata_schema["properties"]["Ecephys"]["properties"].update(
+            ElectricalSeries_raw=get_schema_from_hdmf_class(ElectricalSeries)
+        )
+        return metadata_schema
+
+    def get_metadata(self):
+        channel_ids = self.recording_extractor.get_channel_ids()
+        property_names = self.recording_extractor.get_shared_channel_property_names()
         ecephys_metadata = dict(
             Ecephys=dict(
                 Device=[
@@ -69,7 +82,10 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
                         device="Intan",
                         location="",
                     )
-                    for group_name in unique_group_names
+                    for group_name in set([
+                        self.recording_extractor.get_channel_property(channel_id=channel_id, property_name="group_name")
+                        for channel_id in channel_ids
+                    ])
                 ],
                 Electrodes=[
                     dict(name="group_name", description="The name of the ElectrodeGroup this electrode is a part of.")
@@ -77,24 +93,12 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
                 ElectricalSeries_raw=dict(name="ElectricalSeries_raw", description="Raw acquisition traces."),
             )
         )
-
-        if len(unique_group_names) > 1:
-            for channel_id, group_electrode_number in zip(channel_ids, group_electrode_numbers):
-                self.recording_extractor.set_channel_property(
-                    channel_id=channel_id, property_name="group_electrode_number", value=group_electrode_number
-                )
+        if "group_electrode_number" in property_names:
             ecephys_metadata["Ecephys"]["Electrodes"].append(
                 dict(name="group_electrode_number", description="0-indexed channel within a group.")
             )
-
-        custom_names = [channel["custom_channel_name"] for channel in valid_channels]
-        if any(custom_names):
-            for channel_id, custom_name in zip(channel_ids, custom_names):
-                self.recording_extractor.set_channel_property(
-                    channel_id=channel_id, property_name="custom_channel_name", value=custom_name
-                )
+        if "custom_channel_name" in property_names:
             ecephys_metadata["Ecephys"]["Electrodes"].append(
                 dict(name="custom_channel_name", description="Custom channel name assigned in Intan.")
             )
-
         return ecephys_metadata
--- tests/test_gin.py
+++ tests/test_gin.py
@@ -12,42 +12,55 @@ from nwb_conversion_tools import (
 )
 
 try:
-    from datalad.api import install, Dataset
-    from parameterized import parameterized
+    from datalad.api import install
 
     HAVE_DATALAD = True
 except ImportError:
     HAVE_DATALAD = False
 
-if HAVE_DATALAD and sys.platform == "linux":
+try:
+    from parameterized import parameterized
+
+    HAVE_PARAMETERIZED = True
+except ImportError:
+    HAVE_PARAMETERIZED = False
+
+RUN_LOCAL = False
+LOCAL_PATH = Path("D:/GIN")  # Path to dataset downloaded from https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
+
+if HAVE_PARAMETERIZED and (HAVE_DATALAD and sys.platform == "linux" or RUN_LOCAL):
 
     class TestNwbConversions(unittest.TestCase):
+        dataset = None
+        data_path = LOCAL_PATH
+        savedir = Path(tempfile.mkdtemp())
+
         def setUp(self):
-            pt = Path.cwd() / "ephy_testing_data"
-            if pt.exists():
-                self.dataset = Dataset(pt)
-            else:
+            if RUN_LOCAL:
+                assert self.data_path.exists(), f"The manually specified data path ({self.data_path}) does not exist!"
+            elif HAVE_DATALAD:
                 self.dataset = install("https://gin.g-node.org/NeuralEnsemble/ephy_testing_data")
-            self.savedir = Path(tempfile.mkdtemp())
+                self.data_path = Path.cwd() / "ephy_testing_data"
 
         @parameterized.expand(
             [
                 (
                     IntanRecordingInterface,
                     "intan",
-                    dict(file_path=str(Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhd_test_1.rhd")),
+                    dict(file_path=str(data_path / "intan" / "intan_rhd_test_1.rhd")),
                 ),
                 (
                     IntanRecordingInterface,
                     "intan",
-                    dict(file_path=str(Path.cwd() / "ephy_testing_data" / "intan" / "intan_rhs_test_1.rhs")),
+                    dict(file_path=str(data_path / "intan" / "intan_rhs_test_1.rhs")),
                 ),
             ]
         )
         def test_convert_recording_extractor_to_nwb(self, recording_interface, dataset_path, interface_kwargs):
             print(f"\n\n\n TESTING {recording_interface.__name__}...")
+            if self.dataset is not None:
+                self.dataset.get(dataset_path)
             dataset_stem = Path(dataset_path).stem
-            self.dataset.get(dataset_path)
             nwbfile_path = self.savedir / f"{recording_interface.__name__}_test_{dataset_stem}.nwb"
 
             class TestConverter(NWBConverter):
