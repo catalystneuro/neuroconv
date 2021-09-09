63ff50c6e3db7a8747abee7b6985e0b54a223a4e
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2021-09-09 16:13:09 -0400

    Adding OpenEphys tests (#247)
    * adding openephys GIN; verifying if a local issue occurs on GIN
    
    * Automated changes
    
    * removing binary test
    
    * Automated changes
    
    * switching test files
    
    * Automated changes
    
    * updated to reflect param usage for kwarg names
    
    * Automated changes

--- tests/test_gin.py
+++ tests/test_gin.py
@@ -10,6 +10,7 @@ from nwb_conversion_tools import (
     IntanRecordingInterface,
     NeuralynxRecordingInterface,
     NeuroscopeRecordingInterface,
+    OpenEphysRecordingExtractorInterface,
     SpikeGLXRecordingInterface,
 )
 
@@ -21,17 +22,23 @@ except ImportError:
     HAVE_DATALAD = False
 
 try:
-    from parameterized import parameterized
+    from parameterized import parameterized, param
 
     HAVE_PARAMETERIZED = True
 except ImportError:
     HAVE_PARAMETERIZED = False
 
 RUN_LOCAL = True
-LOCAL_PATH = Path("D:/GIN")  # Path to dataset downloaded from https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
+LOCAL_PATH = Path("E:/GIN")  # Path to dataset downloaded from https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
 
 
 if HAVE_PARAMETERIZED and (HAVE_DATALAD and sys.platform == "linux" or RUN_LOCAL):
+
+    def custom_name_func(testcase_func, param_num, param):
+        return (
+            f"{testcase_func.__name__}_{param_num}_"
+            f"{parameterized.to_safe_name(param.kwargs['recording_interface'].__name__)}"
+        )
 
     class TestNwbConversions(unittest.TestCase):
         dataset = None
@@ -41,6 +48,41 @@ if HAVE_PARAMETERIZED and (HAVE_DATALAD and sys.platform == "linux" or RUN_LOCAL
             data_path = LOCAL_PATH
         else:
             data_path = Path.cwd() / "ephy_testing_data"
+
+        parameterized_expand_list = [
+            param(
+                recording_interface=NeuralynxRecordingInterface,
+                dataset_path="neuralynx/Cheetah_v5.7.4/original_data",
+                interface_kwargs=dict(folder_path=str(data_path / "neuralynx" / "Cheetah_v5.7.4" / "original_data")),
+            ),
+            param(
+                recording_interface=NeuroscopeRecordingInterface,
+                dataset_path="neuroscope/test1",
+                interface_kwargs=dict(file_path=str(data_path / "neuroscope" / "test1" / "test1.dat")),
+            ),
+            param(
+                recording_interface=OpenEphysRecordingExtractorInterface,
+                dataset_path="openephysbinary/v0.4.4.1_with_video_tracking",
+                interface_kwargs=dict(folder_path=str(data_path / "openephysbinary" / "v0.4.4.1_with_video_tracking")),
+            ),
+        ]
+        for suffix in ["rhd", "rhs"]:
+            parameterized_expand_list.append(
+                param(
+                    recording_interface=IntanRecordingInterface,
+                    dataset_path="intan",
+                    interface_kwargs=dict(file_path=str(data_path / "intan" / f"intan_{suffix}_test_1.{suffix}")),
+                )
+            )
+        for suffix in ["ap", "lf"]:
+            sub_path = Path("spikeglx") / "Noise4Sam_g0" / "Noise4Sam_g0_imec0"
+            parameterized_expand_list.append(
+                param(
+                    recording_interface=SpikeGLXRecordingInterface,
+                    dataset_path=sub_path,
+                    interface_kwargs=dict(file_path=str(data_path / sub_path / f"Noise4Sam_g0_t0.imec0.{suffix}.bin")),
+                )
+            )
 
         def setUp(self):
             data_exists = self.data_path.exists()
@@ -55,56 +97,7 @@ if HAVE_PARAMETERIZED and (HAVE_DATALAD and sys.platform == "linux" or RUN_LOCAL
             elif not data_exists:
                 self.dataset = install("https://gin.g-node.org/NeuralEnsemble/ephy_testing_data")
 
-        @parameterized.expand(
-            [
-                (
-                    IntanRecordingInterface,
-                    "intan",
-                    dict(file_path=str(data_path / "intan" / "intan_rhd_test_1.rhd")),
-                ),
-                (
-                    IntanRecordingInterface,
-                    "intan",
-                    dict(file_path=str(data_path / "intan" / "intan_rhs_test_1.rhs")),
-                ),
-                (
-                    NeuralynxRecordingInterface,
-                    "neuralynx/Cheetah_v5.7.4/original_data",
-                    dict(folder_path=str(data_path / "neuralynx" / "Cheetah_v5.7.4" / "original_data")),
-                ),
-                (
-                    NeuroscopeRecordingInterface,
-                    "neuroscope/test1",
-                    dict(file_path=str(data_path / "neuroscope" / "test1" / "test1.dat")),
-                ),
-                (
-                    SpikeGLXRecordingInterface,
-                    "spikeglx/Noise4Sam_g0/Noise4Sam_g0_imec0",
-                    dict(
-                        file_path=str(
-                            data_path
-                            / "spikeglx"
-                            / "Noise4Sam_g0"
-                            / "Noise4Sam_g0_imec0"
-                            / "Noise4Sam_g0_t0.imec0.ap.bin"
-                        )
-                    ),
-                ),
-                (
-                    SpikeGLXRecordingInterface,
-                    "spikeglx/Noise4Sam_g0/Noise4Sam_g0_imec0",
-                    dict(
-                        file_path=str(
-                            data_path
-                            / "spikeglx"
-                            / "Noise4Sam_g0"
-                            / "Noise4Sam_g0_imec0"
-                            / "Noise4Sam_g0_t0.imec0.lf.bin"
-                        )
-                    ),
-                ),
-            ]
-        )
+        @parameterized.expand(input=parameterized_expand_list, name_func=custom_name_func)
         def test_convert_recording_extractor_to_nwb(self, recording_interface, dataset_path, interface_kwargs):
             print(f"\n\n\n TESTING {recording_interface.__name__}...")
             if HAVE_DATALAD:
