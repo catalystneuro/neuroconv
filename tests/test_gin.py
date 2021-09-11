fa3efd45b2ce5c2ddbaef18b5d405a5a308a06f3
Saksham Sharda <saksham20.sharda@gmail.com>
2021-09-11 20:20:38 +0530

    added different ns5 file fortests from GIN

--- tests/test_gin.py
+++ tests/test_gin.py
@@ -149,8 +149,8 @@ if HAVE_PARAMETERIZED and (HAVE_DATALAD and sys.platform == "linux" or RUN_LOCAL
                 ),
                 (
                     BlackrockRecordingExtractorInterface,
-                    "blackrock/blackrock_2_1",
-                    dict(filename=str(data_path / "blackrock" / "blackrock_2_1" / "l101210-001.ns5"), nsx_to_load=5),
+                    "blackrock",
+                    dict(filename=str(data_path / "blackrock" / "FileSpec2.3001.ns5"), nsx_to_load=5),
                 ),
             ]
         )
