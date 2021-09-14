c74c800fb1b4003fe213fa15cf4e9ce636feb744
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2021-09-14 12:39:26 -0400

    generalized get_source_schema() across all interfaces

--- nwb_conversion_tools/datainterfaces/ecephys/baserecordingextractorinterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/baserecordingextractorinterface.py
@@ -23,11 +23,6 @@ class BaseRecordingExtractorInterface(BaseDataInterface, ABC):
 
     RX = None
 
-    @classmethod
-    def get_source_schema(cls):
-        """Compile input schema for the RecordingExtractor."""
-        return get_schema_from_method_signature(cls.__init__)
-
     def __init__(self, **source_data):
         super().__init__(**source_data)
         self.recording_extractor = self.RX(**source_data)
