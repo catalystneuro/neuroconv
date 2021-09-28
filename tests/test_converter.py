30ac86945b12c076b1b97d03974e99356ee95a85
Cody Baker <cbaker9@nd.edu>
2021-09-28 08:55:43 -0400

    altered debug

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -21,8 +21,6 @@ def test_converter():
         nwbfile_path = str(test_dir / "extension_test.nwb")
 
         class NdxEventsInterface(BaseDataInterface):
-            def __init__(self):
-                super().__init__()
 
             def run_conversion(self, nwbfile: NWBFile, metadata: dict):
                 events = LabeledEvents(
