e09915f213286ce36df7cafdee78432476be3e31
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2022-01-03 04:09:19 +0000

    added docstrings for RecordingExtractorDataChunkIterator

--- nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
+++ nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
@@ -17,6 +17,34 @@ class RecordingExtractorDataChunkIterator(GenericDataChunkIterator):
         chunk_mb: float = None,
         chunk_shape: tuple = None,
     ):
+        """
+        Initialize an Iterable object which returns DataChunks with data and their selections on each iteration.
+
+        Parameters
+        ----------
+        recording : RecordingExtractor
+            The RecordingExtractor object (from legacy spikeinterface/spikeextractors)
+            which handles the API for data transfer.
+        buffer_gb : float, optional
+            The upper bound on size in gigabytes (GB) of each selection from the iteration.
+            The buffer_shape will be set implicitly by this argument.
+            The default is 1GB.
+        buffer_shape : tuple, optional
+            If the user desires further control over the shaping of the buffer over all the axes,
+            they may set this explicitly as a tuple of the same length as the data shape indicating the desired shape
+            of each selection.
+            The default is None.
+        chunk_mb : float, optional
+            The upper bound on size in megabytes (MB) of each internally set chunk for the HDF5 dataset.
+            The chunk_shape will be set implicitly by this argument.
+            The default is 1MB, which is highly recommended by the HDF5 group. For more details, see
+            https://support.hdfgroup.org/HDF5/doc/TechNotes/TechNote-HDF5-ImprovingIOPerformanceCompressedDatasets.pdf
+        chunk_shape : tuple, optional
+            If the user desires further control over the shaping of the buffer over all the axes,
+            they may set this explicitly as a tuple of the same length as the data shape indicating the desired shape
+            of each selection.
+            The default is None.
+        """
         self.recording = recording
         self.channel_ids = recording.get_channel_ids()
         super().__init__(buffer_gb=buffer_gb, buffer_shape=buffer_shape, chunk_mb=chunk_mb, chunk_shape=chunk_shape)
