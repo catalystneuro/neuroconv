5c098d2db6d586918fab6e606bcd1ba2f60622d1
Cody Baker <cbaker9@nd.edu>
2022-01-03 12:32:31 -0500

    PR suggestions

--- nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
+++ nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
@@ -28,21 +28,24 @@ class RecordingExtractorDataChunkIterator(GenericDataChunkIterator):
         buffer_gb : float, optional
             The upper bound on size in gigabytes (GB) of each selection from the iteration.
             The buffer_shape will be set implicitly by this argument.
+            Cannot be set if `buffer_shape` is also specified.
             The default is 1GB.
         buffer_shape : tuple, optional
-            If the user desires further control over the shaping of the buffer over all the axes,
-            they may set this explicitly as a tuple of the same length as the data shape indicating the desired shape
-            of each selection.
+            If the user desires further control over the shaping of the buffer over all the axes, they may set this
+            explicitly as a tuple of the same length as the data shape indicating the desired shape of each selection.
+            Cannot be set if `buffer_gb` is also specified.
             The default is None.
         chunk_mb : float, optional
             The upper bound on size in megabytes (MB) of each internally set chunk for the HDF5 dataset.
             The chunk_shape will be set implicitly by this argument.
+            Cannot be set if `chunk_shape` is also specified.
             The default is 1MB, which is highly recommended by the HDF5 group. For more details, see
             https://support.hdfgroup.org/HDF5/doc/TechNotes/TechNote-HDF5-ImprovingIOPerformanceCompressedDatasets.pdf
         chunk_shape : tuple, optional
             If the user desires further control over the shaping of the buffer over all the axes,
             they may set this explicitly as a tuple of the same length as the data shape indicating the desired shape
             of each selection.
+            Cannot be set if `chunk_mb` is also specified.
             The default is None.
         """
         self.recording = recording
