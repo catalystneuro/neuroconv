8dfe26baa1beda36fc7fe77393b0938146a136e8
Cody Baker <cbaker9@nd.edu>
2022-01-03 12:39:23 -0500

    reduced text

--- nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
+++ nwb_conversion_tools/utils/recordingextractordatachunkiterator.py
@@ -23,28 +23,25 @@ class RecordingExtractorDataChunkIterator(GenericDataChunkIterator):
         Parameters
         ----------
         recording : RecordingExtractor
-            The RecordingExtractor object (from legacy spikeinterface/spikeextractors)
-            which handles the API for data transfer.
+            The RecordingExtractor object (from legacy spikeinterface/spikeextractors) which handles the data access.
         buffer_gb : float, optional
             The upper bound on size in gigabytes (GB) of each selection from the iteration.
             The buffer_shape will be set implicitly by this argument.
             Cannot be set if `buffer_shape` is also specified.
             The default is 1GB.
         buffer_shape : tuple, optional
-            If the user desires further control over the shaping of the buffer over all the axes, they may set this
-            explicitly as a tuple of the same length as the data shape indicating the desired shape of each selection.
+            Manual specification of buffer shape to return on each iteration.
+            Must be a multiple of chunk_shape along each axis.
             Cannot be set if `buffer_gb` is also specified.
             The default is None.
         chunk_mb : float, optional
-            The upper bound on size in megabytes (MB) of each internally set chunk for the HDF5 dataset.
+            The upper bound on size in megabytes (MB) of the internal chunk for the HDF5 dataset.
             The chunk_shape will be set implicitly by this argument.
             Cannot be set if `chunk_shape` is also specified.
             The default is 1MB, which is highly recommended by the HDF5 group. For more details, see
             https://support.hdfgroup.org/HDF5/doc/TechNotes/TechNote-HDF5-ImprovingIOPerformanceCompressedDatasets.pdf
         chunk_shape : tuple, optional
-            If the user desires further control over the shaping of the buffer over all the axes,
-            they may set this explicitly as a tuple of the same length as the data shape indicating the desired shape
-            of each selection.
+            Manual specification of the internal chunk shape for the HDF5 dataset.
             Cannot be set if `chunk_mb` is also specified.
             The default is None.
         """
