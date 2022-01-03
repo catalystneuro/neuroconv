efe9fd3f868a28a81ea862ed9ffcab5f90f58495
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2022-01-03 03:43:45 +0000

    restoring PR

--- tests/test_on_data/test_YAML_conversion_specification.py
+++ tests/test_on_data/test_YAML_conversion_specification.py
@@ -4,10 +4,13 @@ from pathlib import Path
 from tempfile import mkdtemp
 from shutil import rmtree
 from jsonschema import validate, RefResolver
+from datetime import datetime
 
 import pytest
+from pynwb import NWBHDF5IO
 
 from nwb_conversion_tools.utils.json_schema import load_dict_from_file
+from nwb_conversion_tools.utils.conversion_tools import run_conversion_from_yaml
 
 # Path to dataset downloaded from https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
 #   ecephys: https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
@@ -46,6 +49,42 @@ class TestYAMLConversionSpecification(unittest.TestCase):
             schema=load_dict_from_file(file_path=schema_folder / "yaml_conversion_specification_schema.json"),
             resolver=RefResolver(base_uri="file://" + str(schema_folder) + "/", referrer=specification_schema),
         )
+
+        def test_run_conversion_from_yaml(self):
+            path_to_test_gin_file = Path(__file__)
+            yaml_file_path = path_to_test_gin_file.parent / "GIN_conversion_specification.yml"
+            run_conversion_from_yaml(
+                specification_file_path=yaml_file_path,
+                data_folder=DATA_PATH,
+                output_folder=self.test_folder,
+                overwrite=True,
+            )
+
+            with NWBHDF5IO(path=self.test_folder / "example_converter_spec_1.nwb", mode="r") as io:
+                nwbfile = io.read()
+                assert nwbfile.session_description == "Subject navigating a Y-shaped maze."
+                assert nwbfile.lab == "My Lab"
+                assert nwbfile.institution == "My Institution"
+                assert nwbfile.session_start_time == datetime.fromisoformat("2020-10-09T21:19:09+00:00")
+                assert nwbfile.subject.subject_id == "1"
+                assert "ElectricalSeries" in nwbfile.acquisition
+            with NWBHDF5IO(path=self.test_folder / "example_converter_spec_2.nwb", mode="r") as io:
+                nwbfile = io.read()
+                assert nwbfile.session_description == "Subject navigating a Y-shaped maze."
+                assert nwbfile.lab == "My Lab"
+                assert nwbfile.institution == "My Institution"
+                assert nwbfile.session_start_time == datetime.fromisoformat("2020-10-10T21:19:09+00:00")
+                assert nwbfile.subject.subject_id == "002"
+                assert "ElectricalSeries" in nwbfile.acquisition
+            with NWBHDF5IO(path=self.test_folder / "example_converter_spec_3.nwb", mode="r") as io:
+                nwbfile = io.read()
+                assert nwbfile.session_description == "no description"
+                assert nwbfile.lab == "My Lab"
+                assert nwbfile.institution == "My Institution"
+                assert nwbfile.session_start_time == datetime.fromisoformat("2020-10-11T21:19:09+00:00")
+                assert nwbfile.subject.subject_id == "Subject Name"
+                assert "ElectricalSeries" in nwbfile.acquisition
+                assert "spike_times" in nwbfile.units
 
 
 if __name__ == "__main__":
