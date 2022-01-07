be10a390ac74c79c494f28e6dc2f627faaff749e
h-mayorquin <h.mayorquin@gmail.com>
2022-01-07 12:07:10 +0100

    added comment for github discussion

--- nwb_conversion_tools/utils/spike_interface.py
+++ nwb_conversion_tools/utils/spike_interface.py
@@ -649,6 +649,8 @@ def add_electrical_series(
     else:
         raise NotImplementedError(f"iterator_type ({iterator_type}) should be either 'v1' or 'v2' (recommended)!")
 
+    # Not sure if recovering the gains from the parent recording should be done here AFTER the data is extracted.
+    
     eseries_kwargs.update(data=H5DataIO(data=ephys_data, compression=compression, compression_opts=compression_opts))
     if not use_times:
         eseries_kwargs.update(
