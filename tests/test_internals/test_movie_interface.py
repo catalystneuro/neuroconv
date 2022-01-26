f89ceaf9fe1c446f9f69a7ff1990289329d6139a
Saksham Sharda <saksham20.sharda@gmail.com>
2022-01-26 16:58:38 +0530

    using unittests

--- tests/test_internals/test_movie_interface.py
+++ tests/test_internals/test_movie_interface.py
@@ -1,134 +1,122 @@
-import shutil
-
+import unittest
+import tempfile
 import numpy as np
-import pytest
 from pynwb import NWBHDF5IO
-
+import os
 from nwb_conversion_tools import NWBConverter, MovieInterface
 
 try:
     import cv2
+    skip_test = False
 except ImportError:
-    pytestmark = pytest.mark.skip(f"cv2 not installed, skipping test module {__name__} ")
+    skip_test = True
 
 
-@pytest.fixture(scope="module")
-def base_path(tmp_path_factory):
-    test_folder = tmp_path_factory.mktemp("movie_tests")
-    yield test_folder
-    print(f"removing {test_folder}")
-    shutil.rmtree(test_folder)
+@unittest.skipIf(skip_test, "cv2 not installed")
+class TestMovieInterface(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.test_dir = tempfile.mkdtemp()
+        self.movie_files = self.create_movies()
+        self.nwb_converter = self.create_movie_converter()
+        self.nwbfile_path = os.path.join(self.test_dir, "movie_test.nwb")
 
-@pytest.fixture
-def create_movies(base_path):
-    movie_file1 = base_path / "test1.avi"
-    movie_file2 = base_path / "test2.avi"
-    if movie_file1.exists():
-        movie_file1.unlink()
-    if movie_file2.exists():
-        movie_file2.unlink()
-    (nf, nx, ny) = (50, 640, 480)
-    writer1 = cv2.VideoWriter(
-        filename=str(movie_file1),
-        apiPreference=None,
-        fourcc=cv2.VideoWriter_fourcc("M", "J", "P", "G"),
-        fps=25,
-        frameSize=(ny, nx),
-        params=None,
-    )
-    writer2 = cv2.VideoWriter(
-        filename=str(movie_file2),
-        apiPreference=None,
-        fourcc=cv2.VideoWriter_fourcc("M", "J", "P", "G"),
-        fps=25,
-        frameSize=(ny, nx),
-        params=None,
-    )
-    for k in range(nf):
-        writer1.write(np.random.randint(0, 255, (nx, ny, 3)).astype("uint8"))
-        writer2.write(np.random.randint(0, 255, (nx, ny, 3)).astype("uint8"))
-    writer1.release()
-    writer2.release()
-    return [str(movie_file1), str(movie_file2)]
-
-
-@pytest.fixture
-def movie_converter(create_movies):
-    class MovieTestNWBConverter(NWBConverter):
-        data_interface_classes = dict(Movie=MovieInterface)
-
-    source_data = dict(Movie=dict(file_paths=create_movies))
-    converter = MovieTestNWBConverter(source_data)
-    yield converter
-    del converter
-
-
-@pytest.fixture(scope="module")
-def nwbfile_path(base_path):
-    return str(base_path / "test.nwb")
-
-
-def test_movie_starting_times(movie_converter, nwbfile_path, create_movies):
-    starting_times = [np.float(np.random.randint(200)) for i in range(len(create_movies))]
-    conversion_opts = dict(Movie=dict(starting_times=starting_times, external_mode=False))
-    movie_converter.run_conversion(nwbfile_path=nwbfile_path, overwrite=True, conversion_options=conversion_opts)
-    with NWBHDF5IO(path=nwbfile_path, mode="r") as io:
-        nwbfile = io.read()
-        mod = nwbfile.acquisition
-        metadata = movie_converter.get_metadata()
-        for no in range(len(metadata["Behavior"]["Movies"])):
-            movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
-            assert movie_interface_name in mod
-            assert starting_times[no] == mod[movie_interface_name].starting_time
-
-
-def test_movie_custom_module(movie_converter, nwbfile_path, create_movies):
-    starting_times = [np.float(np.random.randint(200)) for i in range(len(create_movies))]
-    module_name = "TestModule"
-    module_description = "This is a test module."
-    conversion_opts = dict(
-        Movie=dict(
-            starting_times=starting_times,
-            external_mode=False,
-            module_name=module_name,
-            module_description=module_description,
+    def create_movies(self):
+        movie_file1 = os.path.join(self.test_dir, "test1.avi")
+        movie_file2 = os.path.join(self.test_dir, "test2.avi")
+        (nf, nx, ny) = (10, 640, 480)
+        writer1 = cv2.VideoWriter(
+            filename=movie_file1,
+            apiPreference=None,
+            fourcc=cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+            fps=25,
+            frameSize=(ny, nx),
+            params=None,
         )
-    )
-    movie_converter.run_conversion(nwbfile_path=nwbfile_path, overwrite=True, conversion_options=conversion_opts)
-    with NWBHDF5IO(path=nwbfile_path, mode="r") as io:
-        nwbfile = io.read()
-        assert module_name in nwbfile.processing
-        assert module_description == nwbfile.processing[module_name].description
+        writer2 = cv2.VideoWriter(
+            filename=movie_file2,
+            apiPreference=None,
+            fourcc=cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+            fps=25,
+            frameSize=(ny, nx),
+            params=None,
+        )
 
+        for k in range(nf):
+            writer1.write(np.random.randint(0, 255, (nx, ny, 3)).astype("uint8"))
+            writer2.write(np.random.randint(0, 255, (nx, ny, 3)).astype("uint8"))
+        writer1.release()
+        writer2.release()
+        return [movie_file1, movie_file2]
 
-@pytest.mark.parametrize("chunk_data", [True, False])
-def test_movie_chunking(chunk_data, movie_converter, nwbfile_path, create_movies):
-    starting_times = [np.float(np.random.randint(200)) for i in range(len(create_movies))]
-    conv_ops = dict(
-        Movie=dict(external_mode=False, stub_test=True, starting_times=starting_times, chunk_data=chunk_data)
-    )
-    movie_converter.run_conversion(nwbfile_path=nwbfile_path, overwrite=True, conversion_options=conv_ops)
-    with NWBHDF5IO(path=nwbfile_path, mode="r") as io:
-        nwbfile = io.read()
-        mod = nwbfile.acquisition
-        metadata = movie_converter.get_metadata()
-        for no in range(len(metadata["Behavior"]["Movies"])):
-            movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
-            if chunk_data:
+    def create_movie_converter(self):
+        class MovieTestNWBConverter(NWBConverter):
+            data_interface_classes = dict(Movie=MovieInterface)
+
+        source_data = dict(Movie=dict(file_paths=self.movie_files))
+        return MovieTestNWBConverter(source_data)
+
+    def test_movie_starting_times(self):
+        starting_times = [np.float(np.random.randint(200)) for i in range(len(self.movie_files))]
+        conversion_opts = dict(Movie=dict(starting_times=starting_times, external_mode=False))
+        self.nwb_converter.run_conversion(nwbfile_path=self.nwbfile_path,
+                                          overwrite=True,
+                                          conversion_options=conversion_opts)
+        with NWBHDF5IO(path=self.nwbfile_path, mode="r") as io:
+            nwbfile = io.read()
+            mod = nwbfile.acquisition
+            metadata = self.nwb_converter.get_metadata()
+            for no in range(len(metadata["Behavior"]["Movies"])):
+                movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
+                assert movie_interface_name in mod
+                assert starting_times[no] == mod[movie_interface_name].starting_time
+
+    def test_movie_custom_module(self):
+        starting_times = [np.float(np.random.randint(200)) for i in range(len(self.movie_files))]
+        module_name = "TestModule"
+        module_description = "This is a test module."
+        conversion_opts = dict(
+            Movie=dict(
+                starting_times=starting_times,
+                external_mode=False,
+                module_name=module_name,
+                module_description=module_description,
+            )
+        )
+        self.nwb_converter.run_conversion(nwbfile_path=self.nwbfile_path,
+                                          overwrite=True,
+                                          conversion_options=conversion_opts)
+        with NWBHDF5IO(path=self.nwbfile_path, mode="r") as io:
+            nwbfile = io.read()
+            assert module_name in nwbfile.processing
+            assert module_description == nwbfile.processing[module_name].description
+
+    def test_movie_chunking(self):
+        starting_times = [np.float(np.random.randint(200)) for i in range(len(self.movie_files))]
+        conv_ops = dict(
+            Movie=dict(external_mode=False, stub_test=True, starting_times=starting_times, chunk_data=False)
+        )
+        self.nwb_converter.run_conversion(nwbfile_path=self.nwbfile_path,
+                                          overwrite=True,
+                                          conversion_options=conv_ops)
+
+        with NWBHDF5IO(path=self.nwbfile_path, mode="r") as io:
+            nwbfile = io.read()
+            mod = nwbfile.acquisition
+            metadata = self.nwb_converter.get_metadata()
+            for no in range(len(metadata["Behavior"]["Movies"])):
+                movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
                 assert mod[movie_interface_name].data.chunks is not None  # TODO retrive storage_layout of hdf5 dataset
-            else:
-                assert mod[movie_interface_name].data.chunks is not None
 
-
-def test_movie_external_mode(movie_converter, nwbfile_path, create_movies):
-    starting_times = [np.float(np.random.randint(200)) for i in range(len(create_movies))]
-    conversion_opts = dict(Movie=dict(starting_times=starting_times, external_mode=True))
-    movie_converter.run_conversion(nwbfile_path=nwbfile_path, overwrite=True, conversion_options=conversion_opts)
-    with NWBHDF5IO(path=nwbfile_path, mode="r") as io:
-        nwbfile = io.read()
-        mod = nwbfile.acquisition
-        metadata = movie_converter.get_metadata()
-        for no in range(len(metadata["Behavior"]["Movies"])):
-            movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
-            assert mod[movie_interface_name].external_file[0] == create_movies[no]
+    def test_movie_external_mode(self):
+        starting_times = [np.float(np.random.randint(200)) for i in range(len(self.movie_files))]
+        conversion_opts = dict(Movie=dict(starting_times=starting_times, external_mode=True))
+        self.nwb_converter.run_conversion(nwbfile_path=self.nwbfile_path, overwrite=True,
+                                          conversion_options=conversion_opts)
+        with NWBHDF5IO(path=self.nwbfile_path, mode="r") as io:
+            nwbfile = io.read()
+            mod = nwbfile.acquisition
+            metadata = self.nwb_converter.get_metadata()
+            for no in range(len(metadata["Behavior"]["Movies"])):
+                movie_interface_name = metadata["Behavior"]["Movies"][no]["name"]
+                assert mod[movie_interface_name].external_file[0] == self.movie_files[no]
