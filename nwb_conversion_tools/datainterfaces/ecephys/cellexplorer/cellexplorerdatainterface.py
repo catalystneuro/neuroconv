88c7360125adad68313423681c21e13335e887ae
h-mayorquin <h.mayorquin@gmail.com>
2022-02-07 09:12:23 +0100

    modified the interface to pass the test

--- nwb_conversion_tools/datainterfaces/ecephys/cellexplorer/cellexplorerdatainterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/cellexplorer/cellexplorerdatainterface.py
@@ -2,11 +2,18 @@
 from pathlib import Path
 
 import spikeextractors as se
-from scipy.io import loadmat
 import numpy as np
 
 from ..basesortingextractorinterface import BaseSortingExtractorInterface
 from ....utils.json_schema import FilePathType
+
+try:
+    import scipy.io 
+    import hdf5storage
+    HAVE_SCIPY_AND_HDF5STORAGE = True
+except ImportError:
+    HAVE_SCIPY_AND_HDF5STORAGE = False
+
 
 
 class CellExplorerSortingInterface(BaseSortingExtractorInterface):
@@ -17,28 +24,57 @@ class CellExplorerSortingInterface(BaseSortingExtractorInterface):
     def __init__(self, file_path: FilePathType):
         super().__init__(spikes_matfile_path=file_path)
         self.source_data = dict(file_path=file_path)
-
+        spikes_matfile_path = Path(file_path)
+        
         session_path = Path(file_path).parent
         session_id = session_path.stem
-        spikes_cellinfo_file_path = session_path / f"{session_id}.spikes.cellinfo.mat"
-        if spikes_cellinfo_file_path.is_file():
-            cell_info = loadmat(spikes_cellinfo_file_path).get("spikes", np.empty(0))
-            cell_info_fields = cell_info.dtype.names
+        
+        assert (
+            spikes_matfile_path.is_file()
+        ), f"The spikes_matfile_path ({spikes_matfile_path}) must exist!"
+        
+        
+        try:
+            spikes_mat = scipy.io.loadmat(file_name=str(spikes_matfile_path))
+            self.read_spikes_info_with_scipy = True
+        except NotImplementedError: 
+            spikes_mat = hdf5storage.loadmat(file_name=str(spikes_matfile_path))
+            self.read_spikes_info_with_scipy = False
+        
+        # Logic for scipy
+        if self.read_spikes_info_with_scipy:
+            cell_info = spikes_mat.get("spikes", np.empty(0))
+            self.cell_info_fields = cell_info.dtype.names
             unit_ids = self.sorting_extractor.get_unit_ids()
-            if "cluID" in cell_info_fields:
+            if "cluID" in self.cell_info_fields:
                 for unit_id, value in zip(unit_ids, [int(x) for x in cell_info["cluID"][0][0][0]]):
                     self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="clu_id", value=value)
-            if "shankID" in cell_info_fields:
+            if "shankID" in self.cell_info_fields:
                 for unit_id, value in zip(unit_ids, [f"Group{x}" for x in cell_info["shankID"][0][0][0]]):
                     self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="group_id", value=value)
-            if "region" in cell_info_fields:
+            if "region" in self.cell_info_fields:
                 for unit_id, value in zip(unit_ids, [str(x[0]) for x in cell_info["region"][0][0][0]]):
                     self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="location", value=value)
-
+        else:
+            # Logic for hdf5
+            cell_info = spikes_mat.get("spikes", np.empty(0))
+            self.cell_info_fields = cell_info.dtype.names
+            unit_ids = self.sorting_extractor.get_unit_ids()
+            if "cluID" in self.cell_info_fields:
+                for unit_id, value in zip(unit_ids, [int(x) for x in cell_info["cluID"][0][0]]):
+                    self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="clu_id", value=value)
+            if "shankID" in self.cell_info_fields:
+                for unit_id, value in zip(unit_ids, [f"Group{x}" for x in cell_info["shankID"][0][0]]):
+                    self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="group_id", value=value)
+            if "region" in self.cell_info_fields:
+                for unit_id, value in zip(unit_ids, [str(x[0]) for x in cell_info["region"][0]][0]):
+                    self.sorting_extractor.set_unit_property(unit_id=unit_id, property_name="location", value=value)
+        
+        # CellClass file 
         celltype_mapping = {"pE": "excitatory", "pI": "inhibitory", "[]": "unclassified"}
         celltype_file_path = session_path / f"{session_id}.CellClass.cellinfo.mat"
         if celltype_file_path.is_file():
-            celltype_info = loadmat(celltype_file_path).get("CellClass", np.empty(0))
+            celltype_info = scipy.io.loadmat(celltype_file_path).get("CellClass", np.empty(0))
             if "label" in celltype_info.dtype.names:
                 for unit_id, value in zip(
                     unit_ids, [str(celltype_mapping[str(x[0])]) for x in celltype_info["label"][0][0][0]]
@@ -54,8 +90,8 @@ class CellExplorerSortingInterface(BaseSortingExtractorInterface):
         unit_properties = []
         cellinfo_file_path = session_path / f"{session_id}.spikes.cellinfo.mat"
         if cellinfo_file_path.is_file():
-            cell_info = loadmat(cellinfo_file_path).get("spikes", np.empty(0))
-            cell_info_fields = cell_info.dtype.names
+            cell_info = scipy.io.loadmat(cellinfo_file_path).get("spikes", np.empty(0))
+            cell_info_fields = self.cell_info_fields
             if "cluID" in cell_info_fields:
                 unit_properties.append(
                     dict(
@@ -78,9 +114,10 @@ class CellExplorerSortingInterface(BaseSortingExtractorInterface):
                     )
                 )
 
+        # CellClass file 
         celltype_filepath = session_path / f"{session_id}.CellClass.cellinfo.mat"
         if celltype_filepath.is_file():
-            celltype_info = loadmat(celltype_filepath).get("CellClass", np.empty(0))
+            celltype_info = scipy.io.loadmat(celltype_filepath).get("CellClass", np.empty(0))
             if "label" in celltype_info.dtype.names:
                 unit_properties.append(
                     dict(
