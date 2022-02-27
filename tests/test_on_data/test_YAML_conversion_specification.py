f6f9ddb68680ff18517144066b772287c08389ea
Heberto Mayorquin <h.mayorquin@gmail.com>
2022-02-27 11:27:39 +0100

    Update tests/test_on_data/test_YAML_conversion_specification.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- tests/test_on_data/test_YAML_conversion_specification.py
+++ tests/test_on_data/test_YAML_conversion_specification.py
@@ -13,7 +13,7 @@ from nwb_conversion_tools.utils.json_schema import load_dict_from_file
 from nwb_conversion_tools.utils.conversion_tools import run_conversion_from_yaml
 
 # Load data test configuration
-test_config_dic = load_dict_from_file("./tests/test_on_data/gin_test_config.json")
+test_config_dict = load_dict_from_file("./tests/test_on_data/gin_test_config.json")
 
 # GIN dataset: https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
 if os.getenv("CI"):
