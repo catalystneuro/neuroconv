4a2b0acba2c875a69e3ec16585d3c3b52a8f1133
luiz <luiz_tauffer@hotmail.com>
2022-03-01 14:12:12 +0100

    remove converter

--- nwb_conversion_tools/datainterfaces/icephys/abf/guagwei_converter.py
+++
@@ -1,5 +0,0 @@
-from nwb_conversion_tools import AbfNeoDataInterface, NWBConverter
-
-
-class GuagweiConverter(NWBConverter):
-    data_interface_classes = dict(AbfNeoDataInterface=AbfNeoDataInterface)
