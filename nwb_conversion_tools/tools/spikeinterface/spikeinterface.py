de7bc035f44ccc48bf67acbeeeb4906a32c1803a
h-mayorquin <h.mayorquin@gmail.com>
2022-03-07 19:12:03 +0100

    mistake in description of channel_name

--- nwb_conversion_tools/tools/spikeinterface/spikeinterface.py
+++ nwb_conversion_tools/tools/spikeinterface/spikeinterface.py
@@ -332,7 +332,7 @@ def add_electrodes(
     # Add channel names to properties.
     channel_name_array = checked_recording.get_channel_ids()
     elec_columns["channel_name"].update(
-        description="a string named referencefor the channel",
+        description="a string reference for the channel",
         data=channel_name_array,
         index=False,
     )
