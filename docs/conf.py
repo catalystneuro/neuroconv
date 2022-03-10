a2b71cada06bebbba718a8817667d5071862168b
Cody Baker <cody.c.baker.phd@gmail.com>
2022-03-10 23:19:40 +0000

    Automated changes

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ html_static_path = ["_static"]
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
 
