366a1cbbf5a9014df1a99f7970e5da93b1d2b359
bendichter <ben.dichter@gmail.com>
2022-04-09 20:39:46 -0400

    neuroscope: add code for retrieving and testing for getting the session_start_time

--- nwb_conversion_tools/datainterfaces/ecephys/neuroscope/neuroscope_utils.py
+++ nwb_conversion_tools/datainterfaces/ecephys/neuroscope/neuroscope_utils.py
@@ -1,5 +1,6 @@
 """Authors: Cody Baker and Ben Dichter."""
 from pathlib import Path
+from dateutil import parser
 
 from lxml import etree as et
 
@@ -70,3 +71,12 @@ def get_channel_groups(xml_file_path: str) -> list:
         for group in root.find("anatomicalDescription").find("channelGroups").findall("group")
     ]
     return channel_groups
+
+
+def get_session_start_time(xml_file_path):
+    print(xml_file_path)
+    root = get_xml(xml_file_path)
+    date_elem = safe_nested_find(root, ["generalInfo", "date"])
+    if date_elem is not None:
+        return parser.parse(date_elem.text)
+
---
+++ tests/test_on_data/test_metadata/test_neuroscope_metadata.py
@@ -0,0 +1,25 @@
+import datetime
+
+from nwb_conversion_tools.datainterfaces.ecephys.neuroscope.neuroscope_utils import get_session_start_time
+
+from nwb_conversion_tools.datainterfaces.ecephys.neuroscope.neuroscopedatainterface import NeuroscopeSortingInterface
+
+from ..setup_paths import ECEPHY_DATA_PATH
+
+NEUROSCOPE_PATH = ECEPHY_DATA_PATH / "neuroscope"
+
+
+def test_neuroscope_session_start_time():
+    xml_file_path = str(NEUROSCOPE_PATH / "dataset_1" / "YutaMouse42-151117.xml")
+
+    assert get_session_start_time(xml_file_path) == datetime.datetime(2015, 8, 31, 0, 0)
+
+
+def test_get_metadata():
+
+    sx = NeuroscopeSortingInterface(
+        str(NEUROSCOPE_PATH / "dataset_1"),
+        xml_file_path=str(NEUROSCOPE_PATH / "dataset_1"/ "YutaMouse42-151117.xml"),
+    )
+
+    assert sx.get_metadata()["NWBFile"]["session_start_time"] == datetime.datetime(2015, 8, 31, 0, 0)
