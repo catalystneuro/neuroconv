06057b248708ab90a5078702d0a36dbaff2738ec
Cody Baker <cbaker9@nd.edu>
2022-04-11 12:02:06 -0400

    finished logic of second function, added tests

--- nwb_conversion_tools/utils/globbing.py
+++ nwb_conversion_tools/utils/globbing.py
@@ -1,19 +1,33 @@
 """Authors: Cody Baker."""
 import re
+from warnings import warn
 
 
 def get_fstring_variable_names(fstring: str):
     """
     Extract and return all variable names from an fstring.
 
-    Example: get_fstring_variable_names(fstring="a/{x}b{y}/c{z}") = ["x", "y", "z"]
+    Also returns the separators, the strings remnants that surround the variables.
+
+    Example
+    -------
+        variable_names, separators = get_fstring_variable_names(fstring="a/{x}b{y}/c{z}")
+        variable_names = ["x", "y", "z"]
+        separators = ["a/", "b", "/c"", ""]
     """
     matches = re.findall("{.*?}", fstring)
     variable_names = [match.lstrip("{").rstrip("}") for match in matches]
     assert not any(
         (variable_name == "" for variable_name in variable_names)
     ), "Empty variable name detected in f-string! Please ensure there is text between all enclosing '{' and '}'."
-    return variable_names
+    separators = [x.rstrip("{").lstrip("}") for x in re.findall(pattern="^.*?{|}.*?{|}.*?$", string=fstring)]
+    if separators[0] == "":
+        separators = separators[1:]
+    if separators[-1] == "":
+        separators = separators[:-1]
+    if any((separator == "" for separator in separators)):
+        warn("There is an empty separator between two variables in the f-string! The f-string will not be invertible.")
+    return variable_names, separators
 
 
 def get_fstring_values_from_filename(filename: str, fstring: str):
@@ -29,7 +43,10 @@ def get_fstring_values_from_filename(filename: str, fstring: str):
         String containing non-empty substrings enclosed by "{" and "}".
         These correspond to the names of variables thought to encode the actual filename string.
     """
-    variable_names = get_fstring_variable_names(fstring=fstring)
-    husk = [x.rstrip("{").lstrip("}") for x in re.findall("^.*?{|}.*?{|}.*?$")]
-    variable_values = filename
+    variable_names, separators = get_fstring_variable_names(fstring=fstring)
+    pattern = "^"
+    for separator in separators:
+        pattern += f"{separator}(.+)"
+    pattern += "$"
+    variable_values = re.findall(pattern=pattern, string=filename)[0]
     return {variable_name: variable_value for variable_name, variable_value in zip(variable_names, variable_values)}
--- tests/test_internals/test_globbing_utils.py
+++ tests/test_internals/test_globbing_utils.py
@@ -4,7 +4,7 @@ from nwb_conversion_tools.utils import get_fstring_variable_names, get_fstring_v
 
 
 class TestEmptyFStringAssertion(TestCase):
-    def test_get_fstring_variable_names(self):
+    def test_get_fstring_variable_names_assertion(self):
         with self.assertRaisesWith(
             exc_type=AssertionError,
             exc_msg=(
@@ -16,12 +16,27 @@ class TestEmptyFStringAssertion(TestCase):
 
 
 def test_get_fstring_variable_names():
-    assert get_fstring_variable_names(fstring="a/{x}b{y}/c{z}") == ["x", "y", "z"]
+    variable_names, _ = get_fstring_variable_names(fstring="a/{x}b{y}/c{z}")
+    assert variable_names == ["x", "y", "z"]
+
+
+class TestBadSeparatorAssertion(TestCase):
+    def test_get_fstring_separators_assertion(self):
+        with self.assertWarnsWith(
+            warn_type=UserWarning,
+            exc_msg=(
+                "There is an empty separator between two variables in the f-string! "
+                "The f-string will not be invertible."
+            ),
+        ):
+            get_fstring_variable_names(fstring="a/{x}{y}/c{z}")
+
+
+def test_get_fstring_separators():
+    _, separators = get_fstring_variable_names(fstring="a/{x}b{y}/c{z}")
+    assert separators == ["a/", "b", "/c"]
 
 
 def test_get_fstring_values_from_filename():
-    assert get_fstring_values_from_filename(filename="a/foobthat/cbar", fstring="a/{x}b{y}/c{z}") == [
-        "foo",
-        "that",
-        "bar",
-    ]
+    fstring_values = get_fstring_values_from_filename(filename="a/foobthat/cbar", fstring="a/{x}b{y}/c{z}")
+    assert fstring_values == dict(x="foo", y="that", z="bar")
