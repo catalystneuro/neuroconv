f2989fd6d58cc2f20bc634ce3500c091178732af
Cody Baker <cbaker9@nd.edu>
2022-04-27 16:38:21 -0400

    added standard table for general reference

--- tests/test_internals/test_tools.py
+++ tests/test_internals/test_tools.py
@@ -78,11 +78,33 @@ def test_get_globus_dataset_content_sizes():
     }
 
 
+def test_get_s3_conversion_cost_standard():
+    test_sizes = [
+        1,
+        100,
+        1e3,  # 1 GB
+        1e5,  # 100 GB
+        1e6,  # 1 TB
+        1e7,  # 10 TB
+        1e8,  # 100 TB
+    ]
+    results = [get_s3_conversion_cost(total_mb=total_mb) for total_mb in test_sizes]
+    assert results == [
+        2.9730398740210563e-15,  # 1 MB
+        2.973039874021056e-11,  # 100 MB
+        2.9730398740210564e-09,  # 1 GB
+        2.9730398740210563e-05,  # 100 GB
+        0.002973039874021056,  # 1 TB
+        0.2973039874021056,  # 10 TB
+        29.73039874021056,  # 100 TB
+    ]
+
+
 @pytest.mark.skipif(
     not HAVE_GLOBUS or not LOGGED_INTO_GLOBUS,
     reason="You must have globus installed and be logged in to run this test!",
 )
-def test_get_s3_conversion_cost():
+def test_get_s3_conversion_cost_from_globus():
     content_sizes = get_globus_dataset_content_sizes(
         globus_endpoint_id="188a6110-96db-11eb-b7a9-f57b2d55370d",
         path="/SenzaiY/YutaMouse41/YutaMouse41-150821/originalClu/",
