38d494c6b72780cd7465ee0d2c544a92e82bcb33
Cody Baker <cbaker9@nd.edu>
2022-04-27 14:22:53 -0400

    added globus-specific size retrieval helper

---
+++ src/nwb_conversion_tools/tools/data_transfers.py
@@ -0,0 +1,23 @@
+"""Collection of helper functions for assessing and performing automated data transfers."""
+import os
+import json
+from typing import Dict
+
+try:  # pragma: no cover
+    import globus_cli
+
+    HAVE_GLOBUS = True
+except ModuleNotFoundError:
+    HAVE_GLOBUS = False
+assert HAVE_GLOBUS, "You must install the globus CLI (pip install globus-cli)!"
+
+
+def get_globus_dataset_content_sizes(globus_endpoint_id: str, path: str, recursive: bool = True) -> Dict[str, int]:
+    """
+    May require external login via 'globus login' from CLI.
+
+    Returns dictionary whose keys are file names and values are sizes in bytes.
+    """
+    contents = json.loads(os.popen(f"globus ls -Fjson {globus_endpoint_id}:{path} --recursive").read())
+    files_and_sizes = {item["name"]: item["size"] for item in contents["DATA"] if item["type"] == "file"}
+    return files_and_sizes
--- tests/test_internals/test_tools.py
+++ tests/test_internals/test_tools.py
@@ -4,6 +4,7 @@ from pynwb.base import ProcessingModule
 from hdmf.testing import TestCase
 
 from nwb_conversion_tools.tools.nwb_helpers import get_module, make_nwbfile_from_metadata
+from nwb_conversion_tools.tools.data_transfers import get_globus_dataset_content_sizes
 
 
 class TestConversionTools(TestCase):
@@ -34,3 +35,28 @@ class TestConversionTools(TestCase):
             ),
         ):
             make_nwbfile_from_metadata(metadata=dict())
+
+
+def test_get_globus_dataset_content_sizes():
+    """Test is fixed to a subpath that is somewhat unlikely to change in the future."""
+    assert get_globus_dataset_content_sizes(
+        globus_endpoint_id="188a6110-96db-11eb-b7a9-f57b2d55370d",
+        path="/SenzaiY/YutaMouse41/YutaMouse41-150821/originalClu/",
+    ) == {
+        "YutaMouse41-150821.clu.1": 819862,
+        "YutaMouse41-150821.clu.2": 870498,
+        "YutaMouse41-150821.clu.3": 657938,
+        "YutaMouse41-150821.clu.4": 829761,
+        "YutaMouse41-150821.clu.5": 653502,
+        "YutaMouse41-150821.clu.6": 718752,
+        "YutaMouse41-150821.clu.7": 644541,
+        "YutaMouse41-150821.clu.8": 523422,
+        "YutaMouse41-150821.temp.clu.1": 278025,
+        "YutaMouse41-150821.temp.clu.2": 359573,
+        "YutaMouse41-150821.temp.clu.3": 219280,
+        "YutaMouse41-150821.temp.clu.4": 264388,
+        "YutaMouse41-150821.temp.clu.5": 217834,
+        "YutaMouse41-150821.temp.clu.6": 239890,
+        "YutaMouse41-150821.temp.clu.7": 214835,
+        "YutaMouse41-150821.temp.clu.8": 174434,
+    }
