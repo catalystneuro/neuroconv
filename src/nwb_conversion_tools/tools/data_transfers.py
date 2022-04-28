f32474a47b5df28b87f9eee0b14dd29f0cf6f47d
Cody Baker <cbaker9@nd.edu>
2022-04-28 12:02:01 -0400

    added time equation

--- src/nwb_conversion_tools/tools/data_transfers.py
+++ src/nwb_conversion_tools/tools/data_transfers.py
@@ -25,7 +25,7 @@ def get_globus_dataset_content_sizes(globus_endpoint_id: str, path: str, recursi
     return files_and_sizes
 
 
-def get_s3_conversion_cost(
+def estimate_total_conversion_runtime(
     total_mb: float,
     transfer_rate_mb: float = 20.0,
     conversion_rate_mb: float = 17.0,
@@ -33,7 +33,35 @@ def get_s3_conversion_cost(
     compression_ratio: float = 1.7,
 ):
     """
-    Evaluate potential cost of performing an entire conversion on S3 using full automation.
+    Estimate how long the combined process of data transfer, conversion, and upload is expected to take.
+
+    Parameters
+    ----------
+    total_mb: float
+        The total amount of data (in MB) that will be transferred, converted, and uploaded to dandi.
+    transfer_rate_mb: float, optional
+        Estimate of the transfer rate for the data.
+    conversion_rate_mb: float, optional
+        Estimate of the conversion rate for the data. Can vary widely depending on conversion options and type of data.
+        Figure of 17MB/s is based on extensive compression of high-volume, high-resolution ecephys.
+    upload_rate_mb: float, optional
+        Estimate of the upload rate of a single file to the DANDI archive.
+    compression_ratio: float, optional
+        Esimate of the final average compression ratio for datasets in the file. Can vary widely.
+    """
+    c = 1 / compression_ratio  # compressed_size = total_size * c
+    return total_mb * (1 / transfer_rate_mb + 1 / conversion_rate_mb + c / upload_rate_mb)
+
+
+def estimate_s3_conversion_cost(
+    total_mb: float,
+    transfer_rate_mb: float = 20.0,
+    conversion_rate_mb: float = 17.0,
+    upload_rate_mb: float = 40,
+    compression_ratio: float = 1.7,
+):
+    """
+    Estimate potential cost of performing an entire conversion on S3 using full automation.
 
     Parameters
     ----------
--- tests/test_internals/test_tools.py
+++ tests/test_internals/test_tools.py
@@ -6,7 +6,11 @@ from pynwb.base import ProcessingModule
 from hdmf.testing import TestCase
 
 from nwb_conversion_tools.tools.nwb_helpers import get_module, make_nwbfile_from_metadata
-from nwb_conversion_tools.tools.data_transfers import get_globus_dataset_content_sizes, get_s3_conversion_cost
+from nwb_conversion_tools.tools.data_transfers import (
+    get_globus_dataset_content_sizes,
+    estimate_s3_conversion_cost,
+    estimate_total_conversion_runtime,
+)
 
 try:
     import globus_cli
@@ -78,7 +82,7 @@ def test_get_globus_dataset_content_sizes():
     }
 
 
-def test_get_s3_conversion_cost_standard():
+def test_estimate_s3_conversion_cost_standard():
     test_sizes = [
         1,
         100,
@@ -88,7 +92,7 @@ def test_get_s3_conversion_cost_standard():
         1e7,  # 10 TB
         1e8,  # 100 TB
     ]
-    results = [get_s3_conversion_cost(total_mb=total_mb) for total_mb in test_sizes]
+    results = [estimate_s3_conversion_cost(total_mb=total_mb) for total_mb in test_sizes]
     assert results == [
         2.9730398740210563e-15,  # 1 MB
         2.973039874021056e-11,  # 100 MB
@@ -104,19 +108,19 @@ def test_get_s3_conversion_cost_standard():
     not HAVE_GLOBUS or not LOGGED_INTO_GLOBUS,
     reason="You must have globus installed and be logged in to run this test!",
 )
-def test_get_s3_conversion_cost_from_globus_single_session():
+def test_estimate_s3_conversion_cost_from_globus_single_session():
     content_sizes = get_globus_dataset_content_sizes(
         globus_endpoint_id="188a6110-96db-11eb-b7a9-f57b2d55370d",
         path="/SenzaiY/YutaMouse41/YutaMouse41-150821/originalClu/",
     )
-    assert get_s3_conversion_cost(total_mb=sum(content_sizes.values()) / 1e6) == 1.756555806400279e-13
+    assert estimate_s3_conversion_cost(total_mb=sum(content_sizes.values()) / 1e6) == 1.756555806400279e-13
 
 
 @pytest.mark.skipif(
     not HAVE_GLOBUS or not LOGGED_INTO_GLOBUS,
     reason="You must have globus installed and be logged in to run this test!",
 )
-def test_get_s3_conversion_cost_from_globus_multiple_sessions():
+def test_estimate_s3_conversion_cost_from_globus_multiple_sessions():
     all_content_sizes = {
         session_name: get_globus_dataset_content_sizes(
             globus_endpoint_id="188a6110-96db-11eb-b7a9-f57b2d55370d",
@@ -127,9 +131,31 @@ def test_get_s3_conversion_cost_from_globus_multiple_sessions():
     assert (
         sum(
             [
-                get_s3_conversion_cost(total_mb=sum(content_sizes.values()) / 1e6)
+                estimate_s3_conversion_cost(total_mb=sum(content_sizes.values()) / 1e6)
                 for content_sizes in all_content_sizes.values()
             ]
         )
         == 1.3393785277236152e-07
     )
+
+
+def test_estimate_total_conversion_runtime():
+    test_sizes = [
+        1,
+        100,
+        1e3,  # 1 GB
+        1e5,  # 100 GB
+        1e6,  # 1 TB
+        1e7,  # 10 TB
+        1e8,  # 100 TB
+    ]
+    results = [estimate_total_conversion_runtime(total_mb=total_mb) for total_mb in test_sizes]
+    assert results == [
+        0.12352941176470589,
+        12.352941176470589,
+        123.52941176470588,
+        12352.94117647059,
+        123529.41176470589,
+        1235294.1176470588,
+        12352941.176470589,
+    ]
