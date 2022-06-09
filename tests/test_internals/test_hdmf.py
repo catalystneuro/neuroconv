78a6a5378c7d7c2b995cd0b6d35f7397758a4b0e
Cody Baker <codycbakerphd@gmail.com>
2022-06-09 10:58:14 -0400

    fixed tests

--- tests/test_internals/test_hdmf.py
+++ tests/test_internals/test_hdmf.py
@@ -20,8 +20,8 @@ def test_early_exit():
 
 def test_buffer_padding_long_shape():
     """Uses ~8 MB array with 11 MB buffer size and 1 MB chunk size (default)."""
-    iterator = SliceableDataChunkIterator(data=np.empty(shape=(160000, 20)), buffer_gb=1.1e-2)
-    assert iterator.buffer_shape == (96000, 12)
+    iterator = SliceableDataChunkIterator(data=np.empty(shape=(10 ** 7, 20)), buffer_gb=1.1e-2)
+    assert iterator.buffer_shape == (68482, 20)
 
 
 def test_buffer_padding_mixed_shape():
