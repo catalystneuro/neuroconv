057d6f10924c3861c877c717dd847d004c83cb83
Cody Baker <codycbakerphd@gmail.com>
2022-06-09 08:37:22 -0400

    PR requests; also restored original method to be used for comparison

--- src/nwb_conversion_tools/tools/hdmf.py
+++ src/nwb_conversion_tools/tools/hdmf.py
@@ -1,3 +1,4 @@
+"""Collection of modifications of HDMF functions that are to be tested/used on this repo until propagation upstream."""
 from typing import Tuple
 
 import numpy as np
@@ -21,38 +22,56 @@ class GenericDataChunkIterator(HDMFGenericDataChunkIterator):
         if np.prod(maxshape) * self.dtype.itemsize / 1e9 < buffer_gb:
             return tuple(self.maxshape)
 
-        chunks_per_axis = np.ceil(maxshape / self.chunk_shape)
-        buffer_shape = np.array(self.chunk_shape)
         buffer_bytes = chunk_bytes
         axis_sizes_bytes = maxshape * self.dtype.itemsize
-        chunk_size_order = np.argsort(self.chunk_shape)
+        smallest_chunk_axis, second_smallest_chunk_axis, *_ = np.argsort(self.chunk_shape)
         target_buffer_bytes = buffer_gb * 1e9
 
         # If the smallest full axis does not fit within the buffer size, form a square along the two smallest axes
+        sub_square_buffer_shape = np.array(self.chunk_shape)
         if min(axis_sizes_bytes) > target_buffer_bytes:
             k1 = np.floor((target_buffer_bytes / chunk_bytes) ** 0.5)
-            for idx in [0, 1]:
-                buffer_shape[chunk_size_order[idx]] = k1 * buffer_shape[chunk_size_order[idx]]
-            return tuple(buffer_shape)
+            for axis in [smallest_chunk_axis, second_smallest_chunk_axis]:
+                sub_square_buffer_shape[axis] = k1 * sub_square_buffer_shape[axis]
+            return tuple(sub_square_buffer_shape)
+
+        # Original one-shot estimation has good performance for certain shapes
+        k2 = np.floor(
+            (buffer_gb * 1e9 / (np.prod(self.chunk_shape) * self.dtype.itemsize)) ** (1 / len(self.chunk_shape))
+        )
+        unpadded_buffer_shape = tuple(
+            [
+                min(max(int(x), self.chunk_shape[j]), self.maxshape[j])
+                for j, x in enumerate(k2 * np.array(self.chunk_shape))
+            ]
+        )
+        unpadded_buffer_bytes = np.prod(unpadded_buffer_shape) * self.dtype.itemsize
 
         # Otherwise, try to start by filling the smallest axis completely or calculate best partial fill
+        padded_buffer_shape = np.array(self.chunk_shape)
+        chunks_per_axis = np.ceil(maxshape / self.chunk_shape)
         small_axis_fill_size = chunk_bytes * min(chunks_per_axis)
         full_axes_used = np.zeros(shape=len(self.maxshape), dtype=bool)
         if small_axis_fill_size <= target_buffer_bytes:
             buffer_bytes = small_axis_fill_size
-            buffer_shape[chunk_size_order[0]] = self.maxshape[chunk_size_order[0]]
-            full_axes_used[chunk_size_order[0]] = True
+            padded_buffer_shape[smallest_chunk_axis] = self.maxshape[smallest_chunk_axis]
+            full_axes_used[smallest_chunk_axis] = True
         for axis, chunks_on_axis in enumerate(chunks_per_axis):
             if full_axes_used[axis]:  # If the smallest axis, skip since already used
                 continue
             if chunks_on_axis * buffer_bytes <= target_buffer_bytes:  # If multiple axes can be used together
                 buffer_bytes *= chunks_on_axis
-                buffer_shape[axis] = self.maxshape[axis]
+                padded_buffer_shape[axis] = self.maxshape[axis]
             else:  # Found an axis that is too large to use with the rest of the buffer; calculate how much can be used
-                k2 = np.floor(target_buffer_bytes / buffer_bytes)
-                buffer_shape[axis] *= k2
+                k3 = np.floor(target_buffer_bytes / buffer_bytes)
+                padded_buffer_shape[axis] *= k3
                 break
-        return tuple(buffer_shape)
+        padded_buffer_bytes = np.prod(padded_buffer_shape) * self.dtype.itemsize
+
+        if padded_buffer_bytes >= unpadded_buffer_bytes:
+            return tuple(padded_buffer_shape)
+        else:
+            return tuple(unpadded_buffer_shape)
 
 
 class SliceableDataChunkIterator(GenericDataChunkIterator):
--- tests/test_internals/test_hdmf.py
+++ tests/test_internals/test_hdmf.py
@@ -9,28 +9,28 @@ class TestIteratorAssertions(TestCase):
         with self.assertRaisesWith(
             AssertionError, exc_msg="buffer_gb (5e-06) must be greater than the chunk size (0.000996872)!"
         ):
-            SliceableDataChunkIterator(data=np.random.random(size=(10 ** 3, 10 ** 3)), buffer_gb=0.000005)
+            SliceableDataChunkIterator(data=np.empty(shape=(1000, 1000)), buffer_gb=0.000005)
 
 
 def test_early_exit():
     """Uses a 32 byte array with 1 GB buffer size (default) and 1 MB chunk size (default)."""
-    iterator = SliceableDataChunkIterator(data=np.random.random(size=(2, 2)))
+    iterator = SliceableDataChunkIterator(data=np.empty(shape=(2, 2)))
     assert iterator.maxshape == iterator.buffer_shape
 
 
 def test_buffer_padding_long_shape():
     """Uses ~8 MB array with 11 MB buffer size and 1 MB chunk size (default)."""
-    iterator = SliceableDataChunkIterator(data=np.zeros(shape=(160000, 20)), buffer_gb=1.1e-2)
+    iterator = SliceableDataChunkIterator(data=np.empty(shape=(160000, 20)), buffer_gb=1.1e-2)
     assert iterator.buffer_shape == (48000, 20)
 
 
 def test_buffer_padding_mixed_shape():
     """Uses ~15 MB array with 11 MB buffer size and 1 MB chunk size (default)."""
-    iterator = SliceableDataChunkIterator(data=np.zeros(shape=(20, 40, 2401)), buffer_gb=1.1e-2)
+    iterator = SliceableDataChunkIterator(data=np.empty(shape=(20, 40, 2401)), buffer_gb=1.1e-2)
     assert iterator.buffer_shape == (20, 40, 960)
 
 
 def test_min_axis_too_large():
     """uses ~8 MB array with each contiguous axis at around ~8 KB with 5 KB buffer_size and 1 KB chunk size."""
-    iterator = SliceableDataChunkIterator(data=np.zeros(shape=(10 ** 3, 10 ** 3)), chunk_mb=1e-3, buffer_gb=5e-6)
+    iterator = SliceableDataChunkIterator(data=np.empty(shape=(10 ** 3, 10 ** 3)), chunk_mb=1e-3, buffer_gb=5e-6)
     assert iterator.buffer_shape == (22, 22)
