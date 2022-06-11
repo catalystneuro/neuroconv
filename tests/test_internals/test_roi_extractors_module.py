18c4697fe106ed9115b88462873fdb6201437196
h-mayorquin <h.mayorquin@gmail.com>
2022-06-10 19:52:53 -0700

    correct wrong transposition in add_two_photon_series

--- tests/test_internals/test_roi_extractors_module.py
+++ tests/test_internals/test_roi_extractors_module.py
@@ -1,14 +1,15 @@
+from tempfile import mkdtemp
 import unittest
 from pathlib import Path
 from datetime import datetime
 
 import numpy as np
 
-from pynwb import NWBFile
+from pynwb import NWBFile, NWBHDF5IO
 from pynwb.device import Device
+from roiextractors.testing import generate_dummy_imaging_extractor
 
-
-from nwb_conversion_tools.tools.roiextractors import add_devices
+from nwb_conversion_tools.tools.roiextractors import add_devices, add_two_photon_series
 
 
 class TestAddDevices(unittest.TestCase):
@@ -140,3 +141,106 @@ class TestAddDevices(unittest.TestCase):
         assert len(devices) == 2
         assert "device_metadata" in devices
         assert "device_object" in devices
+
+
+class TestAddTwoPhotonSeries(unittest.TestCase):
+    def setUp(self):
+        self.session_start_time = datetime.now().astimezone()
+        self.nwbfile = NWBFile(
+            session_description="session_description",
+            identifier="file_id",
+            session_start_time=self.session_start_time,
+        )
+        self.metadata = dict(Ophys=dict())
+
+        self.device_name = "optical_device"
+        self.device_metadata = dict(name=self.device_name)
+        self.metadata["Ophys"].update(Device=[self.device_metadata])
+
+        self.optical_channel_metadata = dict(
+            name="optical_channel",
+            emission_lambda=np.nan,
+            description="description",
+        )
+
+        self.imaging_plane_name = "imaging_plane_name"
+        self.imaging_plane_metadata = dict(
+            name=self.imaging_plane_name,
+            optical_channel=[self.optical_channel_metadata],
+            description="image_plane_description",
+            device=self.device_name,
+            excitation_lambda=np.nan,
+            indicator="unknown",
+            location="unknown",
+        )
+
+        self.metadata["Ophys"].update(ImagingPlane=[self.imaging_plane_metadata])
+
+        self.two_photon_series_name = "two_photon_series_name"
+        self.two_photon_series_metadata = dict(
+            name=self.two_photon_series_name, imaging_plane=self.imaging_plane_name, unit="unknown"
+        )
+        self.metadata["Ophys"].update(TwoPhotonSeries=[self.two_photon_series_metadata])
+
+        self.num_frames = 30
+        self.rows = 10
+        self.columns = 15
+        self.imaging_extractor = generate_dummy_imaging_extractor(self.num_frames, rows=self.rows, columns=self.columns)
+
+    def test_add_two_photon_series(self):
+
+        metadata = self.metadata
+
+        add_two_photon_series(imaging=self.imaging_extractor, nwbfile=self.nwbfile, metadata=metadata)
+
+        # Check data
+        acquisition_modules = self.nwbfile.acquisition
+        self.two_photon_series_name in acquisition_modules
+        data_in_hdfm_data_io = acquisition_modules[self.two_photon_series_name].data
+        data_chunk_iterator = data_in_hdfm_data_io.data
+        two_photon_series_extracted = np.concatenate([data_chunk.data for data_chunk in data_chunk_iterator])
+        assert two_photon_series_extracted.shape == (self.num_frames, self.rows, self.columns)
+
+        # Check device
+        devices = self.nwbfile.devices
+        assert self.device_name in devices
+        assert len(devices) == 1
+
+        # Check imaging planes
+        imaging_planes_in_file = self.nwbfile.imaging_planes
+        assert self.imaging_plane_name in imaging_planes_in_file
+        assert len(imaging_planes_in_file) == 1
+
+    def test_add_two_photon_series_roundtrip(self):
+
+        metadata = self.metadata
+
+        add_two_photon_series(imaging=self.imaging_extractor, nwbfile=self.nwbfile, metadata=metadata)
+
+        # Write the data to disk
+        nwbfile_path = Path(mkdtemp()) / "two_photon_roundtrip.nwb"
+        with NWBHDF5IO(nwbfile_path, "w") as io:
+            io.write(self.nwbfile)
+
+        with NWBHDF5IO(nwbfile_path, "r") as io:
+            read_nwbfile = io.read()
+
+            # Check data
+            acquisition_modules = read_nwbfile.acquisition
+            self.two_photon_series_name in acquisition_modules
+            two_photon_series = acquisition_modules[self.two_photon_series_name].data
+            assert two_photon_series.shape == (self.num_frames, self.rows, self.columns)
+
+            # Check device
+            devices = read_nwbfile.devices
+            assert self.device_name in devices
+            assert len(devices) == 1
+
+            # Check imaging planes
+            imaging_planes_in_file = read_nwbfile.imaging_planes
+            assert self.imaging_plane_name in imaging_planes_in_file
+            assert len(imaging_planes_in_file) == 1
+
+
+if __name__ == "__main__":
+    unittest.main()
