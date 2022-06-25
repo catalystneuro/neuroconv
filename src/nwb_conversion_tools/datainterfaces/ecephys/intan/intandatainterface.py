9b5dc5fa921c97ff3c17ae736e9da20389bd2ba0
h-mayorquin <h.mayorquin@gmail.com>
2022-06-24 21:51:30 -0600

    function to extract metadata

--- src/nwb_conversion_tools/datainterfaces/ecephys/intan/intandatainterface.py
+++ src/nwb_conversion_tools/datainterfaces/ecephys/intan/intandatainterface.py
@@ -1,6 +1,7 @@
 """Authors: Cody Baker and Ben Dichter."""
 from pathlib import Path
 
+
 import spikeextractors as se
 from pynwb.ecephys import ElectricalSeries
 
@@ -16,6 +17,31 @@ except ImportError:
 INSTALL_MESSAGE = "Please install pyintan to use this extractor!"
 
 
+def extract_electrode_metadata_with_pyintan(file_path):
+    if ".rhd" in Path(file_path).suffixes:
+        intan_file_metadata = read_rhd(file_path)[1]
+    else:
+        intan_file_metadata = read_rhs(file_path)[1]
+    exclude_chan_types = ["AUX", "ADC", "VDD"]
+    valid_channels = [
+        x for x in intan_file_metadata if not any([y in x["native_channel_name"] for y in exclude_chan_types])
+    ]
+
+    group_names = [channel["native_channel_name"].split("-")[0] for channel in valid_channels]
+    unique_group_names = set(group_names)
+    group_electrode_numbers = [channel["native_order"] for channel in valid_channels]
+    custom_names = [channel["custom_channel_name"] for channel in valid_channels]
+
+    electrodes_metadata = dict(
+        group_names=group_names,
+        unique_group_names=unique_group_names,
+        group_electrode_numbers=group_electrode_numbers,
+        custom_names=custom_names,
+    )
+
+    return electrodes_metadata
+
+
 class IntanRecordingInterface(BaseRecordingExtractorInterface):
     """Primary data interface class for converting a IntanRecordingExtractor."""
 
@@ -24,18 +50,13 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
     def __init__(self, file_path: FilePathType, verbose: bool = True):
         assert HAVE_PYINTAN, INSTALL_MESSAGE
         super().__init__(file_path=file_path, verbose=verbose)
-        if ".rhd" in Path(self.source_data["file_path"]).suffixes:
-            intan_file_metadata = read_rhd(self.source_data["file_path"])[1]
-        else:
-            intan_file_metadata = read_rhs(self.source_data["file_path"])[1]
-        exclude_chan_types = ["AUX", "ADC", "VDD"]
-        valid_channels = [
-            x for x in intan_file_metadata if not any([y in x["native_channel_name"] for y in exclude_chan_types])
-        ]
 
-        group_names = [channel["native_channel_name"].split("-")[0] for channel in valid_channels]
-        unique_group_names = set(group_names)
-        group_electrode_numbers = [channel["native_order"] for channel in valid_channels]
+        electrodes_metadata = extract_electrode_metadata_with_pyintan(file_path)
+
+        group_names = electrodes_metadata["group_names"]
+        group_electrode_numbers = electrodes_metadata["group_electrode_numbers"]
+        unique_group_names = electrodes_metadata["unique_group_names"]
+        custom_names = electrodes_metadata["custom_names"]
 
         channel_ids = self.recording_extractor.get_channel_ids()
         for channel_id, channel_group in zip(channel_ids, group_names):
@@ -47,7 +68,6 @@ class IntanRecordingInterface(BaseRecordingExtractorInterface):
                 self.recording_extractor.set_channel_property(
                     channel_id=channel_id, property_name="group_electrode_number", value=group_electrode_number
                 )
-        custom_names = [channel["custom_channel_name"] for channel in valid_channels]
         if any(custom_names):
             for channel_id, custom_name in zip(channel_ids, custom_names):
                 self.recording_extractor.set_channel_property(
