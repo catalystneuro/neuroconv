ebc2248ae72c7e11d86ed484302428b458776958
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-06-27 14:07:15 -0400

    Update test_gin_ecephys.py

--- tests/test_on_data/test_gin_ecephys.py
+++ tests/test_on_data/test_gin_ecephys.py
@@ -131,7 +131,7 @@ class TestEcephysNwbConversions(unittest.TestCase):
             )
 
     for spikeextractors_backend in [True, False]:
-        sub_path = Path("spikeglx") / "Noise4Sam_g0" / "Noise4Sam_g0_imec0"
+        sub_path = Path("spikeglx") / "TEST_20210920_0_g0" / "TEST_20210920_0_g0_t0.imec0.ap.bin"
         parameterized_recording_list.append(
             param(
                 data_interface=SpikeGLXRecordingInterface,
