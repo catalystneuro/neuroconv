e2ea1c64494037c33678996f22064452f5b6c65e
h-mayorquin <h.mayorquin@gmail.com>
2022-06-30 15:44:18 -0700

    removed test for srm as this is not supported

--- tests/test_on_data/test_gin_ecephys.py
+++ tests/test_on_data/test_gin_ecephys.py
@@ -109,10 +109,6 @@ class TestEcephysNwbConversions(unittest.TestCase):
         ),
         param(
             data_interface=CEDRecordingInterface,
-            interface_kwargs=dict(file_path=str(DATA_PATH / "spike2" / "130322-1LY.smr")),
-        ),
-        param(
-            data_interface=CEDRecordingInterface,
             interface_kwargs=dict(file_path=str(DATA_PATH / "spike2" / "m365_1sec.smrx")),
         ),
     ]
