bd61cbdfb1be6c2ba34d36f07c559d00e2223f3b
h-mayorquin <h.mayorquin@gmail.com>
2022-06-29 20:24:48 -0700

    modified suit2p data interface

--- src/nwb_conversion_tools/datainterfaces/ophys/suite2p/suite2pdatainterface.py
+++ src/nwb_conversion_tools/datainterfaces/ophys/suite2p/suite2pdatainterface.py
@@ -2,7 +2,7 @@ from roiextractors import Suite2pSegmentationExtractor
 
 
 from ..basesegmentationextractorinterface import BaseSegmentationExtractorInterface
-from ....utils import FilePathType, IntType
+from ....utils import FolderPathType, IntType
 
 
 class Suite2pSegmentationInterface(BaseSegmentationExtractorInterface):
@@ -10,5 +10,5 @@ class Suite2pSegmentationInterface(BaseSegmentationExtractorInterface):
 
     SegX = Suite2pSegmentationExtractor
 
-    def __init__(self, file_path: FilePathType, combined: bool = False, plane_no: IntType = 0):
-        super().__init__(file_path=file_path, combined=combined, plane_no=plane_no)
+    def __init__(self, folder_path: FolderPathType, combined: bool = False, plane_no: IntType = 0):
+        super().__init__(folder_path=folder_path, combined=combined, plane_no=plane_no)
