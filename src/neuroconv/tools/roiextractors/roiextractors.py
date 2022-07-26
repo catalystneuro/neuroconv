8d8fff449b84140001ec8d55519c16146894b793
weiglszonja <wszonja@gmail.com>
2022-07-26 16:08:20 +0200

    use new methods in write_segmentation

--- src/neuroconv/tools/roiextractors/roiextractors.py
+++ src/neuroconv/tools/roiextractors/roiextractors.py
@@ -693,77 +693,25 @@ def write_segmentation(
             image_segmentation_name = (
                 "ImageSegmentation" if plane_no_loop == 0 else f"ImageSegmentation_Plane{plane_no_loop}"
             )
-            if image_segmentation_name not in ophys.data_interfaces:
-                image_segmentation = ImageSegmentation(name=image_segmentation_name)
-                ophys.add(image_segmentation)
-            else:
-                image_segmentation = ophys.data_interfaces.get(image_segmentation_name)
+            add_image_segmentation(nwbfile=nwbfile_out, metadata=metadata)
+            image_segmentation = ophys.data_interfaces.get(image_segmentation_name)
 
             # Add imaging plane
-            imaging_plane_name = "ImagingPlane" if plane_no_loop == 0 else f"ImagePlane_{plane_no_loop}"
             add_imaging_plane(nwbfile=nwbfile_out, metadata=metadata)
-            imaging_plane = nwbfile_out.imaging_planes[imaging_plane_name]
 
             # PlaneSegmentation:
-            input_kwargs = dict(
-                description="output from segmenting imaging plane",
-                imaging_plane=imaging_plane,
+            add_plane_segmentation(
+                segmentation_extractor=segext_obj,
+                nwbfile=nwbfile_out,
+                metadata=metadata,
+                plane_segmentation_index=plane_no_loop,
+                iterator_options=dict(buffer_size=buffer_size),
+                compression_options=dict(compression=True, compression_opts=9,)
             )
-            ps_metadata = metadata["Ophys"]["ImageSegmentation"]["plane_segmentations"][0]
-            if ps_metadata["name"] not in image_segmentation.plane_segmentations:
-                ps_exist = False
-            else:
-                ps = image_segmentation.get_plane_segmentation(ps_metadata["name"])
-                ps_exist = True
-            roi_ids = segext_obj.get_roi_ids()
-            accepted_list = segext_obj.get_accepted_list()
-            accepted_list = [] if accepted_list is None else accepted_list
-            rejected_list = segext_obj.get_rejected_list()
-            rejected_list = [] if rejected_list is None else rejected_list
-            accepted_ids = [1 if k in accepted_list else 0 for k in roi_ids]
-            rejected_ids = [1 if k in rejected_list else 0 for k in roi_ids]
-            roi_locations = np.array(segext_obj.get_roi_locations()).T
 
-            def image_mask_iterator():
-                for id in segext_obj.get_roi_ids():
-                    img_msks = segext_obj.get_roi_image_masks(roi_ids=[id]).T.squeeze()
-                    yield img_msks
-
-            if not ps_exist:
-                from hdmf.common import VectorData
-
-                input_kwargs.update(
-                    **ps_metadata,
-                    columns=[
-                        VectorData(
-                            data=H5DataIO(
-                                DataChunkIterator(image_mask_iterator(), buffer_size=buffer_size),
-                                compression=True,
-                                compression_opts=9,
-                            ),
-                            name="image_mask",
-                            description="image masks",
-                        ),
-                        VectorData(
-                            data=roi_locations,
-                            name="RoiCentroid",
-                            description="x,y location of centroid of the roi in image_mask",
-                        ),
-                        VectorData(
-                            data=accepted_ids,
-                            name="Accepted",
-                            description="1 if ROi was accepted or 0 if rejected as a cell during segmentation operation",
-                        ),
-                        VectorData(
-                            data=rejected_ids,
-                            name="Rejected",
-                            description="1 if ROi was rejected or 0 if accepted as a cell during segmentation operation",
-                        ),
-                    ],
-                    id=roi_ids,
-                )
-
-                ps = image_segmentation.create_plane_segmentation(**input_kwargs)
+            plane_segmentation_metadata = metadata["Ophys"]["ImageSegmentation"]["plane_segmentations"]
+            plane_segmentation_name = plane_segmentation_metadata[plane_no_loop]["name"]
+            plane_segmentation = image_segmentation.plane_segmentations[plane_segmentation_name]
 
             # Fluorescence Traces - This should be a function on its own.
             if "Flourescence" not in ophys.data_interfaces:
@@ -772,7 +720,7 @@ def write_segmentation(
             else:
                 fluorescence = ophys.data_interfaces["Fluorescence"]
 
-            roi_table_region = ps.create_roi_table_region(
+            roi_table_region = plane_segmentation.create_roi_table_region(
                 description=f"region for Imaging plane{plane_no_loop}",
                 region=list(range(segext_obj.get_num_rois())),
             )
