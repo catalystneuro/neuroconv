db22feac822c216b0eea4dd91a748338b930c7c4
h-mayorquin <h.mayorquin@gmail.com>
2022-08-01 18:17:52 +0200

    remove tests with empty strings

--- tests/test_on_data/test_metadata/test_edf_metadata.py
+++ tests/test_on_data/test_metadata/test_edf_metadata.py
@@ -20,22 +20,6 @@ class TestEDFMetadata(unittest.TestCase):
         expected_session_start_time = datetime(2022, 3, 2, 10, 42, 19)
         assert extracted_session_start_time == expected_session_start_time
 
-        extracted_experimenter = nwb_metadata["experimenter"]
-        expected_experimenter = ""
-        assert extracted_experimenter == expected_experimenter
-
-    def test_subject_metadata(self):
-
-        subject_metadata = self.interface.extract_subject_metadata()
-
-        extracted_subject_id = subject_metadata["subject_id"]
-        expected_subject_id = ""
-        assert extracted_subject_id == expected_subject_id
-
-        extracted_subject_date_of_birth = subject_metadata["date_of_birth"]
-        expected_subject_date_of_birth = ""
-        assert extracted_subject_date_of_birth == expected_subject_date_of_birth
-
 
 if __name__ == "__main__":
     unittest.main()
