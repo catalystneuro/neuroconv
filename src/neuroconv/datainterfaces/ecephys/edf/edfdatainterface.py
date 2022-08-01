40aad7392bd5454f5107ae38ff2b1097bc201f38
h-mayorquin <h.mayorquin@gmail.com>
2022-08-01 18:00:08 +0200

    Revert "fix dict update"
    This reverts commit 7ff6f156b94276aaa27ae692b816c1462b1b369d.

--- src/neuroconv/datainterfaces/ecephys/edf/edfdatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/edf/edfdatainterface.py
@@ -56,6 +56,6 @@ class EDFRecordingInterface(BaseRecordingExtractorInterface):
         metadata["NWBFile"].update(nwbfile_metadata)
 
         subject_metadata = self.extract_subject_metadata()
-        metadata.get("Subject", dict()).update(subject_metadata)
+        metadata.get("Subject", dict()).upate(subject_metadata)
 
         return metadata
