0c05aee9720613066b8faed8551d67ac77dce6ef
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2022-08-19 16:25:46 +0000

    utilize extra installation and setup glob for ophys

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup, find_packages
-from codecs import open
 from pathlib import Path
-import os
 from shutil import copy
+from collections import defaultdict
 
-path = os.path.abspath(os.path.dirname(__file__))
+root = Path(__file__).parent
 
-with open(os.path.join(path, "README.md")) as f:
+with open(root / "README.md") as f:
     long_description = f.read()
-with open(os.path.join(path, "requirements-minimal.txt")) as f:
+with open(root / "requirements-minimal.txt") as f:
     install_requires = f.readlines()
-with open(os.path.join(path, "requirements-full.txt")) as f:
-    full_dependencies = f.readlines()
-with open(os.path.join(path, "requirements-rtd.txt")) as f:
+with open(root / "requirements-full.txt") as f:
+    full_dependencies = set(f.readlines())
+with open(root / "requirements-rtd.txt") as f:
     documentation_dependencies = f.readlines()
-with open(os.path.join(path, "requirements-testing.txt")) as f:
+with open(root / "requirements-testing.txt") as f:
     testing_suite_dependencies = f.readlines()
 
-extras_require = dict(full=full_dependencies, test=testing_suite_dependencies, docs=documentation_dependencies)
+extras_require = defaultdict(list)
+subpaths = (root / "src" / "neuroconv" / "datainterfaces").rglob("*/requirements.txt")
+for subpath in subpaths:
+    with open(subpath) as f:
+        subpath_requirements = f.readlines()
+    extras_require[subpath.parent.name].extend(subpath_requirements)
+    full_dependencies.update(subpath_requirements)
+
+    if "ophys" in subpath.parts:
+        extras_require["ophys"].extend(subpath_requirements)
+
+
+extras_require.update(full=list(full_dependencies), test=testing_suite_dependencies, docs=documentation_dependencies)
 
 # Create a local copy for the gin test configuration file based on the master file `base_gin_test_config.json`
 gin_config_file_base = Path("./base_gin_test_config.json")
