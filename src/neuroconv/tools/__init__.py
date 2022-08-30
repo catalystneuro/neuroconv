d5749506bdc1050e098a7bc82947245ab311c5e7
h-mayorquin <h.mayorquin@gmail.com>
2022-08-30 17:55:14 +0200

    fix post merge

--- src/neuroconv/tools/__init__.py
+++ src/neuroconv/tools/__init__.py
@@ -1,8 +1,7 @@
 # To be treated as sub-modules or plugins
-# TODO: temporarily disabled until decision to remove in other PR
-# from .spikeinterface import spikeinterface
-# from .roiextractors import roiextractors
-# from .neo import neo
+from .spikeinterface import spikeinterface
+from .roiextractors import roiextractors
+from .neo import neo
 
 # Basically utils but rely on external dependencies
 from .importing import get_package
