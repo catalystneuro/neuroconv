1fb509e604e62d652509d31752816005e0724c7f
weiglszonja <wszonja@gmail.com>
2022-09-01 18:13:42 +0200

    propagate output_struct_name to ExtractSegmentationInterface

--- src/neuroconv/datainterfaces/ophys/extract/extractdatainterface.py
+++ src/neuroconv/datainterfaces/ophys/extract/extractdatainterface.py
@@ -5,6 +5,16 @@ from ....utils import FilePathType, FloatType
 class ExtractSegmentationInterface(BaseSegmentationExtractorInterface):
     """Data interface for ExtractSegmentationExtractor."""
 
-    def __init__(self, file_path: FilePathType, sampling_frequency: FloatType, verbose: bool = True):
+    def __init__(
+            self,
+            file_path: FilePathType,
+            sampling_frequency: FloatType,
+            output_struct_name: str = "output",
+            verbose: bool = True,
+    ):
         self.verbose = verbose
-        super().__init__(file_path=file_path, sampling_frequency=sampling_frequency)
+        super().__init__(
+            file_path=file_path,
+            sampling_frequency=sampling_frequency,
+            output_struct_name=output_struct_name,
+        )
