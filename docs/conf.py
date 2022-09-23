8239b9525a33626aa68b729a2d187d8987934dce
h-mayorquin <h.mayorquin@gmail.com>
2022-09-23 18:38:27 +0200

    installing everything in docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import inspect
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
-from neuroconv.tools import spikeinterface
+# from neuroconv.tools import spikeinterface
 
 project = "NeuroConv"
 copyright = "2022, CatalystNeuro"
