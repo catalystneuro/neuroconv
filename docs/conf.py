57778970081224c0f0e6d747bb4b8435fec5a886
h-mayorquin <h.mayorquin@gmail.com>
2022-09-23 20:11:08 +0200

    try spikeitnerface again with only spikeinterface

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ import inspect
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+import neuroconv.tools.roiextractors.roiextractors
 
 project = "NeuroConv"
 copyright = "2022, CatalystNeuro"
