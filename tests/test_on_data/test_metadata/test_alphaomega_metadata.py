1b3746d1d21d0c456f0953626cca9af11fa17060
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-11-23 20:37:37 -0500

    Apply suggestions from code review
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- tests/test_on_data/test_metadata/test_alphaomega_metadata.py
+++ tests/test_on_data/test_metadata/test_alphaomega_metadata.py
@@ -6,19 +6,14 @@ from neuroconv.datainterfaces import AlphaOmegaRecordingInterface
 from ..setup_paths import ECEPHY_DATA_PATH
 
 
-class TestEDFMetadata(unittest.TestCase):
+class TestAlphaOmegaMetadata(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         folder_path = ECEPHY_DATA_PATH / "alphaomega" / "mpx_map_version4"
         cls.interface = AlphaOmegaRecordingInterface(folder_path=folder_path)
 
     def test_nwb_metadata(self):
-        nwb_metadata = self.interface.get_metadata()["NWBFile"]
-
-        extracted_session_start_time = nwb_metadata["session_start_time"]
-        expected_session_start_time = datetime(2021, 11, 19, 15, 23, 15)
-
-        assert extracted_session_start_time == expected_session_start_time
+        assert self.interface.get_metadata()["NWBFile"]["session_start_time"] == datetime(2021, 11, 19, 15, 23, 15)
 
 
 if __name__ == "__main__":
