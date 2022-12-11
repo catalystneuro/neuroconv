1f5ca1ad4ff4a9dec337eb586140fef80365f9ce
bendichter <ben.dichter@gmail.com>
2022-12-11 14:12:51 -0600

    add function to syncrhonize timestamps

--- src/neuroconv/tools/signal_processing.py
+++ src/neuroconv/tools/signal_processing.py
@@ -1,4 +1,4 @@
-"""Author: Cody Baker."""
+"""Author: Cody Baker and Ben Dichter."""
 from typing import Tuple
 
 import numpy as np
@@ -40,3 +40,35 @@ def parse_rising_and_falling_frames_from_ttl(trace: np.ndarray, num_bins: int = 
     falling_frames = np.where(diff_binned_states < 0)[0]
 
     return rising_frames, falling_frames
+
+
+def syncrhonize_timestamps_from_pulses(pulse_sent_times, pulse_received_times, unsynched_timestamps):
+    """
+
+    Parameters
+    ----------
+    pulse_sent_times: array-like
+        Times that pulses were sent from secondary acquisition system
+    pulse_received_times: array-like
+        Times that pulses were received by primary acquisition system
+    unsynched_timestamps: array-like
+        Timestamps from the secondary acquisition system that need to be synchronized with the primary system
+
+    Returns
+    -------
+    array-like
+
+    """
+    assert len(pulse_sent_times) == len(pulse_received_times), "child and parent have different number of pulses."
+
+    child_idxs = np.searchsorted(unsynched_timestamps, pulse_sent_times)
+    interval_ratios = np.diff(pulse_received_times) / np.diff(pulse_sent_times)
+
+    synched_timestamps = np.array([])
+    for i in np.arange(len(pulse_sent_times)-1):
+        interval_timestamps = (unsynched_timestamps[child_idxs[i]:child_idxs[i+1]] - pulse_sent_times[i]) * \
+                              interval_ratios[i] + pulse_received_times[i]
+        synched_timestamps = np.hstack((synched_timestamps, interval_timestamps))
+
+    return synched_timestamps
+
