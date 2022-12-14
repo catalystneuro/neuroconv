1fbf8e16f738b19599f810e8eadcc3e53a690872
bendichter <ben.dichter@gmail.com>
2022-12-14 13:34:54 -0600

    get optional args of convert_df_to_time_interval from metadata file

--- src/neuroconv/datainterfaces/text/timeintervalsinterface.py
+++ src/neuroconv/datainterfaces/text/timeintervalsinterface.py
@@ -14,9 +14,10 @@ from ...tools.nwb_helpers import make_or_load_nwbfile
 
 def convert_df_to_time_intervals(
     df: pd.DataFrame,
-    name: str,
+    name: str = "trials",
     description: Optional[str] = None,
     column_name_mapping: Dict[str, str] = None,
+    column_descriptions: Dict[str, str] = None,
 ):
     if column_name_mapping is not None:
         df.rename(columns=column_name_mapping, inplace=True)
@@ -29,7 +30,7 @@ def convert_df_to_time_intervals(
         df["stop_time"] = np.r_[df["start_time"][1:].to_numpy(), np.nan]
     for col in df:
         if col not in ("start_time", "stop_time"):
-            time_intervals.add_column(col, col)
+            time_intervals.add_column(col, column_descriptions.get(col, col))
     for i, row in df.iterrows():
         time_intervals.add_row(row.to_dict())
 
@@ -42,15 +43,25 @@ class TimeIntervalsInterface(BaseDataInterface):
         file_path: FilePathType,
         read_kwargs: Optional[dict] = None,
         verbose: bool = True,
-        name: str = "trials",
-        description: Optional[str] = None,
     ):
         read_kwargs = read_kwargs or dict()
         super().__init__(file_path=file_path)
         self.verbose = verbose
 
         self.df = self._read_file(file_path, **read_kwargs)
-        self.time_intervals = convert_df_to_time_intervals(self.df, name=name, description=description)
+
+    def get_metadata(self):
+        return dict(
+            TimeIntervals=dict(
+                trials=dict(
+                    name="trials",
+                    description="trials",
+                )
+            )
+        )
+
+    def get_metadata_schema(self):
+        pass  # todo
 
     def run_conversion(
         self,
@@ -58,11 +69,13 @@ class TimeIntervalsInterface(BaseDataInterface):
         nwbfile: Optional[NWBFile] = None,
         metadata: Optional[dict] = None,
         overwrite: bool = False,
+        tag="trials",
         **conversion_options,
     ):
         with make_or_load_nwbfile(
             nwbfile_path=nwbfile_path, nwbfile=nwbfile, metadata=metadata, overwrite=overwrite, verbose=self.verbose
         ) as nwbfile_out:
+            self.time_intervals = convert_df_to_time_intervals(self.df, **metadata["TimeIntervals"][tag])
             nwbfile_out.add_time_intervals(self.time_intervals)
 
         return nwbfile_out
