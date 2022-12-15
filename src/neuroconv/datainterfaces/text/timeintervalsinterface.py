d8c43fcf20de6ec2d6c9708178da0bee842f953b
bendichter <ben.dichter@gmail.com>
2022-12-15 16:55:31 -0600

    finish intervals component add tests

--- src/neuroconv/datainterfaces/text/timeintervalsinterface.py
+++ src/neuroconv/datainterfaces/text/timeintervalsinterface.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import os
 
 from typing import Dict, Optional
 
@@ -10,6 +11,7 @@ from pynwb.epoch import TimeIntervals
 from ...basedatainterface import BaseDataInterface
 from ...utils.types import FilePathType
 from ...tools.nwb_helpers import make_or_load_nwbfile
+from ...utils.dict import load_dict_from_file
 
 
 def convert_df_to_time_intervals(
@@ -19,10 +21,44 @@ def convert_df_to_time_intervals(
     column_name_mapping: Dict[str, str] = None,
     column_descriptions: Dict[str, str] = None,
 ):
+    """
+    Convert a dataframe to a TimeIntervals object.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The dataframe to convert.
+    name : str, optional
+        The name of the TimeIntervals object.
+    description : str, optional
+        The description of the TimeIntervals object.
+    column_name_mapping: dict, optional
+        If passed, rename subset of columns from key to value.
+    column_descriptions: dict, optional
+        Keys are the names of the columns (after renaming) and values are the descriptions. If not passed,
+        the names of the columns are used as descriptions.
+
+    Returns
+    -------
+    TimeIntervals
+
+    """
     if column_name_mapping is not None:
         df.rename(columns=column_name_mapping, inplace=True)
+
+    default_column_descriptions = dict(
+        start_time="Start time of epoch, in seconds.",
+        stop_time="Stop time of epoch, in seconds.",
+    )
+
+    if column_descriptions is None:
+        column_descriptions = default_column_descriptions
+    else:
+        column_descriptions = dict(default_column_descriptions, **column_descriptions)
+
     if description is None:
         description = name
+
     time_intervals = TimeIntervals(name, description)
     if "start_time" not in df:
         raise ValueError(f"df must contain a column named 'start_time'. Existing columns: {df.columns.to_list()}")
@@ -38,6 +74,9 @@ def convert_df_to_time_intervals(
 
 
 class TimeIntervalsInterface(BaseDataInterface):
+    """
+    Abstract Interface for time intervals.
+    """
     def __init__(
         self,
         file_path: FilePathType,
@@ -49,6 +88,7 @@ class TimeIntervalsInterface(BaseDataInterface):
         self.verbose = verbose
 
         self.df = self._read_file(file_path, **read_kwargs)
+        self.time_intervals = None
 
     def get_metadata(self):
         return dict(
@@ -61,21 +101,28 @@ class TimeIntervalsInterface(BaseDataInterface):
         )
 
     def get_metadata_schema(self):
-        pass  # todo
+        fpath = os.path.join(os.path.split(__file__)[0], "timeintervals.schema.json")
+        return load_dict_from_file(fpath)
 
     def run_conversion(
         self,
-        nwbfile_path: Optional[str] = None,
+        nwbfile_path: Optional[FilePathType] = None,
         nwbfile: Optional[NWBFile] = None,
         metadata: Optional[dict] = None,
         overwrite: bool = False,
         tag="trials",
-        **conversion_options,
+        column_name_mapping: Dict[str, str] = None,
+        column_descriptions: Dict[str, str] = None,
     ):
         with make_or_load_nwbfile(
             nwbfile_path=nwbfile_path, nwbfile=nwbfile, metadata=metadata, overwrite=overwrite, verbose=self.verbose
         ) as nwbfile_out:
-            self.time_intervals = convert_df_to_time_intervals(self.df, **metadata["TimeIntervals"][tag])
+            self.time_intervals = convert_df_to_time_intervals(
+                self.df,
+                column_name_mapping=column_name_mapping,
+                column_descriptions=column_descriptions,
+                **metadata["TimeIntervals"][tag],
+            )
             nwbfile_out.add_time_intervals(self.time_intervals)
 
         return nwbfile_out
---
+++ tests/test_text/test_time_intervals.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+import os
+
+import pandas as pd
+
+from neuroconv.datainterfaces import ExcelTimeIntervalsInterface, CsvTimeIntervalsInterface
+from neuroconv.datainterfaces.text.timeintervalsinterface import convert_df_to_time_intervals
+
+
+trials_xls_path = os.path.join(os.path.dirname(__file__), 'trials.xlsx')
+trials_csv_path = os.path.join(os.path.dirname(__file__), 'trials.csv')
+
+
+def test_convert_df_to_time_intervals():
+    df = pd.read_excel(trials_xls_path)
+    time_intervals = convert_df_to_time_intervals(df)
+
+
+def test_convert_df_to_time_intervals_colname_mapping():
+    df = pd.read_excel(trials_xls_path)
+    time_intervals = convert_df_to_time_intervals(df, column_name_mapping=dict(condition="cond"))
+    assert time_intervals.colnames == ('start_time', 'stop_time', 'cond')
+
+
+def test_convert_df_to_time_intervals_():
+    df = pd.read_excel(trials_xls_path)
+    time_intervals = convert_df_to_time_intervals(
+        df, column_descriptions=dict(condition="This is a custom description")
+    )
+    assert time_intervals["condition"].description == "This is a custom description"
+
+
+def test_excel_time_intervals():
+    interface = ExcelTimeIntervalsInterface(trials_xls_path)
+    metadata = interface.get_metadata()
+    metadata["NWBFile"] = dict(session_start_time=datetime.now().astimezone())
+    nwbfile = interface.run_conversion(metadata=metadata)
+    assert nwbfile.intervals["trials"].colnames == ('start_time', 'stop_time', 'condition')
+
+
+def test_excel_column_name_mapping():
+    interface = ExcelTimeIntervalsInterface(trials_xls_path)
+    metadata = interface.get_metadata()
+    metadata["NWBFile"] = dict(session_start_time=datetime.now().astimezone())
+    nwbfile = interface.run_conversion(metadata=metadata, column_name_mapping=dict(condition="cond"))
+    assert nwbfile.intervals["trials"].colnames == ('start_time', 'stop_time', 'cond')
+
+
+def test_csv():
+    interface = CsvTimeIntervalsInterface(trials_csv_path)
+    metadata = interface.get_metadata()
+    metadata["NWBFile"] = dict(session_start_time=datetime.now().astimezone())
+    nwbfile = interface.run_conversion(metadata=metadata)
+    assert nwbfile.intervals["trials"]
