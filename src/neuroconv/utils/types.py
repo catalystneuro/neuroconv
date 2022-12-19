44e16706c4e241b78e14b4102240a2c20dbc42aa
bendichter <ben.dichter@gmail.com>
2022-12-19 10:18:31 -0600

    change FilePathType and FolderPathType from TypeVar to Union

--- src/neuroconv/utils/types.py
+++ src/neuroconv/utils/types.py
@@ -1,12 +1,11 @@
 """Authors: Luiz Tauffer, Cody Baker, Saksham Sharda and Ben Dichter."""
 from pathlib import Path
 from typing import Optional, Union
-from typing import TypeVar
 
 import numpy as np
 
-FilePathType = TypeVar("FilePathType", str, Path)
-FolderPathType = TypeVar("FolderPathType", str, Path)
+FilePathType = Union[str, Path]
+FolderPathType = Union[str, Path]
 OptionalFilePathType = Optional[FilePathType]
 OptionalFolderPathType = Optional[FolderPathType]
 ArrayType = Union[list, np.ndarray]
