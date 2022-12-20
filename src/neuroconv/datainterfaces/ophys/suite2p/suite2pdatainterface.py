0b0fef1843e0168a0a8c4464224c6ec28fd51f59
bendichter <ben.dichter@gmail.com>
2022-12-20 16:47:10 -0600

    improve numpy docstrings and function signatures throughout. None of these changes should alter any code function.

--- src/neuroconv/datainterfaces/ophys/suite2p/suite2pdatainterface.py
+++ src/neuroconv/datainterfaces/ophys/suite2p/suite2pdatainterface.py
@@ -1,12 +1,21 @@
 from ..basesegmentationextractorinterface import BaseSegmentationExtractorInterface
-from ....utils import FolderPathType, IntType
+from ....utils import FolderPathType
 
 
 class Suite2pSegmentationInterface(BaseSegmentationExtractorInterface):
     """Data interface for Suite2pSegmentationExtractor."""
 
     def __init__(
-        self, folder_path: FolderPathType, combined: bool = False, plane_no: IntType = 0, verbose: bool = True
+        self, folder_path: FolderPathType, combined: bool = False, plane_no: int = 0, verbose: bool = True
     ):
+        """
+
+        Parameters
+        ----------
+        folder_path: FolderPathType
+        combined: bool, default: False
+        plane_no: int, default: 0
+        verbose: bool, default: True
+        """
         super().__init__(folder_path=folder_path, combined=combined, plane_no=plane_no)
         self.verbose = verbose
