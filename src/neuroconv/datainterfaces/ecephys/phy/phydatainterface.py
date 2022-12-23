c7ce3f6addc63e8cbbe9ad6a16ff1411e3bbf4b5
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-12-23 11:06:11 -0500

    Update phydatainterface.py

--- src/neuroconv/datainterfaces/ecephys/phy/phydatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/phy/phydatainterface.py
@@ -1,5 +1,6 @@
 """Authors: Heberto Mayorquin, Cody Baker."""
 from typing import Optional
+from warnings import warn
 
 from ..basesortingextractorinterface import BaseSortingExtractorInterface
 from ....utils import FolderPathType
