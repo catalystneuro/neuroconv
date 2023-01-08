d322b13347d00c5b2143f58fe3ed5499b0ad343d
CodyCBakerPhD <codycbakerphd@gmail.com>
2023-01-08 18:01:14 -0500

    PR suggestions and manual bin control

--- src/neuroconv/tools/signal_processing.py
+++ src/neuroconv/tools/signal_processing.py
@@ -1,42 +1,68 @@
 """Author: Cody Baker."""
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy as np
 
 
-def parse_rising_and_falling_frames_from_ttl(trace: np.ndarray, num_bins: int = 2) -> Tuple[np.ndarray, np.ndarray]:
+def _get_default_bins(trace: np.ndarray) -> np.ndarray:
+    trace_max = float(np.max(trace))  # cast to Python floats during calculation to avoid overflows
+    trace_min = float(np.min(trace))
+    midpoint = (trace_max - trace_min) / 2
+
+    eps = 1 if np.issubdtype(trace.dtype, np.integer) else np.finfo(float).eps * 10
+    bins = np.array([trace_min - eps, midpoint, trace_max + eps])  # +/- buffer on the boundaries to remove edge effects
+    return bins
+
+
+def get_rising_frames_from_ttl(trace: np.ndarray, bins: Optional[np.ndarray] = None) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Parse the frame indices for rising and falling events in a TTL pulse.
+    Return the frame indices for rising events in a TTL pulse.
 
     Parameters
     ----------
     trace: numpy.ndarray
         A TTL pulse.
-    num_bins: int
-        When digitizing the signal to clean up signal noise, specify the level of refinement in terms of the number
-        of bins incurred from a linear interval partition from the minimum to the maximum trace values.
-        The default is 2, signifying a clear on/off signal from a baseline value to a target value.
+    bins: numpy.ndarray, optional
+        The edges of bins to use for digitization.
+        The default is two bins set to [min(trace), midpoint(trace), max(trace)].
 
     Returns
     -------
     rising_frames: numpy.ndarray
         The frame indices of rising events.
-    falling_frames: numpy.ndarray
-        The frame indices of falling events.
     """
-    eps = 1 if np.issubdtype(trace.dtype, np.integer) else np.finfo(float).eps
-    binned_states = np.digitize(
-        x=trace,
-        bins=np.linspace(
-            start=np.min(trace) - eps,  # Must go slightly beyond min/max to prevent binning occurences at the boundary
-            stop=np.max(trace) + eps,
-            num=num_bins + 1,  # 'num' in np.linspace is actually the number of partitions; +1 to translate to # bins
-        ),
-    ).astype("int8")
+    bins = bins or _get_default_bins(trace=trace)
 
+    binned_states = np.digitize(x=trace, bins=bins).astype("int8")
     diff_binned_states = np.diff(binned_states, axis=0)
 
     rising_frames = np.where(diff_binned_states > 0)[0]
+
+    return rising_frames
+
+
+def get_falling_frames_from_ttl(trace: np.ndarray, bins: Optional[np.ndarray] = None) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Return the frame indices for falling events in a TTL pulse.
+
+    Parameters
+    ----------
+    trace: numpy.ndarray
+        A TTL pulse.
+    bins: numpy.ndarray, optional
+        The edges of bins to use for digitization.
+        The default is two bins set to [min(trace), midpoint(trace), max(trace)].
+
+    Returns
+    -------
+    falling_frames: numpy.ndarray
+        The frame indices of falling events.
+    """
+    bins = bins or _get_default_bins(trace=trace)
+
+    binned_states = np.digitize(x=trace, bins=bins).astype("int8")
+    diff_binned_states = np.diff(binned_states, axis=0)
+
     falling_frames = np.where(diff_binned_states < 0)[0]
 
-    return rising_frames, falling_frames
+    return falling_frames
