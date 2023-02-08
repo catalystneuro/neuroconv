1220f2ebb71ec22ecd2bf9206fb90a7596d56684
weiglszonja <wszonja@gmail.com>
2023-02-08 14:44:25 +0100

    infer stream_id from header when stream_id and stream_name is not specified

--- src/neuroconv/datainterfaces/ecephys/openephys/openephyslegacydatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/openephys/openephyslegacydatainterface.py
@@ -1,6 +1,8 @@
 """Authors: Szonja Weigl, Cody Baker."""
 from typing import Optional
+from warnings import warn
 
+from neo.rawio import OpenEphysRawIO
 from spikeinterface.extractors import OpenEphysLegacyRecordingExtractor
 
 from ..baserecordingextractorinterface import BaseRecordingExtractorInterface
@@ -28,11 +30,11 @@ class OpenEphysLegacyRecordingInterface(BaseRecordingExtractorInterface):
     def __init__(
         self,
         folder_path: FolderPathType,
-        stream_id: Optional[str] = "CH",
+        stream_id: Optional[str] = None,
         stream_name: Optional[str] = None,
         block_index: Optional[int] = 0,
         all_annotations: Optional[bool] = False,
-        stub_test: bool = True,
+        stub_test: bool = False,
         verbose: bool = True,
     ):
         """
@@ -43,8 +45,9 @@ class OpenEphysLegacyRecordingInterface(BaseRecordingExtractorInterface):
         ----------
         folder_path : FolderPathType
             Path to OpenEphys directory.
-        stream_id : str, default: "CH"
+        stream_id : str, default: None
             The identifier of the recording stream (e.g. "CH" for channel data).
+            When the recording stream is not specified it uses the first recording stream.
         stream_name : str, default: None
         block_index : int, default: 0
         all_annotations : bool, default: False
@@ -52,6 +55,12 @@ class OpenEphysLegacyRecordingInterface(BaseRecordingExtractorInterface):
         verbose : bool, default: True
         """
         self.RX = OpenEphysLegacyRecordingExtractor
+
+        self.folder_path = folder_path
+        if stream_name is None and stream_id is None:
+            stream_channels = self._get_stream_channels()
+            stream_id = list(stream_channels["id"])[0]
+
         super().__init__(
             folder_path=folder_path,
             stream_id=stream_id,
@@ -63,6 +72,13 @@ class OpenEphysLegacyRecordingInterface(BaseRecordingExtractorInterface):
 
         if stub_test:
             self.subset_channels = [0, 1]
+
+    def _get_stream_channels(self):
+        """Returns the name and identifier of signal streams for this folder."""
+        reader = OpenEphysRawIO(self.folder_path)
+        reader.parse_header()
+
+        return reader.header["signal_streams"]
 
     def get_metadata(self):
         """Auto-fill as much of the metadata as possible. Must comply with metadata schema."""
