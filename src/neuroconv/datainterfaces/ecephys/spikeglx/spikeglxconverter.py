ada1c975b8fc3fdbc377ded0ad5a8c895cf919d1
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2023-02-10 21:30:44 +0000

    some debugs

--- src/neuroconv/datainterfaces/ecephys/spikeglx/spikeglxconverter.py
+++ src/neuroconv/datainterfaces/ecephys/spikeglx/spikeglxconverter.py
@@ -21,12 +21,17 @@ class SpikeGLXConverter(ConverterPipe):
         source_schema["properties"]["folder_path"]["description"] = "Path to the folder containing SpikeGLX streams."
         return source_schema
 
+    @classmethod
+    def get_streams(cls, folder_path: DirectoryPath) -> List[str]:
+        from spikeinterface.extractors import SpikeGLXRecordingExtractor
+
+        available_streams = SpikeGLXRecordingExtractor.get_streams(folder_path=folder_path)[0]
+
     def __init__(
         self,
         folder_path: DirectoryPath,
-        streams: List[str] = False,  # List[Outer[Literal["AP"], Literal["LF"], Literal["NIDQ"]]]
-        #  'Outer' is short-hand for any combination of those literals
-        # TODO think about multiple probe format
+        streams: Optional[List[str]] = None,
+        verbose: bool = False,
     ):
         """
         Read all data from multiple streams stored in the SpikeGLX format.
@@ -38,36 +43,40 @@ class SpikeGLXConverter(ConverterPipe):
 
         Parameters
         ----------
-        file_path : FilePathType
-            Path to .nidq.bin file.
-        stub_test : bool, default: False
-            Whether to shorten file for testing purposes.
-        verbose : bool, default: True
+        folder_path : DirectoryPath
+            Path to folder containing the NIDQ stream and subfolders containing each IMEC stream.
+        streams : list of strings, optional
+            A specific list of streams you wish to load.
+            To see which streams are available, run `SpikeGLXConverter.get_streams(folder_path="path/to/spikeglx")`.
+            By default, all available streams are loaded.
+        verbose : bool, default: False
             Whether to output verbose text.
-        load_sync_channel : bool, default: False
-            Whether or not to load the last channel in the stream, which is typically used for synchronization.
-            If True, then the probe is not loaded.
         """
-        from spikeinterface.extractors import SpikeGLXRecordingExtractor
-
-        available_streams = SpikeGLXRecordingExtractor.get_streams(
-            folder_path=folder_path
-        )  # TODO: wish this was a SI class method...
-
         folder_path = Path(folder_path)
+
+        available_streams = self.get_streams(folder_path=folder_path)
+        streams = streams or available_streams
+
         data_interfaces = dict()
-        for stream in available_streams:
-            file_path = folder_path / f"{folder_path.stem[:-5]}.imec0.{stream}.bin"  # imagining that stream="ap", "lf"
-            # Also might need to consider case where file stem doesn't match folder stem (not typical SpikeGLX though)
+        for stream in streams:
             if "ap" in stream:
+                probe_name = stream[:5]
+                file_path = (
+                    folder_path / f"{folder_path.stem}_{probe_name}" / f"{folder_path.stem}_t0.{probe_name}.ap.bin"
+                )
                 interface = SpikeGLXRecordingInterface(file_path=file_path)
             if "lf" in stream:
+                probe_name = stream[:5]
+                file_path = (
+                    folder_path / f"{folder_path.stem}_{probe_name}" / f"{folder_path.stem}_t0.{probe_name}.ap.bin"
+                )
                 interface = SpikeGLXLFPInterface(file_path=file_path)
             if "nidq" in stream:
+                file_path = folder_path / f"{folder_path.stem}_t0.nidq.bin"
                 interface = SpikeGLXNIDQInterface(file_path=file_path)
             data_interfaces.update({stream: interface})
 
-        self.__init__(data_interfaces=data_interfaces)
+        super().__init__(data_interfaces=data_interfaces, verbose=verbose)
 
     def run_conversion(
         self,
@@ -75,13 +84,10 @@ class SpikeGLXConverter(ConverterPipe):
         nwbfile: Optional[NWBFile] = None,
         metadata: Optional[dict] = None,
         overwrite: bool = False,
-        # conversion_options: Optional[dict] = None,  # This is the only thing that might get complicated
-        # but it will require the same complications no matter what approach we take
     ):
         if metadata is None:
             metadata = self.get_metadata()
         self.validate_metadata(metadata=metadata)
-        # self.validate_conversion_options(conversion_options=conversion_options_to_run)
 
         with make_or_load_nwbfile(
             nwbfile_path=nwbfile_path,
@@ -91,8 +97,4 @@ class SpikeGLXConverter(ConverterPipe):
             verbose=self.verbose,
         ) as nwbfile_out:
             for interface_name, data_interface in self.data_interface_objects.items():
-                data_interface.run_conversion(
-                    nwbfile=nwbfile_out,
-                    metadata=metadata,
-                    # , **conversion_options_to_run.get(interface_name, dict())
-                )
+                data_interface.run_conversion(nwbfile=nwbfile_out, metadata=metadata)
