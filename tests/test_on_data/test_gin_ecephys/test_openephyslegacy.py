9c7fc56ec3f8bfec9aefd05c963ac99f6092f7cc
weiglszonja <wszonja@gmail.com>
2023-02-17 16:35:56 +0100

    add classmethod that returns the available streams in data

--- tests/test_on_data/test_gin_ecephys/test_openephyslegacy.py
+++ tests/test_on_data/test_gin_ecephys/test_openephyslegacy.py
@@ -1,4 +1,4 @@
-import unittest
+from hdmf.testing import TestCase
 
 from neuroconv.datainterfaces.ecephys.openephys.openephyslegacydatainterface import OpenEphysLegacyRecordingInterface
 
@@ -7,7 +7,7 @@ from ..setup_paths import ECEPHY_DATA_PATH
 OPENEPHYS_PATH = ECEPHY_DATA_PATH / "openephys"
 
 
-class TestOpenEphysLegacyConversions(unittest.TestCase):
+class TestOpenEphysLegacyConversions(TestCase):
     def setUp(self) -> None:
         self.folder_path = OPENEPHYS_PATH / "OpenEphys_SampleData_1"
         self.interface = OpenEphysLegacyRecordingInterface(folder_path=self.folder_path)
@@ -16,6 +16,13 @@ class TestOpenEphysLegacyConversions(unittest.TestCase):
         stream_names = self.interface.get_stream_names(folder_path=self.folder_path)
         self.assertEqual(stream_names, ["Signals CH"])
         self.assertEqual(self.interface.source_data["stream_name"], "Signals CH")
+
+    def test_openephyslegacy_raises(self):
+        with self.assertRaisesWith(
+            AssertionError,
+            "The selected stream 'AUX' is not in the available streams '['Signals CH']'!",
+        ):
+            OpenEphysLegacyRecordingInterface(folder_path=self.folder_path, stream_name="AUX")
 
     def test_openephyslegacy_source_schema(self):
         schema = self.interface.get_source_schema()
