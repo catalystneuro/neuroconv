fc46fb2723da754f13954598a0cf3b1d87fd4287
bendichter <ben.dichter@gmail.com>
2023-02-22 15:30:36 -0300

    simplify fetching of Extractor object

--- src/neuroconv/baseextractorinterface.py
+++ src/neuroconv/baseextractorinterface.py
@@ -1,33 +1,14 @@
 """Abstract class defining the structure of all Extractor-based Interfaces."""
+from abc import ABC
 from typing import Optional
 
 from .basedatainterface import BaseDataInterface
 from .tools import get_package
 
 
-class _LazyExtractorImport(type(BaseDataInterface), type):
-    def __getattribute__(self, name):
-        if name == "Extractor" and super().__getattribute__("Extractor") is None:
-            extractor_module = get_package(package_name=super().__getattribute__("ExtractorModuleName"))
-            extractor = getattr(
-                extractor_module,
-                super().__getattribute__("ExtractorName") or self.__name__.replace("Interface", "Extractor"),
-            )
-            return extractor
-        return super().__getattribute__(name)
-
-
-class BaseExtractorInterface(BaseDataInterface, metaclass=_LazyExtractorImport):
+class BaseExtractorInterface(BaseDataInterface, ABC):
     """
     Abstract class defining the structure of all Extractor-based Interfaces.
-
-    Harnesses an override of the class-level __getattribute__ to perform on-demand imports of the extractor class.
-    Since the type of every class is itself a 'type' (a builtin object), direct overrides are not possible.
-    Hence we use a metaclass whose parent is 'type' to act as an intermediary with the builtin.
-    This metaclass also requires a mix-in with the parent ABC class since the ABCMeta causes conflicts otherwise.
-
-    However, the __getattribute__ override is only a 'getter' with respect to the property; setting it in instances
-    of the class is performed by a slight injection into the __new__ of the base class.
     """
 
     # Manually override any of these attributes in a subclass if needed.
@@ -36,6 +17,16 @@ class BaseExtractorInterface(BaseDataInterface, metaclass=_LazyExtractorImport):
     ExtractorName: Optional[str] = None  # Defaults to __name__.replace("Interface", "Extractor").
     Extractor = None  # Loads dynamically on first access attempt
 
-    def __new__(cls, *args, **kwargs):
-        cls.Extractor = getattr(cls, "Extractor")
-        return object.__new__(cls)
+    @classmethod
+    def get_extractor(cls):
+        extractor = getattr(cls, "Extractor", None)
+        if extractor is not None:
+            return extractor
+        extractor_module = get_package(package_name=cls.ExtractorModuleName)
+        extractor = getattr(
+            extractor_module,
+            cls.ExtractorName or cls.__name__.replace("Interface", "Extractor"),
+            )
+        cls.Extractor = extractor
+        return extractor
+
--- src/neuroconv/datainterfaces/ecephys/baserecordingextractorinterface.py
+++ src/neuroconv/datainterfaces/ecephys/baserecordingextractorinterface.py
@@ -26,7 +26,7 @@ class BaseRecordingExtractorInterface(BaseExtractorInterface):
 
         """
         super().__init__(**source_data)
-        self.recording_extractor = self.Extractor(**source_data)
+        self.recording_extractor = self.get_extractor()(**source_data)
         self.subset_channels = None
         self.verbose = verbose
         self.es_key = None  # For automatic metadata extraction
@@ -82,7 +82,7 @@ class BaseRecordingExtractorInterface(BaseExtractorInterface):
         return metadata
 
     def get_original_timestamps(self) -> np.ndarray:
-        return self.Extractor(**self.source_data).get_times()
+        return self.get_extractor()(**self.source_data).get_times()
 
     def get_timestamps(self) -> np.ndarray:
         return self.recording_extractor.get_times()
