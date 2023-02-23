4c64ab22bae353648a8334544370c17b85be1b07
bendichter <ben.dichter@gmail.com>
2023-02-23 08:55:09 -0300

    rmv probe_file_path from spikegadgetsdatainterface

--- src/neuroconv/datainterfaces/ecephys/spikegadgets/spikegadgetsdatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/spikegadgets/spikegadgetsdatainterface.py
@@ -14,9 +14,6 @@ class SpikeGadgetsRecordingInterface(BaseRecordingExtractorInterface):
     def get_source_schema(cls):
         source_schema = super().get_source_schema()
         source_schema["properties"]["file_path"].update(description="Path to SpikeGadgets (.rec) file.")
-        source_schema["properties"]["probe_file_path"].update(
-            description="Optional path to a probe (.prb) file describing electrode features."
-        )
         return source_schema
 
     def __init__(
@@ -36,9 +33,6 @@ class SpikeGadgetsRecordingInterface(BaseRecordingExtractorInterface):
             The early versions of SpikeGadgets do not automatically record the conversion factor ('gain') of the
             acquisition system. Thus it must be specified either as a single value (if all channels have the same gain)
             or an array of values for each channel.
-        probe_file_path : FilePathType, optional
-            Set channel properties and geometry through a .prb file.
-            See https://github.com/SpikeInterface/probeinterface for more information.
         """
 
         super().__init__(file_path=file_path, stream_id="trodes", verbose=verbose)
