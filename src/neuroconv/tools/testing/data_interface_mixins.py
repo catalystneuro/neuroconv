6b9d9dab216902dd4c578d696f4ee598191931a4
bendichter <ben.dichter@gmail.com>
2023-03-08 12:58:22 -0300

    add modular tests for RecordingExtractorInterface classes

--- src/neuroconv/tools/testing/data_interface_mixins.py
+++ src/neuroconv/tools/testing/data_interface_mixins.py
@@ -6,8 +6,11 @@ from pathlib import Path
 from typing import List, Type, Union
 
 from jsonschema.validators import Draft7Validator, validate
+from spikeinterface.core.testing import check_recordings_equal
+from spikeinterface.extractors import NwbRecordingExtractor
 
 from neuroconv.basedatainterface import BaseDataInterface
+from neuroconv.datainterfaces.ecephys.baserecordingextractorinterface import BaseRecordingExtractorInterface
 from neuroconv.utils import NWBMetaDataEncoder
 
 
@@ -88,3 +91,31 @@ class DataInterfaceTestMixin:
                 nwbfile_path = str(self.save_directory / f"{self.data_interface_cls.__name__}_{num}.nwb")
                 self.run_conversion(nwbfile_path)
                 self.check_read_nwb(nwbfile_path=nwbfile_path)
+
+
+class RecordingExtractorInterfaceTestMixin(DataInterfaceTestMixin):
+    data_interface_cls: Type[BaseRecordingExtractorInterface]
+
+    def check_read_nwb(self, nwbfile_path: str):
+        recording = self.interface.recording_extractor
+
+        electrical_series_name = self.interface.get_metadata()["Ecephys"][self.interface.es_key]["name"]
+
+        if recording.get_num_segments() == 1:
+            # Spikeinterface behavior is to load the electrode table channel_name property as a channel_id
+            nwb_recording = NwbRecordingExtractor(file_path=nwbfile_path, electrical_series_name=electrical_series_name)
+            if "channel_name" in recording.get_property_keys():
+                renamed_channel_ids = recording.get_property("channel_name")
+            else:
+                renamed_channel_ids = recording.get_channel_ids().astype("str")
+            recording = recording.channel_slice(
+                channel_ids=recording.get_channel_ids(), renamed_channel_ids=renamed_channel_ids
+            )
+
+            # Edge case that only occurs in testing, but should eventually be fixed nonetheless
+            # The NwbRecordingExtractor on spikeinterface experiences an issue when duplicated channel_ids
+            # are specified, which occurs during check_recordings_equal when there is only one channel
+            if nwb_recording.get_channel_ids()[0] != nwb_recording.get_channel_ids()[-1]:
+                check_recordings_equal(RX1=recording, RX2=nwb_recording, return_scaled=False)
+                if recording.has_scaled_traces() and nwb_recording.has_scaled_traces():
+                    check_recordings_equal(RX1=recording, RX2=nwb_recording, return_scaled=True)
