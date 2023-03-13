2fad3f1b4cece5bbe1d22b1e54cbc650736c86ff
bendichter <ben.dichter@gmail.com>
2023-03-13 12:57:43 -0300

    minor adjustments to basedatainterface.py

--- src/neuroconv/basedatainterface.py
+++ src/neuroconv/basedatainterface.py
@@ -17,7 +17,7 @@ class BaseDataInterface(ABC):
         return get_schema_from_method_signature(cls.__init__, exclude=["source_data"])
 
     def __init__(self, **source_data):
-        self.source_data: dict = source_data
+        self.source_data = source_data
 
     def get_conversion_options_schema(self):
         """Infer the JSON schema for the conversion options from the method signature (annotation typing)."""
@@ -144,18 +144,15 @@ class BaseDataInterface(ABC):
 
         Parameters
         ----------
-        nwbfile_path: FilePathType
+        nwbfile_path : FilePathType
             Path for where to write or load (if overwrite=False) the NWBFile.
             If specified, the context will always write to this location.
-        nwbfile: NWBFile, optional
+        nwbfile : NWBFile, optional
             An in-memory NWBFile object to write to the location.
-        metadata: dict, optional
+        metadata : dict, optional
             Metadata dictionary with information used to create the NWBFile when one does not exist or overwrite=True.
-        overwrite: bool, optional
-            Whether or not to overwrite the NWBFile if one exists at the nwbfile_path.
+        overwrite : bool, default: False
+            Whether to overwrite the NWBFile if one exists at the nwbfile_path.
             The default is False (append mode).
-        verbose: bool, optional
-            If 'nwbfile_path' is specified, informs user after a successful write operation.
-            The default is True.
         """
         raise NotImplementedError("The run_conversion method for this DataInterface has not been defined!")
