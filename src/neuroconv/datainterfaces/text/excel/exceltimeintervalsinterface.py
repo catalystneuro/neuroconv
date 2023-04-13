256e590af101c64e1eb87489485c5634ebfc9e43
bendichter <ben.dichter@gmail.com>
2023-04-13 09:26:01 -0400

    improvements to excel interface

--- src/neuroconv/datainterfaces/text/excel/exceltimeintervalsinterface.py
+++ src/neuroconv/datainterfaces/text/excel/exceltimeintervalsinterface.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 
 from ..timeintervalsinterface import TimeIntervalsInterface
@@ -6,6 +8,28 @@ from ....utils.types import FilePathType
 
 class ExcelTimeIntervalsInterface(TimeIntervalsInterface):
     """Interface for adding data from an Excel file to NWB as a TimeIntervals object"""
+
+    def __init__(
+        self,
+        file_path: FilePathType,
+        read_kwargs: Optional[dict] = None,
+        verbose: bool = True,
+    ):
+        """
+        Parameters
+        ----------
+        file_path : FilePath
+        read_kwargs : dict, optional
+            Passed to pandas.read_excel()
+        verbose : bool, default: True
+        """
+        read_kwargs = read_kwargs or dict()
+        super().__init__(file_path=file_path)
+        self.verbose = verbose
+
+        self._read_kwargs = read_kwargs
+        self.dataframe = self._read_file(file_path, **read_kwargs)
+        self.time_intervals = None
 
     def _read_file(self, file_path: FilePathType, **read_kwargs):
         return pd.read_excel(file_path, **read_kwargs)
