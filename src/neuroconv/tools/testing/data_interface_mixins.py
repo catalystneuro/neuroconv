944cf1c14b4f227736f6e364f6e84005a23eb324
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2023-05-09 18:01:47 +0000

    more improperly resolved conflicts

--- src/neuroconv/tools/testing/data_interface_mixins.py
+++ src/neuroconv/tools/testing/data_interface_mixins.py
@@ -317,6 +317,34 @@ class SortingExtractorInterfaceTestMixin(DataInterfaceTestMixin):
         self.check_align_starting_time_internal()
         self.check_align_starting_time_external()
 
+    def check_align_starting_time_internal(self):
+        fresh_interface = self.data_interface_cls(**self.test_kwargs)
+
+        original_t_starts = [
+            sorting_segment._t_start for sorting_segment in fresh_interface.sorting_extractor._sorting_segments
+        ]
+
+        starting_time = 1.23
+        fresh_interface.align_starting_time(starting_time=starting_time)
+
+        aligned_starting_times = [
+            sorting_segment._t_start for sorting_segment in fresh_interface.sorting_extractor._sorting_segments
+        ]
+        expected_starting_times = [starting_time + original_t_start for original_t_start in original_t_starts]
+        self.assertListEqual(list1=aligned_starting_times, list2=expected_starting_times)
+
+    def check_align_starting_time_external(self):
+        # normally this uses the 'original' timestamps, but SortingInterfaces don't have those so use fresh instead
+        fresh_interface = self.data_interface_cls(**self.test_kwargs)
+
+        recording_interface = self.associated_recording_cls(**self.associated_recording_kwargs)
+        fresh_interface.register_recording(recording_interface=recording_interface)
+
+        unaligned_timestamps = fresh_interface.get_timestamps()
+
+        aligned_timestamps = unaligned_timestamps + 1.23 + np.random.random(size=unaligned_timestamps.shape)
+        fresh_interface.align_timestamps(aligned_timestamps=aligned_timestamps)
+
 
 class AudioInterfaceTestMixin(DataInterfaceTestMixin):
     def check_read_nwb(self, nwbfile_path: str):
