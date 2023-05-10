9453be724e66494e8dc9ea446635ddc24da35ee3
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-05-10 11:08:59 -0400

    fix name

--- src/neuroconv/__init__.py
+++ src/neuroconv/__init__.py
@@ -1,5 +1,5 @@
 from .basedatainterface import BaseDataInterface
-from .baseextractorsinterface import BaseExtractorInterface
+from .baseextractorinterface import BaseExtractorInterface
 from .basetemporalalignmentinterface import BaseTemporalAlignmentInterface
 from .nwbconverter import ConverterPipe, NWBConverter
 from .tools.yaml_conversion_specification import run_conversion_from_yaml
