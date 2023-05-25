f19f4110023e67055d93a3f0e9badfd4d108985d
felixp8 <felp8484@gmail.com>
2023-05-25 15:37:15 -0400

    refactor demo generator and add test

--- tests/test_minimal/test_tools/test_expand_paths.py
+++ tests/test_minimal/test_tools/test_expand_paths.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from pathlib import Path
 
 from neuroconv.tools import LocalPathExpander
+from neuroconv.tools.testing import generate_path_expander_demo_ibl
 
 
 def test_expand_paths(tmpdir):
@@ -98,3 +99,58 @@ def test_expand_paths(tmpdir):
     for x in string_directory_out:
         assert x in expected
     assert len(string_directory_out) == len(expected)
+
+
+def test_expand_paths_ibl(tmpdir):
+    expander = LocalPathExpander()
+
+    # set up IBL Steinmetz video file structure
+    generate_path_expander_demo_ibl(tmpdir)
+    base_directory = Path(tmpdir)
+
+    # NOTE: empty brackets used b/c for some reason NR_0021/2022-06-30/ subdir is 002/ instead of 001/
+    out = expander.expand_paths(
+        dict(
+            ibl_video_file=dict(
+                base_directory=base_directory,
+                file_path="steinmetzlab/Subjects/{subject_id}/{session_start_time:%Y-%m-%d}/{}/raw_video_data/_iblrig_leftCamera.raw.{session_id}.mp4",
+            ),
+            ibl_video_directory=dict(
+                base_directory=base_directory,
+                folder_path="steinmetzlab/Subjects/{subject_id}/{session_start_time:%Y-%m-%d}/{}/raw_video_data",
+            ),
+        ),
+    )
+
+    # build expected output with glob and manual parsing
+    expected = []
+    for file_path in base_directory.glob("steinmetzlab/Subjects/*/*/*/raw_video_data/_iblrig_leftCamera.raw.*.mp4"):
+        subject_id = file_path.parts[-5]
+        session_start_time = datetime.strptime(file_path.parts[-4], "%Y-%m-%d")
+        session_id = file_path.parts[-1].split('.')[-2]
+        expected.append({
+            "source_data": {
+                "ibl_video_file": {"file_path": str(file_path)},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": session_id, "session_start_time": session_start_time},
+                "Subject": {"subject_id": subject_id},
+            },
+        })
+    for folder_path in base_directory.glob("steinmetzlab/Subjects/*/*/*/raw_video_data/"):
+        subject_id = folder_path.parts[-4]
+        session_start_time = datetime.strptime(folder_path.parts[-3], "%Y-%m-%d")
+        expected.append({
+            "source_data": {
+                "ibl_video_directory": {"folder_path": str(folder_path)},
+            },
+            "metadata": {
+                "NWBFile": {"session_start_time": session_start_time},
+                "Subject": {"subject_id": subject_id},
+            },
+        })
+    
+    # test results
+    for x in out:
+        assert x in expected
+    assert len(out) == len(expected)
