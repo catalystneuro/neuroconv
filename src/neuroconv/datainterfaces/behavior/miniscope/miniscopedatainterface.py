b746f035f6f3c06e0a99cd39dae1ec204746f33a
weiglszonja <wszonja@gmail.com>
2023-06-20 14:48:27 +0200

    change import

--- src/neuroconv/datainterfaces/behavior/miniscope/miniscopedatainterface.py
+++ src/neuroconv/datainterfaces/behavior/miniscope/miniscopedatainterface.py
@@ -20,10 +20,14 @@ class MiniscopeBehaviorInterface(BaseDataInterface):
             The path that points to the main Miniscope folder.
             The movie files are expected to be in sub folders within the main folder.
         """
-        natsort = get_package(package_name="natsort", installation_instructions="pip install natsort")
-        self._ndx_miniscope = get_package(
-            package_name="ndx_miniscope", installation_instructions="pip install ndx-miniscope"
+        from ndx_miniscope.utils import (
+            get_recording_start_times,
+            get_starting_frames,
+            get_timestamps,
+            read_miniscope_config,
         )
+
+        natsort = get_package(package_name="natsort", installation_instructions="pip install natsort")
 
         super().__init__(folder_path=folder_path)
 
@@ -40,16 +44,12 @@ class MiniscopeBehaviorInterface(BaseDataInterface):
         ), f"The configuration files ({configuration_file_name} files) are missing from '{self.folder_path}'."
 
         behavcam_subfolders = list(folder_path.glob(f"*/BehavCam*/"))
-        self._miniscope_config = self._ndx_miniscope.utils.read_miniscope_config(
-            folder_path=str(behavcam_subfolders[0])
-        )
+        self._miniscope_config = read_miniscope_config(folder_path=str(behavcam_subfolders[0]))
 
-        self._recording_start_times = self._ndx_miniscope.utils.get_recording_start_times(folder_path=str(folder_path))
-        self._starting_frames = self._ndx_miniscope.utils.get_starting_frames(folder_path=str(folder_path))
+        self._recording_start_times = get_recording_start_times(folder_path=str(folder_path))
+        self._starting_frames = get_starting_frames(folder_path=str(folder_path))
         assert len(self._starting_frames) == len(self._behav_avi_file_paths)
-        self._timestamps = self._ndx_miniscope.utils.get_timestamps(
-            folder_path=str(folder_path), file_pattern="BehavCam*/timeStamps.csv"
-        )
+        self._timestamps = get_timestamps(folder_path=str(folder_path), file_pattern="BehavCam*/timeStamps.csv")
 
     def get_metadata(self) -> DeepDict:
         metadata = super().get_metadata()
@@ -86,7 +86,9 @@ class MiniscopeBehaviorInterface(BaseDataInterface):
             The metadata for the image series should be stored in metadata["Behavior"]["ImageSeries"].
             The metadata for the device to be linked should be stored in metadata["Behavior"]["Device"].
         """
-        self._ndx_miniscope.utils.add_miniscope_image_series(
+        from ndx_miniscope.utils import add_miniscope_image_series
+
+        add_miniscope_image_series(
             nwbfile=nwbfile,
             metadata=metadata,
             external_files=[str(file_path) for file_path in self._behav_avi_file_paths],
