6dcabdcedd1c8342818e9dc800d1710b0536ceba
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-06-28 18:16:52 +0200

    add tests

--- src/neuroconv/utils/checks.py
+++ src/neuroconv/utils/checks.py
@@ -4,10 +4,21 @@ from typing import Optional
 import numpy as np
 
 
+def is_series_regular(series: np.ndarray) -> bool:
+    sampling_rate = series[1] - series[0]
+    num_samples = len(series)
+    last_sample = series[-1]
+
+    estimated_last_sample = series[0] + (num_samples - 1) * sampling_rate
+    is_regular = last_sample == estimated_last_sample
+    return is_regular
+
+
 def calculate_regular_series_rate(series: np.ndarray, tolerance_decimals: int = 6) -> Optional[Real]:
     """Calculates the rate of a series as the difference between all consecutive points.
     If the difference between all time points are all the same value, then the value of
     rate is a scalar otherwise it is None."""
+
     diff_ts = np.diff(series)
     rounded_diff_ts = diff_ts.round(decimals=tolerance_decimals)
     uniq_diff_ts = np.unique(rounded_diff_ts)
--- tests/test_minimal/test_utils/test_utils.py
+++ tests/test_minimal/test_utils/test_utils.py
@@ -1,6 +1,11 @@
-from neuroconv.utils import calculate_regular_series_rate
+from neuroconv.utils import calculate_regular_series_rate, is_series_regular
 
 
 def test_check_regular_series():
     assert calculate_regular_series_rate(series=[1, 2, 3])
     assert not calculate_regular_series_rate(series=[1, 2, 4])
+
+
+def is_series_regular():
+    assert is_series_regular(series=[1, 2, 3])
+    assert not is_series_regular(series=[1, 2, 4])
