642917bbe3b8e726138ced6732133da2707a5db7
Tuan Pham <42875763+tuanpham96@users.noreply.github.com>
2023-07-25 14:00:53 -0400

    add tests for adding "extras" metadata in expand_path

--- tests/test_minimal/test_tools/test_expand_paths.py
+++ tests/test_minimal/test_tools/test_expand_paths.py
@@ -104,6 +104,150 @@ def test_expand_paths(tmpdir):
     assert len(string_directory_out) == len(expected)
 
 
+def test_expand_paths_with_extras(tmpdir='tmp'):
+    expander = LocalPathExpander()
+
+    # set up directory for parsing
+    base_directory = Path(tmpdir)
+    for area_and_modality in ("V1_eeg", "S1_2p"):
+        for subject_id in ("001", "002"):
+            for session_id, session_start_time in (("101", datetime(2021, 1, 1)), ("102", datetime(2021, 1, 2))):
+                Path.mkdir(base_directory / f"{area_and_modality}" / f"sub-{subject_id}" / f"session_{session_id}", parents=True)
+                (
+                    base_directory / f"{area_and_modality}" / f"sub-{subject_id}" / f"session_{session_id}" / f"{session_start_time:%Y-%m-%d}abc"
+                ).touch()
+                (
+                    base_directory / f"{area_and_modality}" / f"sub-{subject_id}" / f"session_{session_id}" / f"{session_start_time:%Y-%m-%d}xyz"
+                ).touch()
+
+    # run path parsing
+    out = expander.expand_paths(
+        dict(
+            aa=dict(
+                base_directory=base_directory,
+                file_path="{recording_area}_{modality}/sub-{subject_id:3}/session_{session_id:3}/{session_start_time:%Y-%m-%d}abc",
+            ),
+            bb=dict(
+                base_directory=base_directory,
+                file_path="{recording_area}_{modality}/sub-{subject_id:3}/session_{session_id:3}/{session_start_time:%Y-%m-%d}xyz",
+            ),
+        ),
+    )
+
+    expected = [
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "V1_eeg" / "sub-002" / "session_101" / "2021-01-01abc")},
+                "bb": {"file_path": str(base_directory / "V1_eeg" / "sub-002" / "session_101" / "2021-01-01xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "101", "session_start_time": datetime(2021, 1, 1)},
+                "Subject": {"subject_id": "002"},
+                "extras": {"recording_area": "V1", "modality": "eeg"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "V1_eeg" / "sub-002" / "session_102" / "2021-01-02abc")},
+                "bb": {"file_path": str(base_directory / "V1_eeg" / "sub-002" / "session_102" / "2021-01-02xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "102", "session_start_time": datetime(2021, 1, 2)},
+                "Subject": {"subject_id": "002"},
+                "extras": {"recording_area": "V1", "modality": "eeg"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "V1_eeg" / "sub-001" / "session_101" / "2021-01-01abc")},
+                "bb": {"file_path": str(base_directory / "V1_eeg" / "sub-001" / "session_101" / "2021-01-01xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "101", "session_start_time": datetime(2021, 1, 1)},
+                "Subject": {"subject_id": "001"},
+                "extras": {"recording_area": "V1", "modality": "eeg"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "V1_eeg" / "sub-001" / "session_102" / "2021-01-02abc")},
+                "bb": {"file_path": str(base_directory / "V1_eeg" / "sub-001" / "session_102" / "2021-01-02xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "102", "session_start_time": datetime(2021, 1, 2)},
+                "Subject": {"subject_id": "001"},
+                "extras": {"recording_area": "V1", "modality": "eeg"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "S1_2p" / "sub-002" / "session_101" / "2021-01-01abc")},
+                "bb": {"file_path": str(base_directory / "S1_2p" / "sub-002" / "session_101" / "2021-01-01xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "101", "session_start_time": datetime(2021, 1, 1)},
+                "Subject": {"subject_id": "002"},
+                "extras": {"recording_area": "S1", "modality": "2p"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "S1_2p" / "sub-002" / "session_102" / "2021-01-02abc")},
+                "bb": {"file_path": str(base_directory / "S1_2p" / "sub-002" / "session_102" / "2021-01-02xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "102", "session_start_time": datetime(2021, 1, 2)},
+                "Subject": {"subject_id": "002"},
+                "extras": {"recording_area": "S1", "modality": "2p"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "S1_2p" / "sub-001" / "session_101" / "2021-01-01abc")},
+                "bb": {"file_path": str(base_directory / "S1_2p" / "sub-001" / "session_101" / "2021-01-01xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "101", "session_start_time": datetime(2021, 1, 1)},
+                "Subject": {"subject_id": "001"},
+                "extras": {"recording_area": "S1", "modality": "2p"},
+            },
+        },
+        {
+            "source_data": {
+                "aa": {"file_path": str(base_directory / "S1_2p" / "sub-001" / "session_102" / "2021-01-02abc")},
+                "bb": {"file_path": str(base_directory / "S1_2p" / "sub-001" / "session_102" / "2021-01-02xyz")},
+            },
+            "metadata": {
+                "NWBFile": {"session_id": "102", "session_start_time": datetime(2021, 1, 2)},
+                "Subject": {"subject_id": "001"},
+                "extras": {"recording_area": "S1", "modality": "2p"},
+            },
+        },
+    ]
+
+
+    # test results
+    for x in out:
+        assert x in expected
+    assert len(out) == len(expected)
+
+    # test again with string inputs to `base_directory`
+    string_directory_out = expander.expand_paths(
+        dict(
+            aa=dict(
+                base_directory=str(base_directory),
+                file_path="{recording_area}_{modality}/sub-{subject_id:3}/session_{session_id:3}/{session_start_time:%Y-%m-%d}abc",
+            ),
+            bb=dict(
+                base_directory=str(base_directory),
+                file_path="{recording_area}_{modality}/sub-{subject_id:3}/session_{session_id:3}/{session_start_time:%Y-%m-%d}xyz",
+            ),
+        ),
+    )
+    for x in string_directory_out:
+        assert x in expected
+    assert len(string_directory_out) == len(expected)
+
 def test_expand_paths_ibl(tmpdir):
     expander = LocalPathExpander()
 
