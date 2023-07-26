e365eedfca1d7981af1fea5c99f263bf1b49d9c3
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-07-26 11:07:02 -0400

    set more efficient default for mask type

--- src/neuroconv/datainterfaces/ophys/basesegmentationextractorinterface.py
+++ src/neuroconv/datainterfaces/ophys/basesegmentationextractorinterface.py
@@ -79,7 +79,7 @@ class BaseSegmentationExtractorInterface(BaseExtractorInterface):
         stub_frames: int = 100,
         include_roi_centroids: bool = True,
         include_roi_acceptance: bool = True,
-        mask_type: Optional[str] = "image",  # Literal["image", "pixel", "voxel"]
+        mask_type: Optional[str] = "pixel",  # Literal["image", "pixel", "voxel"]
         iterator_options: Optional[dict] = None,
         compression_options: Optional[dict] = None,
     ):
