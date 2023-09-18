85bc9277f3b379d1dcea1c8f1f847d0072b1384a
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-09-17 23:05:54 -0400

    Update __init__.py

--- src/neuroconv/tools/nwb_helpers/__init__.py
+++ src/neuroconv/tools/nwb_helpers/__init__.py
@@ -5,7 +5,7 @@ from ._metadata_and_file_helpers import (
     make_nwbfile_from_metadata,
     make_or_load_nwbfile,
 )
-from ._models._base_models import DatasetConfiguration, DatasetInfo
+from ._models._base_models import DatasetInfo
 from ._models._hdf5_models import (
     AVAILABLE_HDF5_COMPRESSION_METHODS,
     HDF5BackendConfiguration,
@@ -16,6 +16,7 @@ from ._models._zarr_models import (
     ZarrBackendConfiguration,
     ZarrDatasetConfiguration,
 )
+from ._dataset_configuration import get_default_backend_configuration
 
 BACKEND_TO_DATASET_CONFIGURATION = dict(hdf5=HDF5DatasetConfiguration, zarr=ZarrDatasetConfiguration)
 BACKEND_TO_CONFIGURATION = dict(hdf5=HDF5BackendConfiguration, zarr=ZarrBackendConfiguration)
