da2c2f9d9fdb58986008428177f5e91671c7ca4f
CodyCBakerPhD <codycbakerphd@gmail.com>
2023-10-06 03:50:04 -0400

    fix

--- tests/test_minimal/test_tools/test_backend_and_dataset_configuration/test_common_dataset_configuration_model.py
+++ tests/test_minimal/test_tools/test_backend_and_dataset_configuration/test_common_dataset_configuration_model.py
@@ -7,7 +7,7 @@ from neuroconv.tools.nwb_helpers import (
     HDF5DatasetConfiguration,
     ZarrDatasetConfiguration,
 )
-from neuroconv.tools.testing import mock_DatasetInfo, mock_HDF5DatasetConfiguration
+from neuroconv.tools.testing import mock_DatasetInfo, mock_HDF5DatasetConfiguration, mock_ZarrDatasetConfiguration
 
 
 @pytest.mark.parametrize(
@@ -37,7 +37,7 @@ def test_validator_chunk_and_buffer_length_consistency(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(78_125, 64, 1),
             buffer_shape=(1_250_000, 384, 1),
@@ -57,7 +57,7 @@ def test_validator_chunk_shape_nonpositive_elements(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(1, -2),
             buffer_shape=(1_250_000, 384),
@@ -77,7 +77,7 @@ def test_validator_buffer_shape_nonpositive_elements(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(78_125, 64),
             buffer_shape=(78_125, -2),
@@ -97,7 +97,7 @@ def test_validator_chunk_shape_exceeds_buffer_shape(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(78_126, 64),
             buffer_shape=(78_125, 384),
@@ -117,7 +117,7 @@ def test_validator_buffer_shape_exceeds_full_shape(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(78_125, 64),
             buffer_shape=(1_250_000, 385),
@@ -137,7 +137,7 @@ def test_validator_chunk_dimensions_do_not_evenly_divide_buffer(
     dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
 ):
     with pytest.raises(ValueError) as error_info:
-        HDF5DatasetConfiguration(
+        dataset_configuration_class(
             dataset_info=mock_DatasetInfo(),
             chunk_shape=(78_125, 7),
             buffer_shape=(1_250_000, 384),
@@ -151,18 +151,18 @@ def test_validator_chunk_dimensions_do_not_evenly_divide_buffer(
 
 
 @pytest.mark.parametrize(
-    argnames="dataset_configuration_class", argvalues=[HDF5DatasetConfiguration, ZarrDatasetConfiguration]
+    argnames="mock_dataset_configuration", argvalues=[mock_HDF5DatasetConfiguration(), mock_ZarrDatasetConfiguration()]
 )
-def test_mutation_validation(dataset_configuration_class: Union[HDF5DatasetConfiguration, ZarrDatasetConfiguration]):
+def test_mutation_validation(
+    mock_dataset_configuration: Union[mock_HDF5DatasetConfiguration, mock_ZarrDatasetConfiguration]
+):
     """
     Only testing on one dummy case to verify the root validator is triggered.
 
     Trust the rest should follow.
     """
-    hdf5_dataset_configuration = mock_HDF5DatasetConfiguration()
-
     with pytest.raises(ValueError) as error_info:
-        hdf5_dataset_configuration.chunk_shape = (1, -2)
+        mock_dataset_configuration.chunk_shape = (1, -2)
 
     expected_error = (
         "Some dimensions of the chunk_shape=(1, -2) are less than or equal to zero for dataset at "
