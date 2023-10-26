26f8dbe60f641a983870ca67195b5007e14f6cee
Ben Dichter <ben.dichter@gmail.com>
2023-10-26 11:23:55 -0400

    Update src/neuroconv/datainterfaces/behavior/neuralynx/nvt_utils.py
    Co-authored-by: Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>

--- src/neuroconv/datainterfaces/behavior/neuralynx/nvt_utils.py
+++ src/neuroconv/datainterfaces/behavior/neuralynx/nvt_utils.py
@@ -120,7 +120,7 @@ def read_data(filename: str) -> Dict[str, np.ndarray]:
         return {name: records[name].squeeze() for name, *_ in RECORD_FORMAT}
 
 
-def truncate_file(source_filename: FilePathType, dest_filename: str, n_records: int = 10) -> None:
+def truncate_file(source_filename: FilePathType, dest_filename: str, n_records: int = 10) -> None:  # pragma: no cover
     """
     Creates a new .nvt file with the same header and truncated data. Useful for creating
     test files.
