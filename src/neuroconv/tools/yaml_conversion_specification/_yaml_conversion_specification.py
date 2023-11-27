c9a19ebc478843d51c24489fa7d8037599a7aad8
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-11-27 11:03:25 -0500

    Apply suggestions from code review

--- src/neuroconv/tools/yaml_conversion_specification/_yaml_conversion_specification.py
+++ src/neuroconv/tools/yaml_conversion_specification/_yaml_conversion_specification.py
@@ -68,7 +68,7 @@ def run_conversion_from_yaml(
         If False, appends the existing NWBFile at the nwbfile_path location, if save_to_file is True.
     """
     from dandi.organize import create_unique_filenames_from_metadata
-    from dandi.pynwb_utils import _get_pynwb_metadata
+     from dandi.metadata import _get_pynwb_metadata
 
     if data_folder_path is None:
         data_folder_path = Path(specification_file_path).parent
