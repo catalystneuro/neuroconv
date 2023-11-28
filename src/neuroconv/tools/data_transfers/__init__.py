e0053d9cc45e6751276b3d01c1047cbb501b123e
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-11-27 21:28:48 -0500

    Update __init__.py

--- src/neuroconv/tools/data_transfers/__init__.py
+++ src/neuroconv/tools/data_transfers/__init__.py
@@ -5,9 +5,9 @@ from ._globus import get_globus_dataset_content_sizes, transfer_globus_content
 from ._helpers import estimate_total_conversion_runtime
 
 __all__ = [
-    estimate_s3_conversion_cost,
-    automatic_dandi_upload,
-    get_globus_dataset_content_sizes,
-    transfer_globus_content,
-    estimate_total_conversion_runtime,
+    "estimate_s3_conversion_cost",
+    "automatic_dandi_upload",
+    "get_globus_dataset_content_sizes",
+    "transfer_globus_content",
+    "estimate_total_conversion_runtime",
 ]
