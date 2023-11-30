6ee5444bb47fce5dbf6783c55f69a0ed9d132aa4
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-11-30 14:33:33 -0500

    move into other block

--- src/neuroconv/utils/json_schema.py
+++ src/neuroconv/utils/json_schema.py
@@ -106,6 +106,16 @@ def get_schema_from_method_signature(method: Callable, exclude: list = None) -> 
                     args_spec[param_name].update(additionalProperties=True)
             elif hasattr(param.annotation, "__args__"):  # Annotation has __args__ if it was made by typing.Union
                 args = param.annotation.__args__
+                print(param_name)
+                print(args)
+                print(type(args))
+                print(args.__name__)
+                if isinstance(args, str):
+                    args = eval(args)
+                print(param_name)
+                print(args)
+                print(type(args))
+                print(args.__name__)
                 valid_args = [x.__name__ in annotation_json_type_map for x in args]
                 if not any(valid_args):
                     raise ValueError(f"No valid arguments were found in the json type mapping for parameter {param}")
