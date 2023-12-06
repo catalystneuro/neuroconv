49e392a8be42c03974c7eb0d4d84eaeb6ce0c2ac
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-12-06 18:03:18 -0500

    adjust size check to be bound

--- tests/test_ophys/test_tools_roiextractors.py
+++ tests/test_ophys/test_tools_roiextractors.py
@@ -1608,8 +1608,8 @@ class TestAddPhotonSeries(TestCase):
         assert self.two_photon_series_name in acquisition_modules
         data_in_hdfm_data_io = acquisition_modules[self.two_photon_series_name].data
         data_chunk_iterator = data_in_hdfm_data_io.data
-        iterator_chunk_mb = data_chunk_iterator.chunk_mb
-        self.assertEqual(chunk_mb, iterator_chunk_mb)
+        iterator_chunk_mb = math.prod(data_chunk_iterator.chunk_shape) * data_chunk_iterator.dtype.itemsize
+        assert iterator_chunk_mb <= chunk_mb
 
     def test_iterator_options_chunk_shape_is_at_least_one(self):
         """Test that when a small chunk_mb is selected the chunk shape is guaranteed to include at least one frame."""
