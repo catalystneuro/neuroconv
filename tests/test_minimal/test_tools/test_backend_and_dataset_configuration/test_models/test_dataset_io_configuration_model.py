bfe3127c84459cef6e761c84167311cf26d9a671
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-02-21 12:14:36 -0500

    simple fix

--- tests/test_minimal/test_tools/test_backend_and_dataset_configuration/test_models/test_dataset_io_configuration_model.py
+++ tests/test_minimal/test_tools/test_backend_and_dataset_configuration/test_models/test_dataset_io_configuration_model.py
@@ -9,7 +9,7 @@ from neuroconv.tools.testing import mock_DatasetInfo
 def test_get_data_io_kwargs_abstract_error():
     with pytest.raises(TypeError) as error_info:
         DatasetIOConfiguration(dataset_info=mock_DatasetInfo(), chunk_shape=(78_125, 64), buffer_shape=(1_250_000, 384))
-    assert "Can't instantiate abstract class DatasetIOConfiguration with abstract" in str(error_info.value)
+    assert "Can't instantiate abstract class DatasetIOConfiguration" in str(error_info.value)
 
 
 def test_get_data_io_kwargs_not_implemented():
