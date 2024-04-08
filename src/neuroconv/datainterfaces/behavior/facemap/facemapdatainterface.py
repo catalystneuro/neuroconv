2f256782a6ebdd79bcd806cfea8fe7eaa3028c03
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-04-08 16:57:22 +0200

    separate multivideo masks and ROI masks

--- src/neuroconv/datainterfaces/behavior/facemap/facemapdatainterface.py
+++ src/neuroconv/datainterfaces/behavior/facemap/facemapdatainterface.py
@@ -18,7 +18,13 @@ class FacemapInterface(BaseTemporalAlignmentInterface):
 
     keywords = ["eye tracking"]
 
-    def __init__(self, mat_file_path: FilePathType, video_file_path: FilePathType, verbose: bool = True):
+    def __init__(
+        self,
+        mat_file_path: FilePathType,
+        video_file_path: FilePathType,
+        include_multivideo_SVD: bool = True,
+        verbose: bool = True,
+    ):
         """
         Load and prepare data for facemap.
 
@@ -32,6 +38,7 @@ class FacemapInterface(BaseTemporalAlignmentInterface):
             Allows verbose.
         """
         super().__init__(mat_file_path=mat_file_path, video_file_path=video_file_path, verbose=verbose)
+        self.include_multivideo_SVD = include_multivideo_SVD
         self.original_timestamps = None
         self.timestamps = None
 
@@ -88,28 +95,79 @@ class FacemapInterface(BaseTemporalAlignmentInterface):
         from ndx_facemap_motionsvd import MotionSVDMasks, MotionSVDSeries
         from pynwb.core import DynamicTableRegion
 
+        # From documentation
+        # motSVD: cell array of motion SVDs [time x components] (in order: multivideo, ROI1, ROI2, ROI3)
+        # uMotMask: cell array of motion masks [pixels x components]  (in order: multivideo, ROI1, ROI2, ROI3)
+        # motion masks of multivideo are reported as 2D-arrays npixels x components
+        # while ROIs motion masks are reported as 3D-arrays x_pixels x y_pixels x components
+
         with h5py.File(self.source_data["mat_file_path"], "r") as file:
 
             behavior_module = get_module(nwbfile=nwbfile, name="behavior", description="behavioral data")
 
             timestamps = self.get_timestamps()
-            n = 0
-            for series_ref, mask_ref in zip(file["proc"]["motSVD"][:], file["proc"]["uMotMask"][:]):
-                mask_ref = mask_ref[0]
-                series_ref = series_ref[0]
+
+            # store multivideo motion mask and motion series
+            if self.include_multivideo_SVD:
+                # add multivideo mask
+                mask_ref = file["proc"]["uMotMask"][0][0]
+                motion_masks_table = MotionSVDMasks(
+                    name=f"MotionSVDMasksMultivideo",
+                    description=f"motion mask for multivideo",
+                )
+
+                multivideo_mask_ref = file["proc"]["wpix"][0][0]
+                multivideo_mask = file[multivideo_mask_ref]
+                multivideo_mask = multivideo_mask[:]
+                non_zero_multivideo_mask = np.where(multivideo_mask == 1)
+                y_indices, x_indices = non_zero_multivideo_mask
+                top = np.min(y_indices)
+                left = np.min(x_indices)
+                bottom = np.max(y_indices)
+                right = np.max(x_indices)
+                submask = multivideo_mask[top : bottom + 1, left : right + 1]
+                componendt_2d_shape = submask.shape
+
+                for component in file[mask_ref]:
+                    componendt_2d = component.reshape(componendt_2d_shape)
+                    motion_masks_table.add_row(image_mask=componendt_2d.T)
+
+                motion_masks = DynamicTableRegion(
+                    name="motion_masks",
+                    data=list(range(len(file["proc"]["motSVD"][:]))),
+                    description="all the multivideo motion mask",
+                    table=motion_masks_table,
+                )
+
+                series_ref = file["proc"]["motSVD"][0][0]
+                data = np.array(file[series_ref])
+
+                motionsvd_series = MotionSVDSeries(
+                    name=f"MotionSVDSeriesMultivideo",
+                    description=f"SVD components for multivideo",
+                    data=data.T,
+                    motion_masks=motion_masks,
+                    unit="unknown",
+                    timestamps=timestamps,
+                )
+                behavior_module.add(motion_masks_table)
+                behavior_module.add(motionsvd_series)
+
+            # store ROIs motion mask and motion series
+            n = 1
+            for series_ref, mask_ref in zip(file["proc"]["motSVD"][1:][0], file["proc"]["uMotMask"][1:][0]):
 
                 motion_masks_table = MotionSVDMasks(
                     name=f"MotionSVDMasksROI{n}",
                     description=f"motion mask for ROI{n}",
                 )
-
                 for component in file[mask_ref]:
-                    motion_masks_table.add_row(image_mask=component)
+                    motion_masks_table.add_row(image_mask=component.T)
 
                 motion_masks = DynamicTableRegion(
                     name="motion_masks",
                     data=list(range(len(file["proc"]["motSVD"][:]))),
-                    description="all the mask",
+                    description="all the ROIs motion mask",
                     table=motion_masks_table,
                 )
 
