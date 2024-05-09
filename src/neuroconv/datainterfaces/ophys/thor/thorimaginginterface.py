d5d36e88f3fe4f380113945fc779cc17e82cb8b6
Ben Dichter <ben.dichter@gmail.com>
2024-05-09 15:08:37 -0500

    add ThorTiffImagingInterface

---
+++ src/neuroconv/datainterfaces/ophys/thor/thorimaginginterface.py
@@ -0,0 +1,36 @@
+from roiextractors.extractors.thorextractors.thorimagingextractor import ThorTiffImagingExtractor
+
+from neuroconv.datainterfaces.ophys.baseimagingextractorinterface import BaseImagingExtractorInterface
+from neuroconv.utils import FilePathType, DeepDict
+
+
+class ThorTiffImagingInterface(BaseImagingExtractorInterface):
+    """Interface for multi-page TIFF files from the ThorLabs acquisition system."""
+
+    display_name = "ThorLabs TIFF Imaging"
+    Extractor = ThorTiffImagingExtractor
+
+    @classmethod
+    def get_source_schema(cls) -> dict:
+        source_schema = super().get_source_schema()
+        source_schema["properties"]["folder_path"]["description"] = (
+            "Directory that contains the TIFF files and " "Experiment.xml file. "
+        )
+        return source_schema
+
+    def __init__(self, folder_path: FilePathType, verbose: bool = False):
+        """
+        Initialize reading of TIFF file.
+
+        Parameters
+        ----------
+        folder_path : FilePathType
+            Directory that contains the TIFF files and Experiment.xml file.
+        verbose : bool, default: False
+        """
+        super().__init__(folder_path=folder_path, verbose=verbose)
+
+    def get_metadata(self, photon_series_type="TwoPhotonSeries") -> DeepDict:
+        metadata = super().get_metadata(photon_series_type=photon_series_type)
+        metadata["NWBFile"]["session_start_time"] = self.extractor.start_time
+        return metadata
