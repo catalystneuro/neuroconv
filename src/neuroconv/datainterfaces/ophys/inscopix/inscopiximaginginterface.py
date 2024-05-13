6317efe2561c5ef8a420137ca0d3b25420623176
Ben Dichter <ben.dichter@gmail.com>
2024-05-13 14:00:23 -0500

    extract metadata from inscopix

--- src/neuroconv/datainterfaces/ophys/inscopix/inscopiximaginginterface.py
+++ src/neuroconv/datainterfaces/ophys/inscopix/inscopiximaginginterface.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from imaplib import Literal
+
 from ..baseimagingextractorinterface import BaseImagingExtractorInterface
-from ....utils import FilePathType
+from ....utils import FilePathType, DeepDict
 
 
 class InscopixImagingInterface(BaseImagingExtractorInterface):
@@ -19,3 +22,26 @@ class InscopixImagingInterface(BaseImagingExtractorInterface):
         verbose : bool, default: True
         """
         super().__init__(file_path=file_path, verbose=verbose)
+
+    def get_metadata(
+        self, photon_series_type: Literal["OnePhotonSeries", "TwoPhotonSeries"] = "TwoPhotonSeries"
+    ) -> DeepDict:
+        metadata = super().get_metadata(photon_series_type=photon_series_type)
+
+        extra_props = self.imaging_extractor.movie.footer['extraProperties']
+
+        if extra_props['animal']['id']:
+            metadata["Subject"]["subject_id"] = extra_props['animal']['id']
+        if extra_props['animal']['species']:
+            metadata["Subject"]["species"] = extra_props['animal']['species']
+        if extra_props["animal"]["sex"]:
+            metadata["Subject"]["sex"] = extra_props["animal"]["sex"].upper()
+        if extra_props["animal"]["dob"]:
+            metadata["Subject"]["date_of_birth"] = extra_props["animal"]["dob"]
+        if extra_props["animal"]["weight"]:
+            metadata["Subject"]["weight"] = str(extra_props["animal"]["weight"])
+
+        if extra_props["date"]:
+            metadata["NWBFile"]["session_start_time"] = datetime.strptime(extra_props["date"], '%Y-%m-%dT%H:%M:%S.%fZ')
+
+        return metadata
