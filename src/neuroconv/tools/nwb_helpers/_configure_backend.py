0316d234220bd7fe7c79304b5be6ff9c0d404360
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-05-16 08:38:42 -0600

    marginal improvement from cody

--- src/neuroconv/tools/nwb_helpers/_configure_backend.py
+++ src/neuroconv/tools/nwb_helpers/_configure_backend.py
@@ -1,74 +1,98 @@
 """Collection of helper functions related to configuration of datasets dependent on backend."""
 
 import sys
-from typing import Dict, Union
+from typing import Union
 
 from hdmf.common import Data
 from pynwb import NWBFile, TimeSeries
 
+from ._configuration_models._base import BackendConfiguration
 from ._configuration_models._hdf5_backend import HDF5BackendConfiguration
 from ._configuration_models._zarr_backend import ZarrBackendConfiguration
 from ..importing import is_package_installed
 
 
-def _resolve_nwbfile_object_ids(
-    nwbfile: NWBFile, backend_configuration: Union[HDF5BackendConfiguration, ZarrBackendConfiguration]
-) -> Dict[str, str]:
+def _remap_backend_configuration_to_nwbfile(
+    nwbfile: NWBFile, backend_configuration: HDF5BackendConfiguration | ZarrBackendConfiguration
+) -> BackendConfiguration:
     """
-    When a configuration is generated on an equivalent NWBFile elsewhere in memory, the object IDs will not match.
+    Remap an existing backend configuration to a new NWBFile.
 
-    But a mapping can be formed to reconcile the two.
-    This is the case for the NWB GUIDE (since they are separate endpoints in the Flask server).
+    If a backend configuration was created with a different NWBFile instance, then
+    the neurodata objects ids in the configuration might not match the new NWBFile.
 
-    The default behavior forms a simple identity mapping.
+    Use this function to remap the configuration to the new NWBFile using the location in file attribute.
+
+    The function returns a new backend configuration instance that is aligned with the provided `nwbfile`
+    without modifying the original `backend_configuration`.
 
     Parameters
     ----------
     nwbfile : pynwb.NWBFile
-        The in-memory pynwb.NWBFile object to configure.
+        The target NWBFile object to which the configuration will be remapped.
     backend_configuration : HDF5BackendConfiguration or ZarrBackendConfiguration
-        The configuration model to use when configuring the datasets for this backend.
+        The existing backend configuration to be remapped.
 
     Returns
     -------
-    dataset_configuration_to_nwbfile_object_ids: Dict[str, str]
-        A mapping of dataset configuration object IDs to their equivalent NWBFile object IDs.
+    BackendConfiguration
+        A new backend configuration instance that is aligned with the provided `nwbfile`.
+
+    Raises
+    ------
+    ValueError
+        If a dataset from the original configuration cannot be found in the new NWBFile.
+
+    Notes
+    -----
+    * This function returns a new copy, it does not modify the input `nwbfile` or the original
+    `backend_configuration`.
+    * If the `nwbfile` and the `backend_configuration` are already aligned (i.e., same identifier
+    and all dataset IDs match), the original `backend_configuration` is returned unchanged.
+    * Remapping is based on the `location_in_file` attribute of dataset configurations.
+
     """
-    nwbfile_object_ids = [nwbfile_object_id for nwbfile_object_id in nwbfile.objects.keys()]
-    dataset_configuration_object_ids = [
-        dataset_configuration.object_id in nwbfile_object_ids
+    # TODO: Might not be necessary if https://github.com/hdmf-dev/hdmf/issues/1108 is implemented
+
+    # First check if update is necessary
+    comes_from_same_nwbfile = nwbfile.identifier == backend_configuration.nwbfile_identifier
+    ids_in_backend_configuration = [
+        dataset_configuration.object_id
         for dataset_configuration in backend_configuration.dataset_configurations.values()
     ]
 
-    # Default to identity
-    dataset_configuration_to_nwbfile_object_ids = {
-        dataset_configuration_object_id: dataset_configuration_object_id
-        for dataset_configuration_object_id in dataset_configuration_object_ids
-    }
-
-    is_same_nwbfile = nwbfile.identifier == backend_configuration.nwbfile_identifier
-    all_ids_in_dataset_configuration_are_in_nwbfile = all(
-        dataset_configuration_object_id in nwbfile_object_ids
-        for dataset_configuration_object_id in dataset_configuration_object_ids
+    ids_in_nwbfile = nwbfile.objects.keys()
+    all_ids_in_backend_configuration_are_in_nwbfile = all(
+        dataset_id in ids_in_nwbfile for dataset_id in ids_in_backend_configuration
     )
-    return_identity = is_same_nwbfile and all_ids_in_dataset_configuration_are_in_nwbfile
-    if return_identity:
-        return dataset_configuration_to_nwbfile_object_ids
 
-    # If the NWBFile is not the same, then we need to find the equivalent object IDs
-    backend_configuration_class = type(backend_configuration)
-    new_default_backend_configuration = backend_configuration_class.from_nwbfile(nwbfile=nwbfile)
-    locations_in_file_to_new_object_ids = {
-        dataset_configuration.location_in_file: dataset_configuration.object_id
-        for dataset_configuration in new_default_backend_configuration.dataset_configurations.values()
-    }
+    if comes_from_same_nwbfile and all_ids_in_backend_configuration_are_in_nwbfile:
+        return backend_configuration
 
-    dataset_configuration_to_nwbfile_object_ids = {
-        dataset_configuration.object_id: locations_in_file_to_new_object_ids[dataset_configuration.location_in_file]
+    # Create a mapping from location to configuration
+    location_to_configuration = {
+        dataset_configuration.location_in_file: dataset_configuration
         for dataset_configuration in backend_configuration.dataset_configurations.values()
     }
 
-    return dataset_configuration_to_nwbfile_object_ids
+    # Create a new backend configuration from the NWBFile
+    backend_configuration_class = type(backend_configuration)
+    remaped_backend_configuration = backend_configuration_class.from_nwbfile(nwbfile=nwbfile)
+
+    # Remap the configurations using the location in file
+    for dataset_configuration in remaped_backend_configuration.dataset_configurations.values():
+        location_in_file = dataset_configuration.location_in_file
+        if location_in_file in location_to_configuration:
+            corresponding_configuration = location_to_configuration[location_in_file]
+            remaped_backend_configuration.dataset_configurations[location_in_file] = corresponding_configuration
+        else:
+            raise ValueError(
+                f"Could not remap backend configuration created with nwbfile with identifier {backend_configuration.nwbfile_identifier} "
+                f"to NWBFile with identifier {nwbfile.identifier}."
+                f"Failed to find a configuration for dataset at location {location_in_file} in the backend configuration."
+            )
+
+    return remaped_backend_configuration
 
 
 def configure_backend(
@@ -87,14 +111,17 @@ def configure_backend(
     is_ndx_events_installed = is_package_installed(package_name="ndx_events")
     ndx_events = sys.modules.get("ndx_events", None)
 
-    dataset_configuration_to_nwbfile_object_ids = _resolve_nwbfile_object_ids(
-        nwbfile=nwbfile, backend_configuration=backend_configuration
-    )
+    # A remapping of the object IDs in the backend configuration might necessary if
+    # The backend configuration was created with a different NWBFile instance
+    if nwbfile.identifier != backend_configuration.nwbfile_identifier:
+        backend_configuration = _remap_backend_configuration_to_nwbfile(
+            nwbfile=nwbfile, backend_configuration=backend_configuration
+        )
 
     # Set all DataIO based on the configuration
     data_io_class = backend_configuration.data_io_class
     for dataset_configuration in backend_configuration.dataset_configurations.values():
-        object_id = dataset_configuration_to_nwbfile_object_ids[dataset_configuration.object_id]
+        object_id = dataset_configuration.object_id
         dataset_name = dataset_configuration.dataset_name
         data_io_kwargs = dataset_configuration.get_data_io_kwargs()
 
