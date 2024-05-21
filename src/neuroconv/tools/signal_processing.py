09325fad4f9aa60c98f6ea8761f892adaf6cec04
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-05-21 10:03:44 -0700

    moved to optogenetics module

--- src/neuroconv/tools/signal_processing.py
+++ src/neuroconv/tools/signal_processing.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional
 
 import numpy as np
 
@@ -61,57 +61,3 @@ def get_falling_frames_from_ttl(trace: np.ndarray, threshold: Optional[float] = 
     falling_frames = np.where(diff < 0)[0] + 1
 
     return falling_frames
-
-
-def create_optogenetic_stimulation_timeseries(
-    *,
-    stimulation_onset_times: np.ndarray,
-    duration: float,
-    frequency: float,
-    pulse_width: float,
-    power: float,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """Create a continuous stimulation time series from stimulation onset times and parameters.
-
-    In the resulting data array, the offset time of each pulse is represented by a 0 power value.
-
-    Parameters
-    ----------
-    stimulation_onset_times : np.ndarray
-        Array of stimulation onset times.
-    duration : float
-        Duration of stimulation in seconds.
-    frequency : float
-        Frequency of stimulation in Hz.
-    pulse_width : float
-        Pulse width of stimulation in seconds.
-    power : float
-        Power of stimulation in W.
-
-    Returns
-    -------
-    np.ndarray
-        Stimulation timestamps.
-    np.ndarray
-        Instantaneous stimulation power.
-
-    Notes
-    -----
-    For continuous (non-pulsed) stimulation of a desired duration, simply set
-    ```
-    pulse_width = duration
-    frequency = 1 / duration
-    ```
-    """
-    num_pulses = int(duration * frequency)
-    inter_pulse_interval = 1 / frequency
-    timestamps, data = [0], [0]
-    for onset_time in stimulation_onset_times:
-        for i in range(num_pulses):
-            pulse_onset_time = onset_time + i * inter_pulse_interval
-            timestamps.append(pulse_onset_time)
-            data.append(power)
-            pulse_offset_time = pulse_onset_time + pulse_width
-            timestamps.append(pulse_offset_time)
-            data.append(0)
-    return np.array(timestamps, dtype=np.float64), np.array(data, dtype=np.float64)
--- tests/test_minimal/test_tools/test_signal_processing.py
+++ tests/test_minimal/test_tools/test_signal_processing.py
@@ -3,7 +3,6 @@ from hdmf.testing import TestCase
 from numpy.testing import assert_array_equal
 
 from neuroconv.tools.signal_processing import (
-    create_ogen_stimulation_timeseries,
     get_falling_frames_from_ttl,
     get_rising_frames_from_ttl,
 )
@@ -165,75 +164,3 @@ class TestGetRisingAndFallingTimesFromTTL(TestCase):
 
         expected_falling_frames = np.array([77_500, 205_000])
         assert_array_equal(x=falling_frames, y=expected_falling_frames)
-
-
-def test_create_ogen_stimulation_timeseries():
-    stimulation_onset_times = [10, 20, 30]
-    duration = 1
-    frequency = 4
-    pulse_width = 0.01
-    power = 1
-    expected_timestamps = [
-        0,
-        10,
-        10.01,
-        10.25,
-        10.26,
-        10.5,
-        10.51,
-        10.75,
-        10.76,
-        20,
-        20.01,
-        20.25,
-        20.26,
-        20.5,
-        20.51,
-        20.75,
-        20.76,
-        30,
-        30.01,
-        30.25,
-        30.26,
-        30.5,
-        30.51,
-        30.75,
-        30.76,
-    ]
-    expected_data = [i % 2 for i in range(len(expected_timestamps))]  # 0, 1, 0, 1, ...
-    expected_timestamps = np.array(expected_timestamps, dtype=np.float64)
-    expected_data = np.array(expected_data, dtype=np.float64)
-
-    timestamps, data = create_ogen_stimulation_timeseries(
-        stimulation_onset_times=stimulation_onset_times,
-        duration=duration,
-        frequency=frequency,
-        pulse_width=pulse_width,
-        power=power,
-    )
-
-    assert_array_equal(timestamps, expected_timestamps)
-    assert_array_equal(data, expected_data)
-
-
-def test_create_ogen_stimulation_timeseries_cts():
-    stimulation_onset_times = [10, 20, 30]
-    duration = 1
-    frequency = 1
-    pulse_width = 1
-    power = 1
-    expected_timestamps = [0, 10, 11, 20, 21, 30, 31]
-    expected_data = [i % 2 for i in range(len(expected_timestamps))]  # 0, 1, 0, 1, ...
-    expected_timestamps = np.array(expected_timestamps, dtype=np.float64)
-    expected_data = np.array(expected_data, dtype=np.float64)
-
-    timestamps, data = create_ogen_stimulation_timeseries(
-        stimulation_onset_times=stimulation_onset_times,
-        duration=duration,
-        frequency=frequency,
-        pulse_width=pulse_width,
-        power=power,
-    )
-
-    assert_array_equal(timestamps, expected_timestamps)
-    assert_array_equal(data, expected_data)
