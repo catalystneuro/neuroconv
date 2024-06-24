1315bfd7f4f8fbf7bcdf80698a85189e13a54f8f
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-06-24 18:44:56 -0400

    fix conditional existence of xml path

--- src/neuroconv/datainterfaces/ecephys/neuroscope/neuroscopedatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/neuroscope/neuroscopedatainterface.py
@@ -305,6 +305,10 @@ class NeuroScopeSortingInterface(BaseSortingExtractorInterface):
         session_path = Path(self.source_data["folder_path"])
         session_id = session_path.stem
         xml_file_path = self.source_data.get("xml_file_path", str(session_path / f"{session_id}.xml"))
+
+        if not Path(xml_file_path).is_file():
+            return metadata
+
         metadata["Ecephys"] = NeuroScopeRecordingInterface.get_ecephys_metadata(xml_file_path=xml_file_path)
         session_start_time = get_session_start_time(str(xml_file_path))
         if session_start_time is not None:
