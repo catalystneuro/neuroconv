55ad151a6b52a495514ea06e8d7f3973a0a96688
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-07-01 09:42:24 +1000

    added temporal alignment

--- src/neuroconv/datainterfaces/ophys/tdt_fp/tdtfiberphotometrydatainterface.py
+++ src/neuroconv/datainterfaces/ophys/tdt_fp/tdtfiberphotometrydatainterface.py
@@ -1,7 +1,7 @@
 import os
 from contextlib import redirect_stdout
 from pathlib import Path
-from typing import Optional
+from typing import Literal, Optional
 
 import numpy as np
 from pynwb.file import NWBFile
@@ -33,20 +33,53 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
         metadata_schema = super().get_metadata_schema()
         return metadata_schema
 
-    def get_original_timestamps(self) -> np.ndarray:
-        return NotImplementedError
+    def load(self):
+        tdt = get_package("tdt", installation_instructions="pip install tdt")
+        folder_path = Path(self.source_data["folder_path"])
+        assert folder_path.is_dir(), f"Folder path {folder_path} does not exist."
+        with open(os.devnull, "w") as f, redirect_stdout(f):
+            tdt_photometry = tdt.read_block(str(folder_path))
+        return tdt_photometry
 
-    def get_timestamps(self) -> np.ndarray:
-        return NotImplementedError
+    def get_original_timestamps(self) -> dict[str, np.ndarray]:
+        tdt_photometry = self.load()
+        stream_name_to_timestamps = {}
+        for stream_name in tdt_photometry.streams:
+            rate = tdt_photometry.streams[stream_name].fs
+            starting_time = 0.0
+            timestamps = np.arange(starting_time, tdt_photometry.streams[stream_name].data.shape[-1] / rate, 1 / rate)
+            stream_name_to_timestamps[stream_name] = timestamps
+        return stream_name_to_timestamps
 
-    def set_aligned_timestamps(self, aligned_timestamps: np.ndarray) -> None:
-        return NotImplementedError
+    def get_timestamps(self) -> dict[str, np.ndarray]:
+        return self.stream_name_to_timestamps
+
+    def set_aligned_timestamps(self, stream_name_to_aligned_timestamps: dict[str, np.ndarray]) -> None:
+        self.stream_name_to_timestamps = stream_name_to_aligned_timestamps
+
+    def get_original_starting_time_and_rate(self) -> dict[str, tuple[float, float]]:
+        tdt_photometry = self.load()
+        stream_name_to_starting_time_and_rate = {}
+        for stream_name in tdt_photometry.streams:
+            rate = tdt_photometry.streams[stream_name].fs
+            starting_time = 0.0
+            stream_name_to_starting_time_and_rate[stream_name] = (starting_time, rate)
+        return stream_name_to_starting_time_and_rate
+
+    def get_starting_time_and_rate(self) -> tuple[float, float]:
+        return self.stream_name_to_starting_time_and_rate
+
+    def set_aligned_starting_time_and_rate(
+        self, stream_name_to_aligned_starting_time_and_rate: dict[str, tuple[float, float]]
+    ) -> None:
+        self.stream_name_to_starting_time_and_rate = stream_name_to_aligned_starting_time_and_rate
 
     def add_to_nwbfile(
         self,
         nwbfile: NWBFile,
         metadata: dict,
         t2: Optional[float] = None,
+        timing_source: Literal["original", "aligned_timestamps", "aligned_starting_time_and_rate"] = "original",
     ):
         # Dynamic Imports
         ndx_fiber_photometry = get_package("ndx_fiber_photometry")
@@ -54,15 +87,20 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
         FiberPhotometry = ndx_fiber_photometry.FiberPhotometry
         FiberPhotometryResponseSeries = ndx_fiber_photometry.FiberPhotometryResponseSeries
         FiberPhotometryTable = ndx_fiber_photometry.FiberPhotometryTable
-        tdt = get_package("tdt", installation_instructions="pip install tdt")
-        read_block = tdt.read_block
 
         # Load Data
-        folder_path = Path(self.source_data["folder_path"])
-        assert folder_path.is_dir(), f"Folder path {folder_path} does not exist."
-        with open(os.devnull, "w") as f, redirect_stdout(f):
-            if t2 is None:
-                tdt_photometry = read_block(str(folder_path))
+        tdt_photometry = self.load()
+
+        # timing_source is used to avoid loading the data twice if alignment is NOT used.
+        # It is also used to determine whether or not to use the aligned timestamps or starting time and rate.
+        if timing_source == "aligned_timestamps":
+            stream_name_to_timestamps = self.get_timestamps()
+        elif timing_source == "aligned_starting_time_and_rate":
+            stream_name_to_starting_time_and_rate = self.get_starting_time_and_rate()
+        else:
+            assert (
+                timing_source == "original"
+            ), "timing_source must be one of 'original', 'aligned_timestamps', or 'aligned_starting_time_and_rate'."
 
         # Add Devices
         device_types = [
@@ -88,14 +126,25 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
                 data = tdt_photometry.streams[commanded_voltage_series_metadata["stream_name"]].data
             else:
                 data = tdt_photometry.streams[commanded_voltage_series_metadata["stream_name"]].data[index, :]
+            if timing_source == "aligned_timestamps":
+                timestamps = stream_name_to_timestamps[commanded_voltage_series_metadata["stream_name"]]
+                timing_kwargs = dict(timestamps=timestamps)
+            elif timing_source == "aligned_starting_time_and_rate":
+                starting_time, rate = stream_name_to_starting_time_and_rate[
+                    commanded_voltage_series_metadata["stream_name"]
+                ]
+                timing_kwargs = dict(starting_time=starting_time, rate=rate)
+            else:
+                starting_time = 0.0
+                rate = tdt_photometry.streams[commanded_voltage_series_metadata["stream_name"]].fs
+                timing_kwargs = dict(starting_time=starting_time, rate=rate)
             commanded_voltage_series = CommandedVoltageSeries(
                 name=commanded_voltage_series_metadata["name"],
                 description=commanded_voltage_series_metadata["description"],
                 data=data,
                 unit=commanded_voltage_series_metadata["unit"],
                 frequency=commanded_voltage_series_metadata["frequency"],
-                starting_time=0.0,
-                rate=tdt_photometry.streams[commanded_voltage_series_metadata["stream_name"]].fs,
+                **timing_kwargs,
             )
             nwbfile.add_acquisition(commanded_voltage_series)
 
@@ -125,7 +174,16 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
 
         # Fiber Photometry Response Series
         first_stream_name = metadata["Ophys"]["FiberPhotometry"]["FiberPhotometryResponseSeries"][0]["stream_names"][0]
-        rate = tdt_photometry.streams[first_stream_name].fs
+        if timing_source == "aligned_timestamps":
+            first_timestamps = stream_name_to_timestamps[first_stream_name]
+            timing_kwargs = dict(timestamps=first_timestamps)
+        elif timing_source == "aligned_starting_time_and_rate":
+            first_starting_time, first_rate = stream_name_to_starting_time_and_rate[first_stream_name]
+            timing_kwargs = dict(starting_time=first_starting_time, rate=first_rate)
+        else:
+            first_rate = tdt_photometry.streams[first_stream_name].fs
+            first_starting_time = 0.0
+            timing_kwargs = dict(starting_time=first_starting_time, rate=first_rate)
         for fiber_photometry_response_series_metadata in metadata["Ophys"]["FiberPhotometry"][
             "FiberPhotometryResponseSeries"
         ]:
@@ -134,9 +192,19 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
                 fiber_photometry_response_series_metadata["stream_names"],
                 fiber_photometry_response_series_metadata["stream_indices"],
             ):
-                assert (
-                    tdt_photometry.streams[stream_name].fs == rate
-                ), f"All streams in the same FiberPhotometryResponseSeries must have the same sampling rate. But stream {stream_name} has a different sampling rate than {first_stream_name}."
+                if timing_source == "aligned_timestamps":
+                    assert np.array_equal(
+                        stream_name_to_timestamps[stream_name], first_timestamps
+                    ), f"All streams in the same FiberPhotometryResponseSeries must have the same timestamps. But stream {stream_name} has different timestamps than {first_stream_name}."
+                elif timing_source == "aligned_starting_time_and_rate":
+                    assert stream_name_to_starting_time_and_rate[stream_name] == (
+                        first_starting_time,
+                        first_rate,
+                    ), f"All streams in the same FiberPhotometryResponseSeries must have the same starting time and rate. But stream {stream_name} has different starting time and rate than {first_stream_name}."
+                else:
+                    assert (
+                        tdt_photometry.streams[stream_name].fs == first_rate
+                    ), f"All streams in the same FiberPhotometryResponseSeries must have the same sampling rate. But stream {stream_name} has a different sampling rate than {first_stream_name}."
                 if stream_index is None:
                     data_trace = tdt_photometry.streams[stream_name].data
                 else:
@@ -154,7 +222,7 @@ class TDTFiberPhotometryInterface(BaseTemporalAlignmentInterface):
                 description=fiber_photometry_response_series_metadata["description"],
                 data=data,
                 unit=fiber_photometry_response_series_metadata["unit"],
-                rate=rate,
                 fiber_photometry_table_region=fiber_photometry_table_region,
+                **timing_kwargs,
             )
             nwbfile.add_acquisition(fiber_photometry_response_series)
