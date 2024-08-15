1ad69ca5ab32bdca537fa09a8554d9310aeb245b
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-08-15 16:45:36 -0700

    added initial test

---
+++ tests/test_minimal/test_tools/test_backend_and_dataset_configuration/test_helpers/test_repack_nwbfile.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+from hdmf_zarr import NWBZarrIO
+from pynwb import NWBHDF5IO, NWBFile
+from pynwb.testing.mock.base import mock_TimeSeries
+from pynwb.testing.mock.file import mock_NWBFile
+
+from neuroconv.tools.nwb_helpers import (
+    get_module,
+    repack_nwbfile,
+)
+
+
+def generate_complex_nwbfile() -> NWBFile:
+    nwbfile = mock_NWBFile()
+
+    raw_array = np.array([[1, 2, 3], [4, 5, 6]])
+    raw_time_series = mock_TimeSeries(name="RawTimeSeries", data=raw_array)
+    nwbfile.add_acquisition(raw_time_series)
+
+    number_of_trials = 10
+    for start_time, stop_time in zip(
+        np.linspace(start=0.0, stop=10.0, num=number_of_trials), np.linspace(start=1.0, stop=11.0, num=number_of_trials)
+    ):
+        nwbfile.add_trial(start_time=start_time, stop_time=stop_time)
+
+    ecephys_module = get_module(nwbfile=nwbfile, name="ecephys")
+    processed_array = np.array([[7.0, 8.0], [9.0, 10.0], [11.0, 12.0], [13.0, 14.0]])
+    processed_time_series = mock_TimeSeries(name="ProcessedTimeSeries", data=processed_array)
+    ecephys_module.add(processed_time_series)
+
+    return nwbfile
+
+
+@pytest.fixture(scope="session")
+def hdf5_nwbfile_path(tmpdir_factory):
+    nwbfile_path = tmpdir_factory.mktemp("data").join("test_repack_nwbfile.nwb.h5")
+    if not Path(nwbfile_path).exists():
+        nwbfile = generate_complex_nwbfile()
+        with NWBHDF5IO(path=str(nwbfile_path), mode="w") as io:
+            io.write(nwbfile)
+    return str(nwbfile_path)
+
+
+@pytest.fixture(scope="session")
+def zarr_nwbfile_path(tmpdir_factory):
+    nwbfile_path = tmpdir_factory.mktemp("data").join("test_repack_nwbfile.nwb.zarr")
+    if not Path(nwbfile_path).exists():
+        nwbfile = generate_complex_nwbfile()
+        with NWBZarrIO(path=str(nwbfile_path), mode="w") as io:
+            io.write(nwbfile)
+    return str(nwbfile_path)
+
+
+def test_repack_nwbfile(hdf5_nwbfile_path):
+    export_path = Path(hdf5_nwbfile_path).parent / "repacked_test_repack_nwbfile.nwb.h5"
+    with NWBHDF5IO(hdf5_nwbfile_path, mode="r") as io:
+        nwbfile = io.read()
+        repack_nwbfile(
+            nwbfile=nwbfile,
+            export_nwbfile_path=export_path,
+        )
+
+    with NWBHDF5IO(export_path, mode="r") as io:
+        nwbfile = io.read()
+        assert nwbfile.acquisition["RawTimeSeries"].data.chunks == (2, 3)
+        assert nwbfile.processing["ecephys"]["ProcessedTimeSeries"].data.chunks == (4, 2)
