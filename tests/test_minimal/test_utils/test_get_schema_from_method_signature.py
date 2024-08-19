d49bf9a5d592ade62af08d2e25008c2beeab7450
CodyCBakerPhD <codycbakerphd@gmail.com>
2024-08-18 23:12:25 -0400

    enhance test

--- tests/test_minimal/test_utils/test_get_schema_from_method_signature.py
+++ tests/test_minimal/test_utils/test_get_schema_from_method_signature.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Dict, Optional, Union
 
 from jsonschema import validate
+from pydantic import DirectoryPath, FilePath
 
 from neuroconv.datainterfaces import AlphaOmegaRecordingInterface
 from neuroconv.utils import get_json_schema_from_method_signature
@@ -19,9 +20,8 @@ def test_get_json_schema_from_method_signature_basic():
     ):
         pass
 
-    json_schema = get_json_schema_from_method_signature(method=basic_method)
-
-    assert json_schema == {
+    test_json_schema = get_json_schema_from_method_signature(method=basic_method)
+    expected_json_schema = {
         "additionalProperties": False,
         "properties": {
             "boolean": {"type": "boolean"},
@@ -39,96 +39,130 @@ def test_get_json_schema_from_method_signature_basic():
         "type": "object",
     }
 
-
-# def test_get_schema_from_method_signature_exclude():
-#     def test1():
-#         pass
-#
-#     json_schema = get_schema_from_method_signature(method=test1)
-#
-#     assert json_schema == {}
-#
-#
-# def test_get_schema_from_method_signature_init():
-#     class SomeInterface:
-#         def __init__(self, file_path: FilePath, folder_path: DirectoryPath, old_annotation_1: str, old_annotation_2: pathlib.Path, old_annotation_3: Union[str, pathlib.Path]):
-#             pass
-#
-#     json_schema = get_schema_from_method_signature(method=SomeInterface.__init__)
-#
-#     assert json_schema == {}
-#
-# def test_get_schema_from_method_signature_class_static():
-#     class SomeInterface:
-#
-#         @staticmethod
-#         def some_static_method(integer: int, string: str, boolean: bool, number: float):
-#             pass
-#     json_schema = get_schema_from_method_signature(method=test1)
-#
-#     assert json_schema == {}
-#
-# def test_get_schema_from_method_signature_class_method():
-#     class SomeInterface:
-#
-#         @classmethod
-#         def some_static_method(cls, integer: int, string: str, boolean: bool, number: float):
-#             pass
-#
-#     json_schema = get_schema_from_method_signature(method=test1)
-#
-#     assert json_schema == {}
-
-# def test_get_json_schema_from_method_signature_previous_1():
-#     class A:
-#         def __init__(self, a: int, b: float, c: Union[Path, str], d: bool, e: str = "hi", f: Dict[str, str] = None):
-#             pass
-#
-#     schema = get_schema_from_method_signature(A.__init__)
-#
-#     correct_schema = dict(
-#         additionalProperties=False,
-#         properties=dict(
-#             a=dict(type="number"),
-#             b=dict(type="number"),
-#             c=dict(type="string"),
-#             d=dict(type="boolean"),
-#             e=dict(default="hi", type="string"),
-#             f=dict(type="object", additionalProperties={"^.*$": dict(type="string")}),
-#         ),
-#         required=[
-#             "a",
-#             "b",
-#             "c",
-#             "d",
-#         ],
-#         type="object",
-#     )
-#
-#     assert schema == correct_schema
+    assert test_json_schema == expected_json_schema
 
 
-def test_get_schema_from_example_data_interface():
-    schema = get_json_schema_from_method_signature(AlphaOmegaRecordingInterface.__init__)
+def test_get_json_schema_from_method_signature_exclude():
+    def basic_method(
+        integer: int,
+        floating: float,
+        string_or_path: Union[Path, str],
+        boolean: bool,
+        dictionary: Dict[str, str],
+        string_with_default: str = "hi",
+        optional_dictionary: Optional[Dict[str, str]] = None,
+    ):
+        pass
 
-    assert schema == {
-        "required": ["folder_path"],
-        "properties": {
-            "folder_path": {
-                "format": "directory",
-                "description": "Path to the folder of .mpx files.",
-                "type": "string",
-            },
-            "verbose": {"description": "Allows verbose.\nDefault is True.", "type": "boolean", "default": True},
-            "es_key": {"type": "string", "default": "ElectricalSeries"},
-        },
-        "type": "object",
+    test_exclude = ["string_with_default", "floating"]
+    test_json_schema = get_json_schema_from_method_signature(method=basic_method, exclude=test_exclude)
+    expected_json_schema = {
         "additionalProperties": False,
+        "properties": {
+            "boolean": {"type": "boolean"},
+            "dictionary": {"additionalProperties": {"type": "string"}, "type": "object"},
+            "integer": {"type": "integer"},
+            "optional_dictionary": {
+                "anyOf": [{"additionalProperties": {"type": "string"}, "type": "object"}, {"type": "null"}],
+                "default": None,
+            },
+            "string_or_path": {"anyOf": [{"format": "path", "type": "string"}, {"type": "string"}]},
+        },
+        "required": ["integer", "string_or_path", "boolean", "dictionary"],
+        "type": "object",
     }
-    assert schema == {
+
+    assert test_json_schema == expected_json_schema
+
+
+def test_get_schema_from_method_signature_init():
+    """Test that 'self' is automatically skipped."""
+
+    class TestClass:
+        def __init__(
+            self,
+            file_path: FilePath,
+            folder_path: DirectoryPath,
+            old_annotation_1: str,
+            old_annotation_2: Path,
+            old_annotation_3: Union[str, Path],
+        ):
+            pass
+
+    test_json_schema = get_json_schema_from_method_signature(method=TestClass.__init__)
+    expected_json_schema = {
+        "additionalProperties": False,
+        "properties": {
+            "file_path": {"format": "file-path", "type": "string"},
+            "folder_path": {"format": "directory-path", "type": "string"},
+            "old_annotation_1": {"type": "string"},
+            "old_annotation_2": {"format": "path", "type": "string"},
+            "old_annotation_3": {"anyOf": [{"type": "string"}, {"format": "path", "type": "string"}]},
+        },
+        "required": ["file_path", "folder_path", "old_annotation_1", "old_annotation_2", "old_annotation_3"],
+        "type": "object",
+    }
+
+    assert test_json_schema == expected_json_schema
+
+
+def test_get_schema_from_method_signature_class_static():
+    """Ensuring that signature assembly prior to passing to Pydantic is not affected by bound or static methods."""
+
+    class TestClass:
+
+        @staticmethod
+        def test_static_method(integer: int, string: str, boolean: bool, number: float):
+            pass
+
+    test_json_schema = get_json_schema_from_method_signature(method=TestClass.test_static_method)
+    expected_json_schema = {
+        "additionalProperties": False,
+        "properties": {
+            "boolean": {"type": "boolean"},
+            "integer": {"type": "integer"},
+            "number": {"type": "number"},
+            "string": {"type": "string"},
+        },
+        "required": ["integer", "string", "boolean", "number"],
+        "type": "object",
+    }
+
+    assert test_json_schema == expected_json_schema
+
+
+def test_get_schema_from_method_signature_class_method():
+    """Test that 'cls' is automatically skipped."""
+
+    class TestClass:
+
+        @classmethod
+        def some_class_method(cls, integer: int, string: str, boolean: bool, number: float):
+            pass
+
+    test_json_schema = get_json_schema_from_method_signature(method=TestClass.some_class_method)
+    expected_json_schema = {
+        "additionalProperties": False,
+        "properties": {
+            "boolean": {"type": "boolean"},
+            "integer": {"type": "integer"},
+            "number": {"type": "number"},
+            "string": {"type": "string"},
+        },
+        "required": ["integer", "string", "boolean", "number"],
+        "type": "object",
+    }
+
+    assert test_json_schema == expected_json_schema
+
+
+def test_get_json_schema_from_example_data_interface():
+    test_json_schema = get_json_schema_from_method_signature(AlphaOmegaRecordingInterface.__init__)
+    expected_json_schema = {
         "properties": {
             "folder_path": {
-                "anyOf": [{"type": "string"}, {"format": "path", "type": "string"}],
+                "format": "directory-path",
+                "type": "string",
                 "description": "Path to the folder of .mpx files.",
             },
             "verbose": {"default": True, "type": "boolean", "description": "Allows verbose.\nDefault is True."},
@@ -138,6 +172,8 @@ def test_get_schema_from_example_data_interface():
         "type": "object",
         "additionalProperties": False,
     }
+
+    assert test_json_schema == expected_json_schema
 
 
 def test_fix_to_358():
@@ -153,8 +189,8 @@ def test_fix_to_358():
         ):
             pass
 
-    conversion_options_schema = get_json_schema_from_method_signature(method=Test358.add_to_nwbfile)
-    assert conversion_options_schema == {
+    test_conversion_options_schema = get_json_schema_from_method_signature(method=Test358.add_to_nwbfile)
+    expected_conversion_options_schema = {
         "properties": {
             "metadata": {"anyOf": [{"type": "object"}, {"type": "null"}], "default": None},
             "tag": {"default": "trials", "type": "string"},
@@ -171,6 +207,8 @@ def test_fix_to_358():
         "additionalProperties": False,
     }
 
+    assert test_conversion_options_schema == expected_conversion_options_schema
+
     # Validation used to fail due to lack of Dict[str, str] support
-    conversion_options = dict(column_name_mapping=dict(condition="cond"))
-    validate(instance=conversion_options, schema=conversion_options_schema)
+    test_conversion_options = dict(column_name_mapping=dict(condition="cond"))
+    validate(instance=test_conversion_options, schema=test_conversion_options_schema)
