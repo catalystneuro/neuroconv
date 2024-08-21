ee7ec524b401c0d40e9a1bfaeae19cf1a570d007
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-08-21 14:18:04 -0700

    updated temp_test

--- temp_test.py
+++ temp_test.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 import numpy as np
@@ -7,9 +8,7 @@ from hdmf_zarr.nwb import NWBZarrIO
 from pynwb import NWBHDF5IO, H5DataIO, TimeSeries
 from pynwb.testing.mock.file import mock_NWBFile
 
-from neuroconv.tools.nwb_helpers._dataset_configuration import (
-    get_existing_dataset_io_configurations,
-)
+from neuroconv.tools.nwb_helpers import repack_nwbfile
 
 
 def write_nwbfile(nwbfile_path: Path, backend: str = "hdf5"):
@@ -37,28 +36,27 @@ def write_nwbfile(nwbfile_path: Path, backend: str = "hdf5"):
 
 def main():
     nwbfile_path = Path("temp.nwb.zarr")
-    repacked_nwbfile_path = Path("repacked_temp.nwb")
+    repacked_nwbfile_path = Path("repacked_temp.nwb.zarr")
     if repacked_nwbfile_path.exists():
-        os.remove(repacked_nwbfile_path)
+        if repacked_nwbfile_path.is_dir():
+            shutil.rmtree(repacked_nwbfile_path)
+        else:
+            os.remove(repacked_nwbfile_path)
     if not nwbfile_path.exists():
         write_nwbfile(nwbfile_path, backend="zarr")
 
-    with NWBZarrIO(str(nwbfile_path), mode="r") as io:
-        nwbfile = io.read()
-        dataset_io_configurations = get_existing_dataset_io_configurations(nwbfile=nwbfile, backend="zarr")
-        print(next(dataset_io_configurations))
-    # backend_configuration_changes = {"acquisition/test_timeseries/data": dict(chunk_shape=(2,))}
-    # repack_nwbfile(
-    #     nwbfile_path=nwbfile_path,
-    #     export_nwbfile_path=repacked_nwbfile_path,
-    #     backend="hdf5",
-    #     backend_configuration_changes=backend_configuration_changes,
-    #     use_default_backend_configuration=False,
-    # )
+    backend_configuration_changes = {"acquisition/test_timeseries/data": dict(chunk_shape=(2,))}
+    repack_nwbfile(
+        nwbfile_path=str(nwbfile_path),
+        export_nwbfile_path=str(repacked_nwbfile_path),
+        backend="zarr",
+        backend_configuration_changes=backend_configuration_changes,
+        use_default_backend_configuration=False,
+    )
 
-    # with NWBHDF5IO(repacked_nwbfile_path, mode="r") as io:
-    #     nwbfile = io.read()
-    #     print(f'{nwbfile.acquisition["test_timeseries"].data.chunks = }')
+    with NWBZarrIO(str(repacked_nwbfile_path), mode="r") as io:
+        nwbfile = io.read()
+        print(f'{nwbfile.acquisition["test_timeseries"].data.chunks = }')
 
 
 if __name__ == "__main__":
