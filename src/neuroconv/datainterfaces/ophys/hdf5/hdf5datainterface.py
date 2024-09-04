04694dae66049f9aa4bfb30679b67a1b52609fe2
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-09-04 17:38:54 -0400

    import

--- src/neuroconv/datainterfaces/ophys/hdf5/hdf5datainterface.py
+++ src/neuroconv/datainterfaces/ophys/hdf5/hdf5datainterface.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import FilePath
+from pydantic import FilePath, validate_call
 
 from ..baseimagingextractorinterface import BaseImagingExtractorInterface
 from ....utils import ArrayType
