8ab8e5d0da0ddbdd20b4202a4f3b524547360847
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-09-04 17:22:49 -0400

    uncomment to see errors

--- src/neuroconv/datainterfaces/ophys/hdf5/hdf5datainterface.py
+++ src/neuroconv/datainterfaces/ophys/hdf5/hdf5datainterface.py
@@ -13,7 +13,7 @@ class Hdf5ImagingInterface(BaseImagingExtractorInterface):
     associated_suffixes = (".h5", ".hdf5")
     info = "Interface for HDF5 imaging data."
 
-    # @validate_call
+    @validate_call
     def __init__(
         self,
         file_path: FilePath,
