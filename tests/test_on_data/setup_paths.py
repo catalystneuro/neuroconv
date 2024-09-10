935bee262e7d6c625ce12ead54b685c8f117c144
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-09-10 15:43:17 -0700

    moved test_config_dict to non-ci if-branch of code

--- tests/test_on_data/setup_paths.py
+++ tests/test_on_data/setup_paths.py
@@ -2,21 +2,19 @@ import os
 import tempfile
 from pathlib import Path
 
-import pytest  # noqa: F401
-
 from neuroconv.utils import load_dict_from_file
-
-# Load the configuration for the data tests
-file_path = Path(__file__).parent.parent.parent / "tests" / "test_on_data" / "gin_test_config.json"
-test_config_dict = load_dict_from_file(file_path)
 
 #  GIN dataset: https://gin.g-node.org/CatalystNeuro/behavior_testing_data
 if os.getenv("CI"):
     LOCAL_PATH = Path(".")  # Must be set to "." for CI
+    test_config_dict = dict(SAVE_OUTPUTS=False)
     print("Running GIN tests on Github CI!")
 else:
     # Override LOCAL_PATH in the `gin_test_config.json` file to a point on your system that contains the dataset folder
     # Use DANDIHub at hub.dandiarchive.org for open, free use of data found in the /shared/catalystneuro/ directory
+    # Load the configuration for the data tests
+    file_path = Path(__file__).parent.parent.parent / "tests" / "test_on_data" / "gin_test_config.json"
+    test_config_dict = load_dict_from_file(file_path)
     LOCAL_PATH = Path(test_config_dict["LOCAL_PATH"])
     print("Running GIN tests locally!")
 
