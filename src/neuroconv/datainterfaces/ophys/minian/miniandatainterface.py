44ff90d97004a63ae72ce8eb2b17adbb3e9844c2
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-10-10 11:01:51 +0200

    add tests

--- src/neuroconv/datainterfaces/ophys/minian/miniandatainterface.py
+++ src/neuroconv/datainterfaces/ophys/minian/miniandatainterface.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from pynwb import NWBFile
+
 from roiextractors.extraction_tools import PathType
 from ..basesegmentationextractorinterface import BaseSegmentationExtractorInterface
 
@@ -28,3 +32,29 @@ class MinianSegmentationInterface(BaseSegmentationExtractorInterface):
         """
         super().__init__(folder_path=folder_path)
         self.verbose = verbose
+
+    def add_to_nwbfile(
+        self,
+        nwbfile: NWBFile,
+        metadata: Optional[dict] = None,
+        stub_test: bool = False,
+        stub_frames: int = 100,
+        include_background_segmentation: bool = True,
+        include_roi_centroids: bool = True,
+        include_roi_acceptance: bool = False,
+        mask_type: Optional[str] = "image",  # Literal["image", "pixel", "voxel"]
+        plane_segmentation_name: Optional[str] = None,
+        iterator_options: Optional[dict] = None,
+    ):
+        super().add_to_nwbfile(
+            nwbfile=nwbfile,
+            metadata=metadata,
+            stub_test=stub_test,
+            stub_frames=stub_frames,
+            include_background_segmentation=include_background_segmentation,
+            include_roi_centroids=include_roi_centroids,
+            include_roi_acceptance=include_roi_acceptance,
+            mask_type=mask_type,
+            plane_segmentation_name=plane_segmentation_name,
+            iterator_options=iterator_options,
+        )
