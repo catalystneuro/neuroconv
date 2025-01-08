46ddd4e96214562d6c7208ae062015a77aafa99a
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-01-08 15:53:33 -0600

    test fix

--- tests/test_on_data/setup_paths.py
+++ tests/test_on_data/setup_paths.py
@@ -18,12 +18,12 @@ if os.getenv("CI"):
 else:
     # Override LOCAL_PATH in the `gin_test_config.json` file to a point on your system that contains the dataset folder
     # Use DANDIHub at hub.dandiarchive.org for open, free use of data found in the /shared/catalystneuro/ directory
-    test_config_path = Path(__file__).parent / "gin_test_config.json"
+    project_root_path = Path(__file__).parent.parent.parent
+    test_config_path = project_root_path / "tests" / "test_on_data"/ "gin_test_config.json"
     config_file_exists = test_config_path.exists()
     if not config_file_exists:
 
-        root = test_config_path.parent.parent
-        base_test_config_path = root / "base_gin_test_config.json"
+        base_test_config_path = project_root_path / "base_gin_test_config.json"
 
         test_config_path.parent.mkdir(parents=True, exist_ok=True)
         copy(src=base_test_config_path, dst=test_config_path)
@@ -40,4 +40,4 @@ BEHAVIOR_DATA_PATH = LOCAL_PATH / "behavior_testing_data"
 ECEPHY_DATA_PATH = LOCAL_PATH / "ephy_testing_data"
 OPHYS_DATA_PATH = LOCAL_PATH / "ophys_testing_data"
 
-TEXT_DATA_PATH = Path(__file__).parent.parent.parent / "tests" / "test_text"
+TEXT_DATA_PATH = project_root_path / "tests" / "test_text"
