753703ac9e45868612f92cf6db8d1264f917546c
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-01-16 11:14:11 -0600

    Update src/neuroconv/datainterfaces/ecephys/basesortingextractorinterface.py

--- src/neuroconv/datainterfaces/ecephys/basesortingextractorinterface.py
+++ src/neuroconv/datainterfaces/ecephys/basesortingextractorinterface.py
@@ -18,7 +18,7 @@ class BaseSortingExtractorInterface(BaseExtractorInterface):
 
     ExtractorModuleName = "spikeinterface.extractors"
 
-    def __init__(self, verbose: bool = False, **source_data):
+    def __init__(self, verbose: bool = True, **source_data):
         super().__init__(**source_data)
         self.sorting_extractor = self.get_extractor()(**source_data)
         self.verbose = verbose
