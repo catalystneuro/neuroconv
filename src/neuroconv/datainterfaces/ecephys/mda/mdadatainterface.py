01f14482811de2e556bec5cfee6db1d77ba75511
Ryan Ly <rly@lbl.gov>
2025-02-24 13:33:55 -0800

    Use MdaSortingExtractor.max_channel and add descrip

--- src/neuroconv/datainterfaces/ecephys/mda/mdadatainterface.py
+++ src/neuroconv/datainterfaces/ecephys/mda/mdadatainterface.py
@@ -38,29 +38,18 @@ class MdaSortingInterface(BaseSortingExtractorInterface):
         """
         super().__init__(file_path=file_path, sampling_frequency=sampling_frequency, verbose=verbose)
 
-        # TODO: append channel indices (1-index) from the firings.mda file if available
-        # This is a draft implementation
-
-        # NOTE: These indices are based on the order of channels in the input data to the sorter
-        # If the user sorts a different timeseries array than what is stored as raw data in the NWB file,
-        # then there will be a mismatch in the electrode IDs / it is not possible to know which channel
-        # in the raw data do the channel indices from the firings.mda correspond to...
-
-        # TODO: Collect the max channel values for each spike event for each unit across segments
-        # Should this be done in spikeinterface or neuroconv?
-        from collections import defaultdict
-
-        label_to_channel = defaultdict(list)
-        for segment in self.sorting_extractor._sorting_segments:
-            for spike_event_index in range(len(segments._labels)):
-                label = segments._labels[spike_event_index]
-                max_channel = segments._max_channels[spike_event_index]
-                label_to_channel[label].append(max_channel)
-
-        # Check that all units have the same max channel value
-        for label in label_to_channel.keys():
-            channels = label_to_channel[label]
-            assert all(channels == channels[0]), f"Unit {label} has spike events where the max channel values differ"
-
-        # TODO: ensure NWB file has electrodes in the electrodes table to map to
-        self.set_property(key="electrodes", values=self.sorting_extractor.get_segment)
+    def get_metadata(self):
+        metadata = super().get_metadata()
+        # See MdaSortingExtractor documentation: 
+        # https://github.com/SpikeInterface/spikeinterface/blob/2c6e800a820aa0618007018b94a047f71f82ace5/src/spikeinterface/extractors/mdaextractors.py#L180
+        # https://mountainsort.readthedocs.io/en/latest/first_sort.html#format-of-the-firings-mda
+        metadata["Ecephys"]["UnitProperties"] = [
+            dict(
+                name="max_channel", 
+                description=(
+                    "1-indexed primary channel for the unit. The channel identification number "
+                    "is relative and may not correspond to the ElectricalSeries also in the file. "
+                    "In other words, if you only sort channels 61-64, the max_channel will be 1-4."
+            ),
+        ]
+        return metadata
