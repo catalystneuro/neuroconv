9ddb2a2a1641fe53d8346df902f3fcc719daa893
weiglszonja <wszonja@gmail.com>
2025-05-08 15:46:42 +0200

    Refactor AudioInterface test

--- tests/test_behavior/test_audio_interface.py
+++ tests/test_behavior/test_audio_interface.py
@@ -263,23 +263,15 @@ class TestAudioInterface(AudioInterfaceTestMixin):
             file_path = Path(temp_dir) / "test.1.wav"
             create_24bit_wav_file(file_path)
 
-            # Create a converter with the AudioInterface
-            class AudioTestNWBConverter(NWBConverter):
-                data_interface_classes = dict(Audio=AudioInterface)
-
-            # Initialize the converter with the 24-bit WAV file
-            source_data = dict(Audio=dict(file_paths=[file_path]))
-            nwb_converter = AudioTestNWBConverter(source_data)
-
+            # Initialize a fresh AudioInterface with the file
+            interface = AudioInterface(file_paths=[file_path])
             # Get metadata
-            metadata = nwb_converter.get_metadata()
+            metadata = interface.get_metadata()
             metadata["NWBFile"].update(session_start_time=self.session_start_time)
 
             # Run conversion
             nwbfile_path = str(self.test_dir / "audio_test.1.nwb")
-
-            # This will fail if the AudioInterface doesn't handle 24-bit WAV files correctly
-            nwb_converter.run_conversion(nwbfile_path=nwbfile_path, metadata=metadata, overwrite=True)
+            interface.run_conversion(nwbfile_path=nwbfile_path, metadata=metadata, overwrite=True)
 
             # Verify the file was created and can be read
             with NWBHDF5IO(path=nwbfile_path, mode="r") as io:
