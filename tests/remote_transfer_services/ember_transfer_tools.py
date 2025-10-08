a3e0428dfe463effcbb53bc4a9009889c741fcf2
CodyCBakerPhD <codycbakerphd@gmail.com>
2025-10-08 01:03:11 -0400

    just monkeypatch user input

--- tests/remote_transfer_services/ember_transfer_tools.py
+++ tests/remote_transfer_services/ember_transfer_tools.py
@@ -12,19 +12,15 @@ from neuroconv.tools.nwb_helpers import (
     make_nwbfile_from_metadata,
 )
 
-DANDI_API_KEY = os.getenv("DANDI_API_KEY")
-if DANDI_API_KEY is not None:
-    del os.environ["DANDI_API_KEY"]  # Will only remove within this test run
-
 EMBER_API_KEY = os.getenv("EMBER_API_KEY")
 HAVE_EMBER_KEY = EMBER_API_KEY is not None and EMBER_API_KEY != ""  # can be "" from external forks
 
 
 @pytest.mark.skipif(
     not HAVE_EMBER_KEY,
-    reason="You must set your HAVE_EMBER_KEY to run this test!",
+    reason="You must set your EMBER_API_KEY to run this test!",
 )
-def test_automatic_ember_upload(tmp_path):
+def test_automatic_ember_upload(tmp_path, monkeypatch):
     nwb_folder_path = tmp_path / "test_nwb"
     nwb_folder_path.mkdir()
     metadata = get_default_nwbfile_metadata()
@@ -36,4 +32,17 @@ def test_automatic_ember_upload(tmp_path):
     with NWBHDF5IO(path=nwb_folder_path / "test_nwb_1.nwb", mode="w") as io:
         io.write(make_nwbfile_from_metadata(metadata=metadata))
 
+    # Note: It is not a valid usage to have a shell that contains both DANDI_API_KEY and EMBER_API_KEY
+    # So in the tests we will ensure that only the appropriate one is set at runtime
+    DANDI_API_KEY_PRESENT = "DANDI_API_KEY" in os.environ
+    if DANDI_API_KEY_PRESENT:
+        DANDI_API_KEY = os.environ.pop(key="DANDI_API_KEY")
+
+    # Some systems and setups (mostly CI) have trouble passing the env variable to the keyring; just mimic user input
+    monkeypatch.setattr("getpass.getpass", lambda _: EMBER_API_KEY)
+    monkeypatch.setattr("builtins.input", lambda _: EMBER_API_KEY)
     automatic_dandi_upload(dandiset_id="000431", nwb_folder_path=nwb_folder_path, instance="ember")
+
+    # Restore the environment variable in case any other tests in this session need it
+    if DANDI_API_KEY_PRESENT:
+        os.environ["DANDI_API_KEY"] = DANDI_API_KEY
