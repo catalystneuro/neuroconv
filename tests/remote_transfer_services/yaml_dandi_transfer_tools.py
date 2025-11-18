3a498d3a9acfe1b4d14bb428e4e7aa41a505cf34
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-11-18 10:41:49 -0800

    Swapped finicky Z time zone with more robust + time zone.

--- tests/remote_transfer_services/yaml_dandi_transfer_tools.py
+++ tests/remote_transfer_services/yaml_dandi_transfer_tools.py
@@ -44,5 +44,5 @@ def test_run_conversion_from_yaml_with_dandi_upload():
 
         # Past uploads may have created the same apparent file, so look at the modification time to ensure
         # this test is actually testing the most recent upload
-        date_modified = datetime.fromisoformat(test_asset_metadata["dateModified"])
+        date_modified = datetime.fromisoformat(test_asset_metadata["dateModified"].replace("Z", "+00:00"))
         assert datetime.now(timezone.utc) - date_modified < timedelta(minutes=10)
