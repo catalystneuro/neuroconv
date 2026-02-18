7418ef98ce69c053eb11df197054a31fe9329e1d
Ben Dichter <ben.dichter@gmail.com>
2026-02-17 22:32:40 -0500

    Apply suggestion from @bendichter

--- src/neuroconv/utils/json_schema.py
+++ src/neuroconv/utils/json_schema.py
@@ -62,8 +62,6 @@ class _NWBConversionOptionsEncoder(_GenericNeuroconvEncoder):
 
     def default(self, obj):
         # Serialize classes to their qualified name (e.g. progress_bar_class in iterator_options)
-        if isinstance(obj, type):
-            return f"{obj.__module__}.{obj.__qualname__}"
 
         # Serialize callable objects (e.g. callback functions in progress_bar_options)
         if callable(obj):
